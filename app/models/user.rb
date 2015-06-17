6675711e150be4ebddb29fdc7c14c47cf6896971
Darin London <darin.london@duke.edu>
2015-06-17 16:46:06 -0400

    a user can now generate a JWT api_token signed with the secret_key_base with id, uuid, and exp 2 hours from now spec passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
+require 'jwt'
+
 class User < ActiveRecord::Base
   has_many :user_authentication_services
   accepts_nested_attributes_for :user_authentication_services
+
+  def api_token
+    JWT.encode({
+      'id' => id,
+      'uuid' => uuid,
+      'exp' => Time.now.to_i + 2.hours.to_i
+    }, Rails.application.secrets.secret_key_base)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 require 'shoulda-matchers'
+require 'jwt'
 
 RSpec.describe User, type: :model do
   subject {FactoryGirl.create(:user)}
 
   it 'should have_many user_authentication_services' do
     should have_many :user_authentication_services
+  end
+
+  describe 'api_token' do
+    it 'should return a JWT signed with the secret_key_base' do
+      expect(subject).to respond_to 'api_token'
+      token = subject.api_token
+      decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      expect(decoded_token).to be
+      expect(decoded_token).to have_key('id')
+      expect(decoded_token['id']).to eq(subject.id)
+      expect(decoded_token).to have_key('uuid')
+      expect(decoded_token['uuid']).to eq(subject.uuid)
+      expect(decoded_token).to have_key('exp')
+      expect(decoded_token['exp']).to eq(Time.now.to_i + 2.hours.to_i)
+    end
   end
 end
