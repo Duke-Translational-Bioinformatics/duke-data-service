d5e2a898fe21d3bb2adc0fe812bfd3a238beda71
Darin London <darin.london@duke.edu>
2015-06-18 10:27:59 -0400

    removed user and authentication_service from the factory these can be set after both the user and authentication_service are created in the tests.

--- spec/factories/user_authentication_services.rb
+++ spec/factories/user_authentication_services.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :user_authentication_service do
     uid { "#{Faker::Name.first_name}_#{Faker::Number.number(3) }" }
-    user
-    authentication_service
   end
 end
