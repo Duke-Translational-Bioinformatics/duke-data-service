180a8e264c9b2e58e590629dcb1e9676b371e331
Darin London <darin.london@duke.edu>
2015-06-22 18:19:08 -0700

    parses the fragment for the access token

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,8 +1,20 @@
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      access_token: ''
+    };
+  },
+  componentDidMount: function() {
+    if (window.location.href.indexOf('#access_token') > 0){
+      var parts = window.location.hash.split('&');
+      this.setState({access_token: parts[0].split('=')[1]});
+    }
+  },
   render: function() {
     return (
       <div>
         <h1>App {this.props.foo}</h1>
+        <p>access_token: {this.state.access_token}</p>
       </div>
     )
   }
