d1621de05e22c56305a33454da7884998eb0883e
James Mosca <jcm73@duke.edu>
2015-06-28 23:40:49 -0400

    Added permission model, spec, and serializer

--- app/models/auth_role.rb
+++ app/models/auth_role.rb
@@ -1,4 +1,7 @@
 class AuthRole < ActiveRecord::Base
+  belongs_to :user
+  has_many :permissions
+
   validates :text_id, presence: true, uniqueness: true
   validates :name, presence: true
   validates :description, presence: true
