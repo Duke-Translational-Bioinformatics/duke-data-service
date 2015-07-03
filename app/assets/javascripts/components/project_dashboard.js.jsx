fa86bf500b64d34fec21df9a42b54b7722ad999e
Darin London <darin.london@duke.edu>
2015-07-03 10:07:22 -0400

    ProjectDashboard   - bugfix changed class= to className=   - modified hardcoded project list to have id key and UUID as value instead of uid as key ProjectList   - passes project.id as key to ProjectSummary instead of project.uid
    Note ProjectList.js.jsx renamed to project_list.js.jsx for consistency in component file naming
    
    ProjectList

--- app/assets/javascripts/components/project_dashboard.js.jsx
+++ app/assets/javascripts/components/project_dashboard.js.jsx
@@ -2,19 +2,19 @@ var ProjectDashboard = React.createClass({
   loadProjects: function() {
     this.setState({projects: [
       {
-        "uid": "ca29f7df-33ca-46dd-a015-92c46fdb6fd1",
+        "id": "ca29f7df-33ca-46dd-a015-92c46fdb6fd1",
         "name": "Knockout Mouse Project (KOMP)",
         "description": "Goal of generating a targeted knockout mutation...",
         "is_deleted": false
       },
       {
-        "uid": "ac927ffd-ca33-dd46-0a51-c492db6fd16f",
+        "id": "ac927ffd-ca33-dd46-0a51-c492db6fd16f",
         "name": "Mouse RNASeq",
         "description": "RNASeq of reverse transcriptase",
         "is_deleted": false
       },
       {
-        "uid": "92c46fdb6fd1-a015-46dd-33ca-ca29f7df",
+        "id": "92c46fdb6fd1-a015-46dd-33ca-ca29f7df",
         "name": "Mouse Behavior",
         "description": "Observations of Mouse behaviors",
         "is_deleted": false
@@ -42,7 +42,7 @@ var ProjectDashboard = React.createClass({
             </MainMenu>
             <form className="navbar-form navbar-left">
               <div className="form-group">
-                <input type="text" class="form-control" placeholder="Search" />
+                <input type="text" className="form-control" placeholder="Search" />
               </div>
             </form>
           </ul>
--- app/assets/javascripts/components/project_list.js.jsx
+++ app/assets/javascripts/components/project_list.js.jsx
@@ -2,7 +2,7 @@ var ProjectList = React.createClass({
   render: function() {
     var projectSummaries = this.props.projects.map(function(project) {
       return (
-        <ProjectSummary key={project.uid} project={project} />
+        <ProjectSummary key={project.id} project={project} />
       )
     });
 
