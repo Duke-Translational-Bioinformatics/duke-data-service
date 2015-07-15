1f612559a75e0054a2a2389c2cc1b6d0f422cf4a
Darrin Mann <darrin.mann@duke.edu>
2015-07-15 17:19:46 -0400

    Adding associations to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :memberships
   has_many :storage_folders
+  has_many :project_permissions
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
