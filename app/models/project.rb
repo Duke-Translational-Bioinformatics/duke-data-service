c19da9a317fddc45f8a7187d0dba2d7f815f2776
James Mosca <jcm73@duke.edu>
2015-07-22 10:19:58 -0400

    Add 'project_admin' auth_role to the User that creates a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,11 @@ class Project < ActiveRecord::Base
   include StringIdCreator
   self.primary_key = 'id'
 
-  before_create :create_string_id  
+  before_create :create_string_id
+  after_save :add_project_admin_role_to_user
   after_initialize :init
 
+  belongs_to :creator, class_name: "User"
   has_many :memberships
   has_many :storage_folders
   has_many :project_permissions
@@ -16,4 +18,11 @@ class Project < ActiveRecord::Base
   def init
     self.is_deleted = false if self.is_deleted.nil?
   end
+
+  private
+  def add_project_admin_role_to_user
+    user = self.creator
+    user.update(auth_role_ids: user.auth_roles=(['project_admin']))
+  end
+
 end
