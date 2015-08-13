4dc54b290bf89900891eec0b2ecaf9cec52e43ff
Darin London <darin.london@duke.edu>
2015-08-13 13:27:14 -0400

    ChunkSpec specifies that Chunk should implement a 'temporary_url' method Chunk passes its spec
    UploadSpec specifies that Upload should implement 'temporary_url', and 'create_manifest' methods
    Upload passes its spec
    
    All of these methods are designed to wrap a storage_provider method with self as an argument
    Consult the StorageProviderSpec for details of these methods behavior

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,4 +2,12 @@ class Upload < ActiveRecord::Base
   belongs_to :project
   belongs_to :storage_provider
   has_many :chunks
+
+  def temporary_url
+    storage_provider.get_signed_url(self)
+  end
+
+  def create_manifest
+    storage_provider.create_slo_manifest(self)
+  end
 end
--- spec/factories/uploads.rb
+++ spec/factories/uploads.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :upload do
-    project_id ""
-name "MyString"
-content_type "MyString"
-size 1
-fingerprint_value "MyString"
-fingerprint_algorithm "MyString"
-storage_provider_id 1
+    project_id { SecureRandom.uuid }
+    name { Faker::Internet.slug }
+    content_type "text/plain"
+    size { Faker::Number.number(2) }
+    fingerprint_value { SecureRandom.hex(32) }
+    fingerprint_algorithm "md5"
+    storage_provider_id { Faker::Number.number(2) }
   end
 
 end
