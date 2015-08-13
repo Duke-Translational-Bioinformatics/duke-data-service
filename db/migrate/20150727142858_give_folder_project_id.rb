6ec711f50f6b6221c7a38cc29a91355f10fec0f0
James Mosca <jcm73@duke.edu>
2015-08-13 11:34:06 -0400

    Deleted files weren't deleted on git

--- db/migrate/20150727142858_give_folder_project_id.rb
+++
@@ -1,5 +0,0 @@
-class GiveFolderProjectId < ActiveRecord::Migration
-  def change
-    add_column :folders, :project_id, :string
-  end
-end
--- db/migrate/20150807155544_add_is_deleted_boolean_to_folders.rb
+++
@@ -1,5 +0,0 @@
-class AddIsDeletedBooleanToFolders < ActiveRecord::Migration
-  def change
-    add_column :folders, :is_deleted, :boolean
-  end
-end
