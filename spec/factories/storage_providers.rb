9855824494efa3498c5dbd6f08ca5e298df46a25
Darrin Mann <darrin.mann@duke.edu>
2015-08-25 16:58:59 -0400

    Filling out the factory to work with local_swift.env

--- spec/factories/storage_providers.rb
+++ spec/factories/storage_providers.rb
@@ -10,8 +10,12 @@ FactoryGirl.define do
     secondary_key { SecureRandom.hex }
 
     trait :swift do
+      name { ENV['SWIFT_ACCT'] || Faker::Name.name }
       url_root { ENV['SWIFT_URL_ROOT'] || 'http://192.168.99.100:12345' }
+      provider_version { ENV['SWIFT_VERSION'] || Faker::App.version }
       auth_uri { ENV['SWIFT_AUTH_URI'] || '/auth/v1.0' }
+      service_user { ENV["SWIFT_USER"] || Faker::Internet.user_name }
+      service_pass { ENV['SWIFT_PASS'] || Faker::Internet.password }
     end
 
     trait :swift_env do
