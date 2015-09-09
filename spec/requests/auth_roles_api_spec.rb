95ce2f6792be84e6b13af43de3d4336d466428e8
Darin London <darin.london@duke.edu>
2015-09-09 14:20:37 -0400

    AuthRoleApiSpec specifies   get /auth_roles     without a context should require authentication and return all AuthRoles     with a context should require authentication and a valid context of project or system       - returns 404 with invalid context       - returns just the AuthRoles for the given context when valid
      get /auth_roles/:id
        requires authentication
        returns the description of the auth_role
        returns 404 when AuthRole not found
    
    AuthRoleApi passes its spec

--- spec/requests/auth_roles_api_spec.rb
+++ spec/requests/auth_roles_api_spec.rb
@@ -1,100 +1,73 @@
 require 'rails_helper'
 
 describe DDS::V1::AuthRolesAPI do
-  let(:json_headers) { { 'Accept' => 'application/json', 'Content-Type' => 'application/json'} }
-  let(:user_auth) { FactoryGirl.create(:user_authentication_service, :populated) }
-  let(:user) { user_auth.user }
-  let (:api_token) { user_auth.api_token }
-  let(:json_headers_with_auth) {{'Authorization' => api_token}.merge(json_headers)}
-  let(:auth_role) { FactoryGirl.create(:auth_role) }
-  let(:serialized_auth_role) { AuthRoleSerializer.new(auth_role).to_json }
+  include_context 'with authentication'
+
+  let(:context) { 'project' }
+  let(:auth_role) { FactoryGirl.create(:auth_role, contexts: [context]) }
+  let(:other_context_auth_role) { FactoryGirl.create(:auth_role, contexts: ['system']) }
+  let(:resource) { auth_role }
+  let(:resource_class) { AuthRole }
+  let(:resource_serializer) { AuthRoleSerializer }
 
   describe 'List authorization roles' do
-    let(:url) { "/api/v1/project/auth_roles" }
-    it 'should return a list of authorization roles for a given context' do
-      get url, nil, json_headers_with_auth
-      expect(response.status).to eq(200)
-      expect(response.body).to be
-      expect(response.body).not_to eq('null')
-      response_json = JSON.parse(response.body)
-      expect(response_json).not_to be_empty
-      expect(response_json).to have_key('results')
-      #Check for exact result
-      expect(response_json['results']).to eq([
-        {
-         'id' => "project_admin",
-         'name' => "Project Admin",
-         'description' => "Can update project details, delete project, manage project level permissions and perform all file operations",
-         'permissions' => [{ 'id' => "view_project"}, {'id' => "update_project"}, {'id' => "delete_project"}, {'id' => "manage_project_permissions"}, {'id' => "download_file"}, {'id' => "create_file"}, {'id' => "update_file"},
-           {'id' => "delete_file"}],
-         'contexts' => [ "project" ],
-         'is_deprecated' => false
-       },
-        {
-         'id' => "project_viewer",
-         'name' => "Project Viewer",
-         'description' => "Can only view project and file meta-data",
-         'permissions' => [{ 'id' => "view_project" }],
-         'contexts' => [ "project" ],
-         'is_deprecated' => false
-       },
-        {
-         'id' => "file_downloader",
-         'name' => "File Downloader",
-         'description' => "Can download files",
-         'permissions' => [{ 'id' => "view_project"}, {'id' => "download_file" }],
-         'contexts' => [ "project" ],
-         'is_deprecated' => false
-       },
-        {
-         'id' => "file_uploader",
-         'name' => "File Uploader",
-         'description' => "Can upload files",
-         'permissions' => [{ 'id' => "view_project"}, {'id' => "create_file" }],
-         'contexts' => [ "project" ],
-         'is_deprecated' => false
-       },
-        {
-         'id' => "file_editor",
-         'name' => "File Editor",
-         'description' => "Can view, download, create, update and delete files",
-         'permissions' => [{ 'id' => "view_project"}, {'id' => "download_file"}, {'id' => "create_file"}, {'id' => "update_file"}, {'id' => "delete_file"}],
-         'contexts' => [ "project" ],
-         'is_deprecated' => false
-        }
-        ])
-      #This isn't really necessary but may be kept if the above hard-coded hash is removed to keep it clean
-      returned_auth_roles = response_json['results']
-      returned_auth_roles.each do |rrole|
-        expect(rrole).to have_key('id')
-        expect(rrole).to have_key('name')
-        expect(rrole).to have_key('description')
-        expect(rrole).to have_key('permissions')
-        expect(rrole).to have_key('contexts')
-        expect(rrole).to have_key('is_deprecated')
+    let(:url) {"/api/v1/auth_roles"}
+
+    describe 'for a context' do
+      let(:payload) {{context: context}}
+      subject { get(url, payload, headers) }
+
+      it_behaves_like 'a listable resource' do
+        it 'should only include authorization_roles for the given context' do
+          expect(auth_role).to be_persisted
+          expect(other_context_auth_role).to be_persisted
+          is_expected.to eq(200)
+          expect(response.body).not_to eq('null')
+          expect(response.body).to include(resource_serializer.new(resource).to_json)
+          expect(response.body).not_to include(resource_serializer.new(other_context_auth_role).to_json)
+        end
+      end
+
+      it_behaves_like 'an authenticated resource'
+
+      describe 'that does not exist' do
+        let!(:payload) {{context: 'notexists'}}
+        it 'should return 404 with error' do
+          is_expected.to eq(404)
+          expect(response.body).to be
+          expect(response.body).not_to eq('null')
+          response_json = JSON.parse(response.body)
+          expect(response_json).to have_key('error')
+          expect(response_json['error']).to eq('404')
+          expect(response_json).to have_key('reason')
+          expect(response_json['reason']).to eq("Unknown Context")
+          expect(response_json).to have_key('suggestion')
+          expect(response_json['suggestion']).to eq("Context should be either project or system")
+        end
       end
     end
 
-    it 'should require an auth token' do
-      get url, nil, json_headers
-      expect(response.status).to eq(401)
+    describe 'without a context' do
+      subject { get(url, nil, headers) }
+
+      it_behaves_like 'a listable resource'
+
+      it_behaves_like 'an authenticated resource'
     end
   end
 
-  describe 'List details of a single authorization role' do
-    let(:auth_role_id) { auth_role.id }
-    let(:url) { "/api/v1/project/auth_roles/#{auth_role_id}" }
-    it 'should return a json with a specific authorization role' do
-      expect(auth_role).to be_persisted
-      get url, nil, json_headers_with_auth
-      expect(response.status).to eq(200)
-      expect(response.body).to be
-      expect(response.body).not_to eq('null')
-      expect(response.body).to include(serialized_auth_role)
-    end
-    it 'should require an auth token' do
-      get url, nil, json_headers
-      expect(response.status).to eq(401)
+  describe 'View authorization role details' do
+    let(:url) {"/api/v1/auth_roles/#{resource.id}"}
+    describe 'GET' do
+      subject { get(url, nil, headers) }
+
+      it_behaves_like 'a viewable resource'
+
+      it_behaves_like 'an authenticated resource'
+
+      it_behaves_like 'an identified resource' do
+        let(:url) {"/api/v1/auth_roles/notexists_authrole_id"}
+      end
     end
   end
 end
