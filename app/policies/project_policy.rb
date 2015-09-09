a5280d8c9aa0c6704d2e71d6c214405229f48c9c
Darrin Mann <darrin.mann@duke.edu>
2015-09-09 13:57:07 -0400

    Adding specs and code for project policy permissions

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,7 +1,19 @@
 class ProjectPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def update?
+    scope.where(:id => record.id).exists?
+  end
+
+  def destroy?
+    scope.where(:id => record.id).exists?
+  end
+
   class Scope < Scope
     def resolve
-      scope
+      scope.joins(:project_permissions).where(project_permissions: {user: user})
     end
   end
 end
