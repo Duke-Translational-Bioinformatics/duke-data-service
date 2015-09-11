0cbef8cf560269c1a2e0d7474d46da6dc3b710d6
Darrin Mann <darrin.mann@duke.edu>
2015-09-11 15:26:35 -0400

    Creating project_permission policy

---
+++ app/policies/project_permission_policy.rb
@@ -0,0 +1,19 @@
+class ProjectPermissionPolicy < ApplicationPolicy
+  def create?
+    scope.where(:id => record.id).exists?
+  end
+
+  def update?
+    scope.where(:id => record.id).exists?
+  end
+
+  def destroy?
+    scope.where(:id => record.id).exists?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.joins(:project_permissions).where(project_permissions: {user: user})
+    end
+  end
+end
