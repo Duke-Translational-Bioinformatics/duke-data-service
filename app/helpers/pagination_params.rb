ce6293ae0171b1ac97d7e1713c1201c8795b516b
Darin London <darin.london@duke.edu>
2015-09-15 16:47:32 -0400

    changed the swagger docs for these parameters

--- app/helpers/pagination_params.rb
+++ app/helpers/pagination_params.rb
@@ -2,7 +2,7 @@ module PaginationParams
   extend Grape::API::Helpers
 
   params :pagination do
-    optional :page, type: Integer, desc: 'Requested Page'
-    optional :per_page, type: Integer, desc: 'Number of Objects per page (default 100)'
+    optional :page, type: Integer, desc: 'Requested Page (default first page)'
+    optional :per_page, type: Integer, desc: 'Number of Objects per page (default 25)'
   end
 end
