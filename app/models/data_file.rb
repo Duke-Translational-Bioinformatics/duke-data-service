83f8b2844cd6d65e0b3e777641523d755cdedfd3
Chet Corey <cc462@rad-chet.dhe.duke.edu>
2015-09-22 10:45:28 -0400

    updated folder_id for models

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -1,7 +1,6 @@
 class DataFile < ActiveRecord::Base
-  has_many :children, class_name: "Folder", foreign_key: "parent_id"
   belongs_to :project
-	belongs_to :parent, class_name: "Folder"
+	belongs_to :folder
   belongs_to :upload
   has_many :project_permissions, through: :project
 
@@ -10,8 +9,8 @@ class DataFile < ActiveRecord::Base
   validates :upload_id, presence: true
 
   def virtual_path
-    if parent
-      [parent.virtual_path, self.name].join('/')
+    if folder
+      [folder.virtual_path, self.name].join('/')
     else
       "/#{self.name}"
     end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,15 +1,22 @@
 class Folder < ActiveRecord::Base
-  has_many :children, class_name: "Folder", foreign_key: "parent_id"
+  after_initialize :init
+
+  has_many :children, class_name: "Folder", foreign_key: "folder_id"
   belongs_to :project
-	belongs_to :parent, class_name: "Folder"
+	belongs_to :folder
   has_many :project_permissions, through: :project
 
   validates :name, presence: true
   validates :project_id, presence: true
 
+  def init
+    self.is_deleted = false if self.is_deleted.nil?
+    self.save
+  end
+
   def virtual_path
-    if parent
-      [parent.virtual_path, self.name].join('/')
+    if folder
+      [folder.virtual_path, self.name].join('/')
     else
       "/#{self.name}"
     end
