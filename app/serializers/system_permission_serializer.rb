fa601792a78dc2a5a8ad8f637e2f4483f0900af7
Darrin Mann <darrin.mann@duke.edu>
2015-09-22 03:07:24 -0400

    Creating and testing SystemPermission serializer

---
+++ app/serializers/system_permission_serializer.rb
@@ -0,0 +1,20 @@
+class SystemPermissionSerializer < ActiveModel::Serializer
+  self.root = false
+  attributes :user, :auth_role
+
+  def user
+    {
+      id: object.user.id,
+      username: object.user.username,
+      full_name: object.user.display_name
+    }
+  end
+
+  def auth_role
+    {
+      id: object.auth_role.id,
+      name: object.auth_role.name,
+      description: object.auth_role.description
+    }
+  end
+end
--- spec/models/system_permission_spec.rb
+++ spec/models/system_permission_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SystemPermission, type: :model do
+  subject { FactoryGirl.create(:system_permission) }
+
   describe 'associations' do
     it 'should have a user' do
       should belong_to(:user)
@@ -12,7 +14,6 @@ RSpec.describe SystemPermission, type: :model do
   end
 
   describe 'validations' do
-    subject { FactoryGirl.create(:system_permission) }
 
     it 'should have a unique user_id' do
       should validate_presence_of(:user_id)
@@ -25,5 +26,24 @@ RSpec.describe SystemPermission, type: :model do
   end
 
   describe 'serialization' do
+    it 'should serialize to json' do
+      serializer = SystemPermissionSerializer.new subject
+      payload = serializer.to_json
+      expect(payload).to be
+      parsed_json = JSON.parse(payload)
+      expect(parsed_json).to have_key('user')
+      expect(parsed_json).to have_key('auth_role')
+      expect(parsed_json['user']).to eq({
+        'id' => subject.user.id,
+        'username' => subject.user.username,
+        'full_name' => subject.user.display_name
+      })
+      expect(subject.auth_role).to be
+      expect(parsed_json['auth_role']).to eq({
+        'id' => subject.auth_role.id,
+        'name' => subject.auth_role.name,
+        'description' => subject.auth_role.description
+      })
+    end
   end
 end
