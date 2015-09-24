c90f32b08cdd6964f15c2e7c9cc32220691b4a33
Darin London <darin.london@duke.edu>
2015-09-24 11:21:49 -0400

    added section to launch the local swift server if swift.env is not empty

--- launch_application.sh
+++ launch_application.sh
@@ -4,12 +4,21 @@ if [ -z $COMPOSE_FILE ]
 then
   docker-compose up -d
   docker-compose -f dc-dev.utils.yml run rake db:migrate
+  docker-compose -f dc-dev.utils.yml run rake db:seed
   docker-compose -f dc-dev.utils.yml run authservice
-    docker-compose -f dc-dev.utils.yml run rake storage_provider:create
+  docker-compose -f dc-dev.utils.yml run rake storage_provider:create
+  if [ -s swift.env ]
+  then
+    docker-compose -f dc-dev.utils.yml up -d swift
+  fi
 else
   docker-compose up -d server
   docker-compose run rake db:migrate
   docker-compose run rake db:seed
   docker-compose run authservice
   docker-compose run rake storage_provider:create
+  if [ -s swift.env ]
+  then
+    docker-compose up -d swift
+  fi
 fi
