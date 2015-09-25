53d7bdd3555ea7135e2d5e654517d8a8dc30c5d9
Darin London <darin.london@duke.edu>
2015-09-25 12:47:46 -0400

    if the swift.env file is not empty it will launch the swift service before creating the storage_provider

--- launch_application.sh
+++ launch_application.sh
@@ -6,19 +6,19 @@ then
   docker-compose -f dc-dev.utils.yml run rake db:migrate
   docker-compose -f dc-dev.utils.yml run rake db:seed
   docker-compose -f dc-dev.utils.yml run authservice
-  docker-compose -f dc-dev.utils.yml run rake storage_provider:create
   if [ -s swift.env ]
   then
     docker-compose -f dc-dev.utils.yml up -d swift
   fi
+  docker-compose -f dc-dev.utils.yml run rake storage_provider:create
 else
   docker-compose up -d server
   docker-compose run rake db:migrate
   docker-compose run rake db:seed
   docker-compose run authservice
-  docker-compose run rake storage_provider:create
   if [ -s swift.env ]
   then
     docker-compose up -d swift
   fi
+  docker-compose run rake storage_provider:create
 fi
