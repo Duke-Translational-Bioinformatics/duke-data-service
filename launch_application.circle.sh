17ff681d9a6f169af9c8779cce4b5d6dd3cea127
Darin London <darin.london@duke.edu>
2015-10-14 12:17:42 -0400

    try using sudo with docker so that it can remove intermediate containers launch_application sleeps a bit longer after starting the server before migrating the db

--- launch_application.circle.sh
+++ launch_application.circle.sh
@@ -5,7 +5,9 @@ sleep 5
 docker-compose ps
 curl http://swift.circle.host:12345/info
 docker-compose up -d server
-sleep 5
+echo "SLEEPING"
+sleep 10
+docker-compose ps
 docker-compose run rake db:migrate
 docker-compose run rake db:seed
 docker-compose run authservice
