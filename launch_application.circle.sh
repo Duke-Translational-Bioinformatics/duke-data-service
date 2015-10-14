0aa7ae4249c4aae263f3feba47ed074a5a16f7de
Darin London <darin.london@duke.edu>
2015-10-14 09:48:00 -0400

    more circle troubleshooting

--- launch_application.circle.sh
+++ launch_application.circle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+docker-compose up -d swift
+curl http:0.0.0.0:12345/info
+sleep 5
 docker-compose up -d server
 sleep 5
 docker-compose run rake db:migrate
 docker-compose run rake db:seed
 docker-compose run authservice
-docker-compose up -d swift
-sleep 5
 docker-compose run rake storage_provider:create
