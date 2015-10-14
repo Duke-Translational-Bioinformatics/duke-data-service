e75452ea684837a234d99abe5d80ee5d276826b4
Darin London <darin.london@duke.edu>
2015-10-14 11:47:57 -0400

    circle.yml   - adds hosts entries for dds.circle.host and swift.circle.host to 0.0.0.0 swift.circle.env   - sets swift.circle.host as the SWIFT_URL_ROOT (which should work both inside the container with a link, and on the circle host using circle.yml hosts docker-compose.circle.sh   - links swift into server as swift.circle.host launch_application.circle.sh   - tests connection to http://swift.circle.host:12345/info

--- launch_application.circle.sh
+++ launch_application.circle.sh
@@ -3,7 +3,7 @@
 docker-compose up -d swift
 sleep 5
 docker-compose ps
-curl http://0.0.0.0:12345/info
+curl http://swift.circle.host:12345/info
 docker-compose up -d server
 sleep 5
 docker-compose run rake db:migrate
