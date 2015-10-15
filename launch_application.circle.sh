ba7b2eecce76ca5d1b1090b29d7c94ebfef5ce79
Darin London <darin.london@duke.edu>
2015-10-15 12:04:16 -0400

    switched creating the storage_provider back to the launch_application instead of as a seperate post test in circle.yml added links to swift to the docker-compose.circle.yml rake service definition so storage_provider:create works.

--- launch_application.circle.sh
+++ launch_application.circle.sh
@@ -7,3 +7,4 @@ curl http://swift.circle.host:8080/info
 docker-compose run rake db:migrate
 docker-compose run rake db:seed
 docker-compose run authservice
+docker-compose run rake storage_provider:create
