30f24b875d1cf4237990022bf65ae31adefbc1b8
Darrin Mann <darrin.mann@duke.edu>
2015-10-29 15:21:20 -0400

    First pass at api endpiont for project children

--- app/api/dds/v1/children_api.rb
+++ app/api/dds/v1/children_api.rb
@@ -16,6 +16,22 @@ module DDS
         authorize folder, :show?
         #folder
       end
+
+      desc 'List project children' do
+        detail 'Returns the immediate children of the project.'
+        named 'list project children'
+        failure [
+          [200, "Valid API Token in 'Authorization' Header"],
+          [401, "Missing, Expired, or Invalid API Token in 'Authorization' Header"],
+          [404, 'Project does not exist']
+        ]
+      end
+      get '/projects/:id/children', root: false do
+        authenticate!
+        project = hide_logically_deleted Project.find(params[:id])
+        authorize project, :show?
+        #folder
+      end
     end
   end
 end
--- spec/requests/children_api_spec.rb
+++ spec/requests/children_api_spec.rb
@@ -8,12 +8,15 @@ describe DDS::V1::ChildrenAPI do
   let(:project) { folder.project }
   let(:file) { FactoryGirl.create(:data_file, parent: parent) }
 
-  let(:file_class) { DataFile }
-  let(:file_serializer) { DataFileSerializer }
+  let(:root_folder) { FactoryGirl.create(:folder, :root, project: project) }
+  let(:root_file) { FactoryGirl.create(:data_file, :root, project: project) }
 
   let(:other_permission) { FactoryGirl.create(:project_permission, user: current_user) }
   let(:other_folder) { FactoryGirl.create(:folder, project: other_permission.project) }
   let(:deleted_folder) { FactoryGirl.create(:folder, :deleted, project: project) }
+
+  let(:file_class) { DataFile }
+  let(:file_serializer) { DataFileSerializer }
 
   let(:resource_class) { Folder }
   let(:resource_serializer) { FolderSerializer }
@@ -49,6 +52,41 @@ describe DDS::V1::ChildrenAPI do
       end
       it_behaves_like 'a logically deleted resource' do
         let(:deleted_resource) { parent }
+      end
+    end
+  end
+
+  describe 'Project children collection' do
+    let(:resource) { root_folder }
+    let(:parent_id) { project.id }
+    let(:url) { "/api/v1/projects/#{parent_id}/children" }
+
+    describe 'GET' do
+      subject { get(url, nil, headers) }
+
+      it_behaves_like 'a listable resource' do
+        let(:unexpected_resources) { [
+          deleted_folder,
+          other_folder
+        ] }
+      end
+
+      context 'with file child' do
+        let(:resource_class) { file_class }
+        let(:resource_serializer) { file_serializer }
+        let!(:resource) { root_file }
+
+        it_behaves_like 'a listable resource'
+      end
+
+      it_behaves_like 'an authenticated resource'
+      it_behaves_like 'an authorized resource'
+      it_behaves_like 'an identified resource' do
+        let(:parent_id) { 'notfoundid' }
+        let(:resource_class) { Project }
+      end
+      it_behaves_like 'a logically deleted resource' do
+        let(:deleted_resource) { project }
       end
     end
   end
