9439ff9bdec228d6927689ddf8814a8f275298b7
chet <chetcorey@hotmail.com>
2015-10-29 12:13:38 -0400

    container added as STI

---
+++ db/migrate/20151029161136_create_containers.rb
@@ -0,0 +1,15 @@
+class CreateContainers < ActiveRecord::Migration
+  def change
+    create_table :containers, id: :uuid do |t|
+      t.string :name
+      t.string :type
+      t.uuid :parent_id
+      t.uuid :project_id
+      t.uuid :creator_id
+      t.uuid :upload_id
+      t.boolean :is_deleted
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/containers.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :container do
+    name "MyString"
+    type ""
+    parent_id ""
+    project_id ""
+    creator_id ""
+    upload_id ""
+    is_deleted false
+  end
+
+end
