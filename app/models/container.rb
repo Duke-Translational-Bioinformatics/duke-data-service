f92e12d817d9b4b63f6135039872c89fcb3c874e
chet <chetcorey@hotmail.com>
2015-11-02 11:15:40 -0500

    Folder and DataFile Models inheriting Container
    Folder and DataFile Models are inheriting the Container class through single table inheritance.
    The tests are passing with the new arrangement.
    The immutable validator needs to be added to the Container for project_id, only after it has passing tests for both Folder and File.

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,2 +1,19 @@
 class Container < ActiveRecord::Base
+  include SerializedAudit
+  include Kinded
+
+  audited
+  belongs_to :project
+	belongs_to :parent, class_name: "Folder"
+  has_many :project_permissions, through: :project
+
+  validates :name, presence: true
+
+  def virtual_path
+    if parent
+      [parent.virtual_path, self.name].join('/')
+    else
+      "/#{self.name}"
+    end
+  end
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,26 +1,12 @@
+# Folder and DataFile are siblings in the Container class through single table inheritance.
+
 class Folder < Container
-  include SerializedAudit
-  include Kinded
-
-  audited
   has_many :children, class_name: "Folder", foreign_key: "parent_id"
-  belongs_to :project
-	belongs_to :parent, class_name: "Folder"
-  has_many :project_permissions, through: :project
 
-  validates :name, presence: true
   validates :project_id, presence: true, immutable: true
   validates :is_deleted, absence: true, if: :children_present?
 
   def children_present?
     !children.empty?
-  end
-
-  def virtual_path
-    if parent
-      [parent.virtual_path, self.name].join('/')
-    else
-      "/#{self.name}"
-    end
   end
 end
