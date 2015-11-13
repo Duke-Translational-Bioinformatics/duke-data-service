2c4731f204ee13dea6e7fc4a9cdeebb9875255c1
Darin London <darin.london@duke.edu>
2015-11-13 12:04:09 -0500

    The default version4 UUID is not sortable, but the version1 includes the mac address, which may cause trouble for URLs and JSON serialization, and is a little less secure. Adding a default_scope to these to sort makes most of our tests pass. Modified the new_object definition in project_affiliates_api_spec and uploads_api_spec to find the expected new_object based on the payload, instead of relying on resource_class.last which does not work as expected, even with a default scope.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  default_scope { order('created_at DESC') }
   include SerializedAudit
   include Kinded
   audited
--- app/models/user_authentication_service.rb
+++ app/models/user_authentication_service.rb
@@ -1,4 +1,5 @@
 class UserAuthenticationService < ActiveRecord::Base
+  default_scope { order('created_at DESC') }
   belongs_to :user
   belongs_to :authentication_service
 
