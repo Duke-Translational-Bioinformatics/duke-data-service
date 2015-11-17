04f623801ea7b30b074f93239e2eec3b394dd52e
Darin London <darin.london@duke.edu>
2015-11-17 11:07:32 -0500

    creating file was not using the new endpoint specified in apiary

--- workflow.sh
+++ workflow.sh
@@ -19,7 +19,8 @@ then
   exit 1
 fi
 echo ${resp} | jq
-project_id=`echo ${resp} | jq '.id' | sed 's/\"//g'`
+project_id=`echo ${resp} | jq -r '.id'`
+project_kind=`echo ${resp} | jq -r '.kind'`
 upload_size=`wc -c test_file.txt | awk '{print $1}'`
 upload_md5=`md5 test_file.txt | awk '{print $NF}'`
 echo "creating upload"
@@ -30,7 +31,7 @@ then
   exit 1
 fi
 echo ${resp} | jq
-upload_id=`echo ${resp} | jq '.id' | sed 's/\"//g'`
+upload_id=`echo ${resp} | jq -r '.id'`
 for chunk in chunk*.txt
 do
    md5=`md5 ${chunk} | awk '{print $NF}'`
@@ -44,8 +45,8 @@ do
      exit 1
    fi
    echo ${resp} | jq
-   host=`echo ${resp} | jq '.host' | sed 's/\"//g'`
-   put_url=`echo ${resp} | jq '.url'| sed 's/\"//g'`
+   host=`echo ${resp} | jq -r '.host'`
+   put_url=`echo ${resp} | jq -r '.url'`
    echo "posting data"
    curl -v -T ${chunk} "${host}${put_url}"
    if [ $? -gt 0 ]
@@ -63,20 +64,22 @@ then
 fi
 echo ${resp} | jq
 echo "creating file"
-resp=`curl -# -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: ${auth_token}" -d '{"upload":{"id":"'${upload_id}'"}}' "http://192.168.99.100:3001/api/v1/projects/${project_id}/files"`
+resp=`curl -# -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: ${auth_token}" -d '{"parent":{"kind":"'${project_kind}'","id":"'${project_id}'"},"upload":{"id":"'${upload_id}'"}}' "http://192.168.99.100:3001/api/v1/files"`
 if [ $? -gt 0 ]
 then
   echo "Problem! ${resp}"
   exit 1
 fi
 echo ${resp} | jq
-file_id=`echo $resp | jq '.id' | sed 's/\"//g'`
+file_id=`echo $resp | jq -r '.id'`
+echo "FILE ${file_id} Created:"
 curl -# --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: ${auth_token}" "http://192.168.99.100:3001/api/v1/files/${file_id}" | jq
 if [ $? -gt 0 ]
 then
   echo "Problem!"
   exit 1
 fi
+echo "FILE ${file_id} download:"
 curl -# -L --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: ${auth_token}" "http://192.168.99.100:3001/api/v1/files/${file_id}/download"
 if [ $? -gt 0 ]
 then
