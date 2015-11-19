cc0756a139083566e0d60624fceb0c4f57438e7b
Darin London <darin.london@duke.edu>
2015-11-19 08:58:54 -0500

    sinatra can now serve assets on the /portal/assets endpoint so they no longer need to reside in public
    index.erb uses asset_path in front of the asset paths
    portal.rb sets the asset_path in calls to the index

--- portal.rb
+++ portal.rb
@@ -3,9 +3,18 @@ require 'sinatra/base'
 class Portal < Sinatra::Base
   set :root, File.dirname(__FILE__)
   set :views, settings.root + '/portal'
-  set :public_folder, settings.root + '/portal'
 
-  get '/' do
+  get /asset\/*(.*)/ do |asset|
+    if asset == '/lib/config.js'
+      @authServiceName = 'FOO'
+      erb :'lib/config.js'
+    else
+      send_file "#{settings.root}/portal/#{asset}"
+    end
+  end
+
+  get '/*' do
+    @asset_path = "/portal/asset/"
     erb :index
   end
 end
