39755e04480ccff0b2b5a54604f12870969e202c
Darin London <darin.london@duke.edu>
2015-11-19 11:07:21 -0500

    moved the git clone of the portal into the script so that it gets the current version at run time instead of build time

--- docker/builds/genportal/genportal.sh
+++ docker/builds/genportal/genportal.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+cd /var/www
+git clone https://github.com/Duke-Translational-Bioinformatics/duke-data-service-portal.git
+cd /var/www/duke-data-service-portal
 npm install react-cookie
 npm install
 gulp build --type production
 mv dist/index.html /var/www/app/portal/index.erb
-rsync -avz dist/ /var/www/app/public/
+rsync -avz dist/ /var/www/app/portal/
 echo "Generated"
 exit
