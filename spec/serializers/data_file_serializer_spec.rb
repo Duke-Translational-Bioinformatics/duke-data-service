fbdaa87c606e306711e7be82601b431c05d0c8a2
Darrin Mann <darrin.mann@duke.edu>
2015-12-01 11:29:10 -0500

    Spec testing for has_many :ancestors and removing ancestor hash tests.

--- spec/serializers/data_file_serializer_spec.rb
+++ spec/serializers/data_file_serializer_spec.rb
@@ -7,53 +7,18 @@ RSpec.describe DataFileSerializer, type: :serializer do
 
   it_behaves_like 'a has_one association with', :upload, UploadPreviewSerializer
   it_behaves_like 'a has_one association with', :project, ProjectPreviewSerializer
+  it_behaves_like 'a has_many association with', :ancestors, AncestorSerializer
 
   it_behaves_like 'a json serializer' do
     it 'should have expected keys and values' do
       is_expected.to have_key('id')
       is_expected.to have_key('parent')
       is_expected.to have_key('name')
-      is_expected.to have_key('ancestors')
       is_expected.to have_key('is_deleted')
       expect(subject['id']).to eq(resource.id)
       expect(subject['parent']['id']).to eq(resource.parent_id)
       expect(subject['name']).to eq(resource.name)
       expect(subject['is_deleted']).to eq(resource.is_deleted)
-    end
-
-    describe 'ancestors' do
-      context 'with a parent folder' do
-        let(:resource) { child_file }
-        it 'should return the project and parent' do
-          expect(resource.project).to be
-          expect(resource.parent).to be
-          expect(subject['ancestors']).to eq [
-            {
-              'kind' => resource.project.kind,
-              'id' => resource.project.id,
-              'name' => resource.project.name
-            },
-            {
-              'kind' => resource.parent.kind,
-              'id' => resource.parent.id,
-              'name' => resource.parent.name
-            }
-          ]
-        end
-      end
-
-      context 'without a parent' do
-        let(:resource) { root_file }
-        it 'should return the project' do
-          expect(resource.project).to be
-          expect(subject['ancestors']).to eq [
-            {
-              'kind' => resource.project.kind,
-              'id' => resource.project.id,
-              'name' => resource.project.name }
-          ]
-        end
-      end
     end
   end
 end
