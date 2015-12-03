a8f5782f154316f89006047bbf9ca8ab77bf31a2
Darin London <darin.london@duke.edu>
2015-12-03 12:27:30 -0500

    the genportal.sh script now removes everything in the DDS portal/ directory before installing the freshly built artifacts. This ensures that deletions are respected.

--- docker/builds/genportal/genportal.sh
+++ docker/builds/genportal/genportal.sh
@@ -6,6 +6,7 @@ cd /var/www/duke-data-service-portal
 npm install react-cookie
 npm install
 gulp build --type production
+rm -rf /var/www/app/portal/*
 mv dist/index.html /var/www/app/portal/index.erb
 rsync -avz dist/ /var/www/app/portal/
 echo "Generated"
