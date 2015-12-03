a3e1f8471bc8f68014cca7ad612695769688b2b8
Darin London <darin.london@duke.edu>
2015-12-03 12:33:10 -0500

    no longer serves lib/config.js sets up the serviceID, baseUrl, authServiceUri, authServiceName, and securityState expected by the index.erb DDS_PORTAL_CONFIG uses Environment variables that are shared by the Rails application

--- portal.rb
+++ portal.rb
@@ -5,16 +5,16 @@ class Portal < Sinatra::Base
   set :views, settings.root + '/portal'
 
   get /asset\/*(.*)/ do |asset|
-    if asset == '/lib/config.js'
-      @authServiceName = 'FOO'
-      erb :'lib/config.js'
-    else
-      send_file "#{settings.root}/portal/#{asset}"
-    end
+    send_file "#{settings.root}/portal/#{asset}"
   end
 
   get '/*' do
     @asset_path = "/portal/asset/"
+    @serviceID = ENV['SERVICE_ID']
+    @baseUrl = request.url.gsub(request.path, '')
+    @authServiceUri = ENV['AUTH_SERVICE_BASE_URI']
+    @authServiceName = ENV['AUTH_SERVICE_NAME']
+    @securityState = SecureRandom.hex
     erb :index
   end
 end
