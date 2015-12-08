8548336b06ea64860687e73a35c30af7029c87c3
Darin London <darin.london@duke.edu>
2015-12-08 11:09:38 -0500

    added some statements to help see how cache is being used in the circle console

--- docker/circle/cache_ruby.sh
+++ docker/circle/cache_ruby.sh
@@ -5,8 +5,11 @@ if [ $? -gt 0 ]
 then
   if [ ! -e docker/circle/ruby_2.2.2.docker.tgz ]
   then
+    echo "pulling and caching ruby:2.2.2"
     docker pull ruby:2.2.2
     docker save ruby:2.2.2 | gzip > docker/circle/ruby_2.2.2.docker.tgz
   fi
+  echo "loading cached ruby:2.2.2"
   docker load -i docker/circle/ruby_2.2.2.docker.tgz
 fi
+echo "ruby:2.2.2 installed"
