665ba9bafd0cfe9d0f1d6217af4df1b0216394b6
Darin London <darin.london@duke.edu>
2015-12-10 10:03:54 -0500

    uses the local dds branch as the branch to fetch of the repo to build

--- docker/builds/genportal/genportal.sh
+++ docker/builds/genportal/genportal.sh
@@ -4,16 +4,23 @@ last_commit=`awk '{print $1}' /var/www/app/portal.version`
 next_commit=`git ls-remote https://github.com/Duke-Translational-Bioinformatics/duke-data-service-portal.git refs/heads/develop | awk '{print $1}'`
 if [ ${last_commit} != ${next_commit} ]
 then
+  cd /var/www/app
+  dds_branch=`git branch | grep '^*' | awk '{print $NF}'`
   cd /var/www
   git clone https://github.com/Duke-Translational-Bioinformatics/duke-data-service-portal.git
   cd /var/www/duke-data-service-portal
+  portal_branch=`git branch | grep '^*' | awk '{print $NF}'`
+  if [ ${dds_branch} != ${portal_branch} ]
+  then
+    git checkout ${dds_branch}
+  fi
   npm install react-cookie
   npm install
   gulp build --type production
   rm -rf /var/www/app/portal/*
   mv dist/index.html /var/www/app/portal/index.erb
   rsync -avz dist/ /var/www/app/portal/ /var/www/app/portal.version
-  git ls-remote https://github.com/Duke-Translational-Bioinformatics/duke-data-service-portal.git refs/heads/develop > /var/www/app/portal.version
+  git ls-remote https://github.com/Duke-Translational-Bioinformatics/duke-data-service-portal.git "refs/heads/${dds_branch}" > /var/www/app/portal.version
   echo "Generated ${next_commit}"
 else
   echo "nothing new"
