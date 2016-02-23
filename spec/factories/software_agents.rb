97099402fa2e0d5124e56c4b60f6ac6e0e46b927
Darrin Mann <darrin.mann@duke.edu>
2016-02-23 11:00:22 -0500

    Initial commit for the SoftwareAgent migration, model, and serializer.

---
+++ spec/factories/software_agents.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :software_agent do
+    name { "#{Faker::Team.name}_#{rand(10**3)}" }
+    description { Faker::Hacker.say_something_smart }
+    repo_url { Faker::Internet.url }
+    association :creator, factory: :user
+
+    trait :deleted do
+      is_deleted true
+    end
+  end
+end
