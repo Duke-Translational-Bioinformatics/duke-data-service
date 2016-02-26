abf746afeaac90d9637ce76600c039882717d40a
Darin London <darin.london@duke.edu>
2016-02-26 12:17:45 -0500

    UserAuthenticationSpec no longer expects UserAuthenticationService to support the api_token method
    UserAuthenticationService no longer supports the api_token method
    
    ApiTokenSpec expects the api_token method to work without arguments
    ApiToken meets expectations

--- spec/models/api_token_spec.rb
+++ spec/models/api_token_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ApiToken do
     }.to raise_error(RuntimeError)
   end
 
-  context 'UserAuthenticationService Authenticated User' do
+  context 'with UserAuthenticationService Authenticated User' do
     let(:auth_service) { FactoryGirl.create(:authentication_service) }
     let(:user_authentication_service) { FactoryGirl.create(:user_authentication_service,
       user: user,
@@ -26,16 +26,25 @@ RSpec.describe ApiToken do
       }.to raise_error(RuntimeError)
     end
 
-    it 'should return an api_token for the user and authentication_service' do
-      token = subject.api_token
-      decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-      expect(decoded_token).to be
-      expect(decoded_token).to have_key('id')
-      expect(decoded_token['id']).to eq(user.id)
-      expect(decoded_token).to have_key('service_id')
-      expect(decoded_token['service_id']).to eq(auth_service.service_id)
-      expect(decoded_token).to have_key('exp')
-      expect(decoded_token['exp']).to eq(Time.now.to_i + 2.hours.to_i)
+    describe 'api_token method' do
+      it 'should not require arguments' do
+        expect(subject).to respond_to 'api_token'
+        expect{
+          subject.api_token
+        }.not_to raise_error
+      end
+
+      it 'should return JWT signed with the secret_key_base for the user and authentication_service' do
+        token = subject.api_token
+        decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        expect(decoded_token).to be
+        expect(decoded_token).to have_key('id')
+        expect(decoded_token['id']).to eq(user.id)
+        expect(decoded_token).to have_key('service_id')
+        expect(decoded_token['service_id']).to eq(auth_service.service_id)
+        expect(decoded_token).to have_key('exp')
+        expect(decoded_token['exp']).to eq(Time.now.to_i + 2.hours.to_i)
+      end
     end
   end
 end
