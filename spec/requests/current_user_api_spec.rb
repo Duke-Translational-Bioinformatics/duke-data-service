2f1e8944ee6c96a8543a25f129a3246c5532da3c
Darin London <darin.london@duke.edu>
2016-02-29 11:49:20 -0500

    refactored get /current_user tests to use shared_examples and contexts

--- spec/requests/current_user_api_spec.rb
+++ spec/requests/current_user_api_spec.rb
@@ -3,79 +3,79 @@ require 'jwt'
 require 'securerandom'
 
 describe DDS::V1::CurrentUserAPI do
-  let(:json_headers) { { 'Accept' => 'application/json', 'Content-Type' => 'application/json'} }
-  let (:auth_service) { FactoryGirl.create(:authentication_service) }
-  let (:user) { FactoryGirl.create(:user) }
-  let (:user_authentication_service) {
-    FactoryGirl.create(:user_authentication_service,
-      user: user,
-      authentication_service: auth_service,
-    )
-  }
-  let (:api_token) { user_authentication_service.api_token }
+  let(:resource) { current_user }
+  let(:resource_class) { User }
+  let(:resource_serializer) { UserSerializer }
 
   describe 'get /current_user' do
-    let(:wrong_secret_api_token) {
-      JWT.encode({
-        'id' => user.id,
-        'authentication_service_id' => user_authentication_service.authentication_service_id,
-        'exp' => Time.now.to_i + 2.hours.to_i
-      }, 'nottherightsecret')
-    }
-    let(:expired_api_token) {
-      JWT.encode({
-        'id' => user.id,
-        'authentication_service_id' => user_authentication_service.authentication_service_id,
-        'exp' => Time.now.to_i - 2.hours.to_i,
-      }, Rails.application.secrets.secret_key_base)
-    }
+    let(:url) { '/api/v1/current_user' }
+    subject { get(url, nil, headers) }
 
-    it 'should return JSON serialized User when provided a valid api_token as the Authorization Header' do
-      get '/api/v1/current_user', nil, {'Authorization' => api_token}.merge(json_headers)
-      expect(response.status).to eq(200)
-      expect(response.body).to be
-      expect(response.body).not_to eq('null')
-      expect(response.status).to eq(200)
-      expect(response.body).to eq(UserSerializer.new(user, root: false).to_json)
+    context 'with valid api_token' do
+      include_context 'with authentication'
+      it_behaves_like 'a viewable resource'
     end
 
-    it 'should respond with an error when not provided an api_token' do
-      get '/api/v1/current_user', nil, json_headers
-      expect(response.status).to eq(401)
-      expect(response.body).to be
-      error_response = JSON.parse(response.body)
-      %w(error reason suggestion).each do |expected_key|
-        expect(error_response).to have_key expected_key
+    context 'when not provided an api_token' do
+      include_context 'without authentication'
+
+      it 'should respond with an error' do
+        is_expected.to eq(401)
+        expect(response.body).to be
+        error_response = JSON.parse(response.body)
+        %w(error reason suggestion).each do |expected_key|
+          expect(error_response).to have_key expected_key
+        end
+        expect(error_response['error']).to eq(401)
+        expect(error_response['reason']).to eq('no api_token')
+        expect(error_response['suggestion']).to eq('you might need to login through an authenticaton service')
       end
-      expect(error_response['error']).to eq(401)
-      expect(error_response['reason']).to eq('no api_token')
-      expect(error_response['suggestion']).to eq('you might need to login through an authenticaton service')
     end
 
-    it 'should respond with an error when the api_token has not been signed by the secret_key_base' do
-      get '/api/v1/current_user', nil, {'Authorization' => wrong_secret_api_token}.merge(json_headers)
-      expect(response.status).to eq(401)
-      expect(response.body).to be
-      error_response = JSON.parse(response.body)
-      %w(error reason suggestion).each do |expected_key|
-        expect(error_response).to have_key expected_key
+    context 'when the api_token has not been signed by the secret_key_base' do
+      include_context 'with authentication'
+      let(:api_token) {
+        JWT.encode({
+          'id' => current_user.id,
+          'authentication_service_id' => user_auth.authentication_service_id,
+          'exp' => Time.now.to_i + 2.hours.to_i
+        }, 'nottherightsecret')
+      }
+
+      it 'should respond with an error' do
+        is_expected.to eq(401)
+        expect(response.body).to be
+        error_response = JSON.parse(response.body)
+        %w(error reason suggestion).each do |expected_key|
+          expect(error_response).to have_key expected_key
+        end
+        expect(error_response['error']).to eq(401)
+        expect(error_response['reason']).to eq('invalid api_token')
+        expect(error_response['suggestion']).to eq('token not properly signed')
       end
-      expect(error_response['error']).to eq(401)
-      expect(error_response['reason']).to eq('invalid api_token')
-      expect(error_response['suggestion']).to eq('token not properly signed')
     end
 
-    it 'should respond with an error when the api_token has expired' do
-      get '/api/v1/current_user', nil, {'Authorization' => expired_api_token}.merge(json_headers)
-      expect(response.status).to eq(401)
-      expect(response.body).to be
-      error_response = JSON.parse(response.body)
-      %w(error reason suggestion).each do |expected_key|
-        expect(error_response).to have_key expected_key
+    context 'when the api_token has expired' do
+      include_context 'with authentication'
+      let(:api_token) {
+        JWT.encode({
+          'id' => current_user.id,
+          'authentication_service_id' => user_auth.authentication_service_id,
+          'exp' => Time.now.to_i - 2.hours.to_i,
+        }, Rails.application.secrets.secret_key_base)
+      }
+
+      it 'should respond with an error' do
+        is_expected.to eq(401)
+        expect(response.body).to be
+        error_response = JSON.parse(response.body)
+        %w(error reason suggestion).each do |expected_key|
+          expect(error_response).to have_key expected_key
+        end
+        expect(error_response['error']).to eq(401)
+        expect(error_response['reason']).to eq('expired api_token')
+        expect(error_response['suggestion']).to eq('you need to login with your authenticaton service')
       end
-      expect(error_response['error']).to eq(401)
-      expect(error_response['reason']).to eq('expired api_token')
-      expect(error_response['suggestion']).to eq('you need to login with your authenticaton service')
     end
   end
 
@@ -84,8 +84,7 @@ describe DDS::V1::CurrentUserAPI do
 
     let(:url) { '/api/v1/current_user/usage' }
     subject { get(url, nil, headers) }
-    let(:resource) { user }
-    let(:resource_class) { User }
+    let(:resource) { current_user }
     let(:resource_serializer) { UserUsageSerializer }
 
     it_behaves_like 'a viewable resource'
