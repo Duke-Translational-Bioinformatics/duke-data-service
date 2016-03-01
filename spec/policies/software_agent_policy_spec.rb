2d7c18e1cadbdbbcc9be787eaa99dff8f0b57a26
Darrin Mann <darrin.mann@duke.edu>
2016-03-01 16:19:33 -0500

    DRYing up the SoftwareAgentPolicy specs

--- spec/policies/software_agent_policy_spec.rb
+++ spec/policies/software_agent_policy_spec.rb
@@ -1,46 +1,55 @@
 require 'rails_helper'
 
 describe SoftwareAgentPolicy do
+  include_context 'policy declarations'
 
-  let(:user) { FactoryGirl.create(:user) }
-  let!(:software_agent) { FactoryGirl.create(:software_agent) }
-  let(:creator) { software_agent.creator }
-  let(:system_admin) { system_permission.user }
-  let(:system_permission) { FactoryGirl.create(:system_permission) }
+  let(:software_agent) { FactoryGirl.create(:software_agent) }
+  let(:other_software_agent) { FactoryGirl.create(:software_agent) }
 
-  let(:user_scope) { subject.new(user, software_agent).scope }
-  let(:system_admin_scope) { subject.new(system_admin, software_agent).scope }
-  let(:creator_scope) { subject.new(creator, software_agent).scope }
+  context 'when user is creator of software_agent' do
+    let(:user) { software_agent.creator }
 
-  subject { described_class }
-
-  permissions ".scope" do
-    it {expect(user_scope.all).to include(software_agent)}
-    it {expect(system_admin_scope.all).to include(software_agent)}
-    it {expect(creator_scope.all).to include(software_agent)}
+    describe '.scope' do
+      it { expect(resolved_scope).to include(software_agent) }
+      it { expect(resolved_scope).to include(other_software_agent) }
+    end
+    permissions :show?, :create? do
+      it { is_expected.to permit(user, software_agent) }
+      it { is_expected.to permit(user, other_software_agent) }
+    end
+    permissions :update?, :destroy? do
+      it { is_expected.to permit(user, software_agent) }
+      it { is_expected.not_to permit(user, other_software_agent) }
+    end
   end
 
-  permissions :show? do
-    it {is_expected.to permit(user, software_agent)}
-    it {is_expected.to permit(creator, software_agent)}
-    it {is_expected.to permit(system_admin, software_agent)}
+  context 'when user does not have system_permission' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    describe '.scope' do
+      it { expect(resolved_scope).to include(software_agent) }
+      it { expect(resolved_scope).to include(other_software_agent) }
+    end
+    permissions :show?, :create? do
+      it { is_expected.to permit(user, software_agent) }
+      it { is_expected.to permit(user, other_software_agent) }
+    end
+    permissions :update?, :destroy? do
+      it { is_expected.not_to permit(user, software_agent) }
+      it { is_expected.not_to permit(user, other_software_agent) }
+    end
   end
 
-  permissions :create? do
-    it {is_expected.to permit(user, software_agent)}
-    it {is_expected.to permit(creator, software_agent)}
-    it {is_expected.to permit(system_admin, software_agent)}
-  end
+  context 'when user has system_permission' do
+    let(:user) { FactoryGirl.create(:system_permission).user }
 
-  permissions :update? do
-    it {is_expected.not_to permit(user, software_agent)}
-    it {is_expected.to permit(creator, software_agent)}
-    it {is_expected.to permit(system_admin, software_agent)}
-  end
-
-  permissions :destroy? do
-    it {is_expected.not_to permit(user, software_agent)}
-    it {is_expected.to permit(creator, software_agent)}
-    it {is_expected.to permit(system_admin, software_agent)}
+    describe '.scope' do
+      it { expect(resolved_scope).to include(software_agent) }
+      it { expect(resolved_scope).to include(other_software_agent) }
+    end
+    permissions :show?, :create?, :update?, :destroy? do
+      it { is_expected.to permit(user, software_agent) }
+      it { is_expected.to permit(user, other_software_agent) }
+    end
   end
 end
