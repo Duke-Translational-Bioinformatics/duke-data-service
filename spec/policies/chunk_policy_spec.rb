2b9d2827f125fff6c509a409803a58f327bcade6
Darrin Mann <darrin.mann@duke.edu>
2016-03-01 15:01:17 -0500

    Removing unused let statements

--- spec/policies/chunk_policy_spec.rb
+++ spec/policies/chunk_policy_spec.rb
@@ -4,12 +4,9 @@ describe ChunkPolicy do
   include_context 'policy declarations'
 
   let(:project_permission) { FactoryGirl.create(:project_permission) }
-  let(:user) { project_permission.user }
   let(:upload) { FactoryGirl.create(:upload, project: project_permission.project) }
   let(:chunk) { FactoryGirl.create(:chunk, upload: upload) }
   let(:other_chunk) { FactoryGirl.create(:chunk) }
-
-  let(:scope) { subject.new(user, chunk).scope }
 
   context 'when user has project_permission' do
     let(:user) { project_permission.user }
