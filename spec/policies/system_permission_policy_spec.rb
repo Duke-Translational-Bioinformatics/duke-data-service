f824a8c42ff37f3d2e5fc095ea07887bf3a2f28e
Darrin Mann <darrin.mann@duke.edu>
2016-03-01 16:05:41 -0500

    DRYing up the SystemPermissionPolicy specs

--- spec/policies/system_permission_policy_spec.rb
+++ spec/policies/system_permission_policy_spec.rb
@@ -1,86 +1,38 @@
 require 'rails_helper'
 
 describe SystemPermissionPolicy do
-  let(:permission) { FactoryGirl.create(:system_permission) }
-  let(:user) { permission.user }
-  let(:other_user) { FactoryGirl.create(:user) }
-  let(:system_permission) { FactoryGirl.build(:system_permission) }
+  include_context 'policy declarations'
+
+  let(:system_permission) { FactoryGirl.create(:system_permission) }
   let(:other_system_permission) { FactoryGirl.create(:system_permission) }
   
-  let(:scope) { subject.new(user, system_permission).scope }
-  let(:other_scope) { subject.new(other_user, system_permission).scope }
+  context 'when user has system_permission' do
+    let(:user) { system_permission.user }
 
-  subject { described_class }
-
-  permissions ".scope" do
-    it 'returns system_permissions with system permission' do
-      expect(system_permission.save).to be_truthy
-      expect(permission).to be_persisted
-      expect(other_system_permission).to be_persisted
-      expect(scope.all).to include(permission)
-      expect(scope.all).to include(system_permission)
-      expect(scope.all).to include(other_system_permission)
+    describe '.scope' do
+      it { expect(resolved_scope).to include(system_permission) }
+      it { expect(resolved_scope).to include(other_system_permission) }
     end
-
-    it 'does not return system_permissions without system permission' do
-      expect(system_permission.save).to be_truthy
-      expect(permission).to be_persisted
-      expect(other_system_permission).to be_persisted
-      expect(other_scope.all).not_to include(permission)
-      expect(other_scope.all).not_to include(system_permission)
-      expect(other_scope.all).not_to include(other_system_permission)
+    permissions :show?, :create?, :update? do
+      it { is_expected.to permit(user, system_permission) }
+      it { is_expected.to permit(user, other_system_permission) }
+    end
+    permissions :destroy? do
+      it { is_expected.not_to permit(user, system_permission) }
+      it { is_expected.to permit(user, other_system_permission) }
     end
   end
 
-  permissions :show? do
-    it 'denies access without system permission' do
-      is_expected.not_to permit(other_user, system_permission)
-      is_expected.not_to permit(other_user, other_system_permission)
-    end
+  context 'when user does not have system_permission' do
+    let(:user) { FactoryGirl.create(:user) }
 
-    it 'grants access with system permission' do
-      is_expected.to permit(user, system_permission)
-      is_expected.to permit(user, other_system_permission)
+    describe '.scope' do
+      it { expect(resolved_scope).not_to include(system_permission) }
+      it { expect(resolved_scope).not_to include(other_system_permission) }
     end
-  end
-
-  permissions :create? do
-    it 'denies access without system permission' do
-      is_expected.not_to permit(other_user, system_permission)
-      is_expected.not_to permit(other_user, other_system_permission)
-    end
-
-    it 'grants access with system permission' do
-      is_expected.to permit(user, system_permission)
-      is_expected.to permit(user, other_system_permission)
-    end
-  end
-
-  permissions :update? do
-    it 'denies access without system permission' do
-      is_expected.not_to permit(other_user, system_permission)
-      is_expected.not_to permit(other_user, other_system_permission)
-    end
-
-    it 'grants access with system permission' do
-      is_expected.to permit(user, system_permission)
-      is_expected.to permit(user, other_system_permission)
-    end
-  end
-
-  permissions :destroy? do
-    it 'denies access without system permission' do
-      is_expected.not_to permit(other_user, system_permission)
-      is_expected.not_to permit(other_user, other_system_permission)
-    end
-
-    it 'grants access with system permission' do
-      is_expected.to permit(user, system_permission)
-      is_expected.to permit(user, other_system_permission)
-    end
-
-    it 'denies access for current user permission' do
-      is_expected.not_to permit(user, permission)
+    permissions :show?, :create?, :update?, :destroy? do
+      it { is_expected.not_to permit(user, system_permission) }
+      it { is_expected.not_to permit(user, other_system_permission) }
     end
   end
 end
