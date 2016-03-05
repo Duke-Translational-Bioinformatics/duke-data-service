0b83af7b27910fa757006ad2c502f069ce48f7cc
Darin London <darin.london@duke.edu>
2016-03-05 12:29:41 -0500

    Changed to reflect new serialized audit testing framework

--- app/models/software_agent.rb
+++ app/models/software_agent.rb
@@ -1,10 +1,9 @@
 class SoftwareAgent < ActiveRecord::Base
-  include SerializedAudit
   audited
 
   belongs_to :creator, class_name: "User"
   has_one :api_key
-  
+
   validates :name, presence: true, unless: :is_deleted
   validates :creator_id, presence: true, unless: :is_deleted
 end
--- spec/models/software_agent_spec.rb
+++ spec/models/software_agent_spec.rb
@@ -2,12 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SoftwareAgent, type: :model do
   subject { FactoryGirl.create(:software_agent) }
-  let(:is_logically_deleted) { true }
   let(:deleted_software_agent) { FactoryGirl.create(:software_agent, :deleted) }
 
-  it_behaves_like 'an audited model' do
-    it_behaves_like 'with a serialized audit'
-  end
+  it_behaves_like 'an audited model'
 
   describe 'associations' do
     it { should belong_to(:creator) }
