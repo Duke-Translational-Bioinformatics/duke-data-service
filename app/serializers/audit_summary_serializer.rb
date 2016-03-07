a849e06205d951b41dfb6a5787f1c16933b1a102
Darin London <darin.london@duke.edu>
2016-03-07 16:12:11 -0500

    SerializersSharedExample   added a 'creation' shared context that can be used to add the software_agent to the creation audit when called in the context     of a software_agent   modified 'with update' and 'with destroy' contexts to support being called with an option 'with software_agent' context    which adds the software_agent to the audit comment   changed the let variables update and destroy to updated and destroyed, to be syntactially similar to creation:     expect(creation).to be_truthy     expect(updated).to be_truthy     expect(destroyed).to be_truthy
    added creation context
    creation, update, and destroy contexts test for audits in the context of a browser client and in the context of a software_agent
    
    AuditSummarySerializer now checks the comments on the creation, update, and deletion audits to see if a software_agent_id is present,
      and adds a summary of the agent to the created_by, updated_by, and deleted_by hashes

--- app/serializers/audit_summary_serializer.rb
+++ app/serializers/audit_summary_serializer.rb
@@ -10,22 +10,60 @@ module AuditSummarySerializer
     else
       delete_audit = object.audits.where(action: "destroy").last
     end
-    creator = creation_audit ?
-        User.where(id: creation_audit.user_id).first :
-        nil
-    last_updator = last_update_audit ?
-        User.where(id: last_update_audit.user_id).first :
-        nil
-    deleter = delete_audit ?
-        User.where(id: delete_audit.user_id).first :
-        nil
+    creator = nil
+    creation_agent_info = {}
+
+    if creation_audit
+      creator = User.where(id: creation_audit.user_id).first
+
+      if creation_audit.comment && creation_audit.comment["software_agent_id"]
+        creation_agent = SoftwareAgent.find(creation_audit.comment["software_agent_id"])
+        creation_agent_info = {
+          "agent": {
+            "id": creation_agent.id,
+            "name": creation_agent.name
+          }
+        }
+      end
+    end
+
+    last_updator = nil
+    last_update_agent_info = {}
+    if last_update_audit
+      last_updator = User.where(id: last_update_audit.user_id).first
+      if last_update_audit.comment && last_update_audit.comment["software_agent_id"]
+        last_update_agent = SoftwareAgent.find(last_update_audit.comment["software_agent_id"])
+        last_update_agent_info = {
+          "agent": {
+            "id": last_update_agent.id,
+            "name": last_update_agent.name
+          }
+        }
+      end
+    end
+
+    deleter = nil
+    deletion_agent_info = {}
+    if delete_audit
+      deleter = User.where(id: delete_audit.user_id).first
+      if delete_audit.comment && delete_audit.comment["software_agent_id"]
+        deletion_agent = SoftwareAgent.find(delete_audit.comment["software_agent_id"])
+        deletion_agent_info = {
+          "agent": {
+            "id": deletion_agent.id,
+            "name": deletion_agent.name
+          }
+        }
+      end
+    end
+
     {
       created_on: creation_audit ? creation_audit.created_at : nil,
-      created_by: creator ? creator.audited_user_info : nil,
+      created_by: creator ? creator.audited_user_info.merge(creation_agent_info) : nil,
       last_updated_on: last_update_audit ? last_update_audit.created_at : nil,
-      last_updated_by: last_updator ? last_updator.audited_user_info : nil,
+      last_updated_by: last_updator ? last_updator.audited_user_info.merge(last_update_agent_info) : nil,
       deleted_on: delete_audit ? delete_audit.created_at : nil,
-      deleted_by: deleter ? deleter.audited_user_info : nil
+      deleted_by: deleter ? deleter.audited_user_info.merge(deletion_agent_info) : nil
     }
   end
 end
