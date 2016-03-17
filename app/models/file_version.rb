ce539ea07f757f9ce69eb651657ea921979c4fb0
Darrin Mann <darrin.mann@duke.edu>
2016-03-17 10:20:25 -0400

    Removing upload validations on file versions

--- app/models/file_version.rb
+++ app/models/file_version.rb
@@ -9,18 +9,6 @@ class FileVersion < ActiveRecord::Base
   validates :upload_id, presence: true, unless: :is_deleted
   validates :creator_id, presence: true, unless: :is_deleted
 
-  validates_each :upload, :upload_id, unless: :is_deleted do |record, attr, value|
-    if record.upload
-      if record.upload.creator_id != record.creator_id
-        record.errors.add(attr, 'created by another user')
-      elsif record.upload.error_at
-        record.errors.add(attr, 'cannot have an error')
-      elsif !record.upload.completed_at
-        record.errors.add(attr, 'must be completed successfully')
-      end
-    end
-  end
-
   delegate :name, to: :data_file
   delegate :http_verb, to: :upload
 
--- spec/models/file_version_spec.rb
+++ spec/models/file_version_spec.rb
@@ -18,49 +18,8 @@ RSpec.describe FileVersion, type: :model do
   end
 
   describe 'validations' do
-    let(:completed_upload) { FactoryGirl.create(:upload, :completed, creator: subject.creator) }
-    let(:incomplete_upload) { FactoryGirl.create(:upload, creator: subject.creator) }
-    let(:upload_with_error) { FactoryGirl.create(:upload, :with_error, creator: subject.creator) }
-    let(:not_creator_of_upload) { FactoryGirl.create(:upload, :completed) }
-
-    it 'should have a upload_id' do
-      should validate_presence_of(:upload_id)
-    end
-
-    it 'should require upload has no error' do
-      should allow_value(completed_upload.id).for(:upload_id)
-      should_not allow_value(upload_with_error.id).for(:upload_id)
-      should allow_value(completed_upload).for(:upload)
-      should_not allow_value(upload_with_error).for(:upload)
-      should_not allow_value(upload_with_error).for(:upload)
-      expect(subject.valid?).to be_falsey
-      expect(subject.errors.keys).to include(:upload)
-      expect(subject.errors[:upload]).to include('cannot have an error')
-    end
-
-    it 'should require a completed upload' do
-      should allow_value(completed_upload.id).for(:upload_id)
-      should_not allow_value(incomplete_upload.id).for(:upload_id)
-      should allow_value(completed_upload).for(:upload)
-      should_not allow_value(incomplete_upload).for(:upload)
-      expect(subject.valid?).to be_falsey
-      expect(subject.errors.keys).to include(:upload)
-      expect(subject.errors[:upload]).to include('must be completed successfully')
-    end
-
-    it 'should require creator equal upload.creator' do
-      should allow_value(completed_upload.id).for(:upload_id)
-      should_not allow_value(not_creator_of_upload.id).for(:upload_id)
-      should allow_value(completed_upload).for(:upload)
-      should_not allow_value(not_creator_of_upload).for(:upload)
-      expect(subject.valid?).to be_falsey
-      expect(subject.errors.keys).to include(:upload)
-      expect(subject.errors[:upload]).to include('created by another user')
-    end
-
-    it 'should require a creator_id' do
-      should validate_presence_of :creator_id
-    end
+    it { is_expected.to validate_presence_of :upload_id }
+    it { is_expected.to validate_presence_of :creator_id }
 
     it 'should allow is_deleted to be set' do
       should allow_value(true).for(:is_deleted)
