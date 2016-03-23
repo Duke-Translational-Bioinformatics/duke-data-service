77ac383133f3e3b7f20d9120ffe05d7163293e1b
Darrin Mann <darrin.mann@duke.edu>
2016-03-23 11:00:23 -0400

    Allowing granular permission selection in project_permissions factory

--- spec/factories/auth_roles.rb
+++ spec/factories/auth_roles.rb
@@ -16,7 +16,7 @@ FactoryGirl.define do
       name "Project Admin"
       description "Can update project details, delete project, manage project level permissions and perform all file operations"
       contexts %w(project)
-      permissions %w(view_project update_project delete_project manage_project_permissions download_file create_file update_file delete_file)
+      permissions { AuthRole.available_permissions(:project) }
     end
 
     trait :deprecated do
--- spec/factories/project_permissions.rb
+++ spec/factories/project_permissions.rb
@@ -1,8 +1,21 @@
 FactoryGirl.define do
   factory :project_permission do
+    transient do
+      with_permissions false
+      without_permissions false
+    end
+
     project
     user
-    auth_role
+    auth_role {
+      if with_permissions
+        create(:auth_role, permissions: with_permissions)
+      elsif without_permissions
+        create(:auth_role, permissions: AuthRole.available_permissions - without_permissions)
+      else
+        create(:auth_role)
+      end
+    }
 
     trait :deleted do
       association :project, factory: [:project, :deleted]
