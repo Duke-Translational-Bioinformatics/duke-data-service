246dfcd73969b2e7f431793a7461df0b5e213329
Darrin Mann <darrin.mann@duke.edu>
2016-03-24 18:06:02 -0400

    AffiliationPolicy authorizes via auth role permissions

--- spec/policies/affiliation_policy_spec.rb
+++ spec/policies/affiliation_policy_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe AffiliationPolicy do
   include_context 'policy declarations'
 
-  let(:project_permission) { FactoryGirl.create(:project_permission) }
+  let(:auth_role) { FactoryGirl.create(:auth_role) }
+  let(:project_permission) { FactoryGirl.create(:project_permission, auth_role: auth_role) }
   let(:affiliation) { FactoryGirl.create(:affiliation, project: project_permission.project) }
   let(:other_affiliation) { FactoryGirl.create(:affiliation) }
 
@@ -13,13 +14,48 @@ describe AffiliationPolicy do
   context 'when user has project_permission' do
     let(:user) { project_permission.user }
 
-    describe '.scope' do
-      it { expect(resolved_scope).to include(affiliation) }
-      it { expect(resolved_scope).not_to include(other_affiliation) }
+    context 'without view_project, update_project' do
+      let(:auth_role) { FactoryGirl.create(:auth_role, without_permissions: %w(view_project update_project)) }
+      describe '.scope' do
+        it { expect(resolved_scope).not_to include(affiliation) }
+        it { expect(resolved_scope).not_to include(other_affiliation) }
+      end
+      permissions :show?, :create?, :update?, :destroy? do
+        it { is_expected.not_to permit(user, affiliation) }
+        it { is_expected.not_to permit(user, other_affiliation) }
+      end
     end
-    permissions :show?, :create?, :update?, :destroy? do
-      it { is_expected.to permit(user, affiliation) }
-      it { is_expected.not_to permit(user, other_affiliation) }
+
+    context 'with view_project permission' do
+      let(:auth_role) { FactoryGirl.create(:auth_role, permissions: %w(view_project)) }
+      describe '.scope' do
+        it { expect(resolved_scope).to include(affiliation) }
+        it { expect(resolved_scope).not_to include(other_affiliation) }
+      end
+      permissions :show? do
+        it { is_expected.to permit(user, affiliation) }
+        it { is_expected.not_to permit(user, other_affiliation) }
+      end
+      permissions :create?, :update?, :destroy? do
+        it { is_expected.not_to permit(user, affiliation) }
+        it { is_expected.not_to permit(user, other_affiliation) }
+      end
+    end
+
+    context 'with update_project permission' do
+      let(:auth_role) { FactoryGirl.create(:auth_role, permissions: %w(update_project)) }
+      describe '.scope' do
+        it { expect(resolved_scope).not_to include(affiliation) }
+        it { expect(resolved_scope).not_to include(other_affiliation) }
+      end
+      permissions :show? do
+        it { is_expected.not_to permit(user, affiliation) }
+        it { is_expected.not_to permit(user, other_affiliation) }
+      end
+      permissions :create?, :update?, :destroy? do
+        it { is_expected.to permit(user, affiliation) }
+        it { is_expected.not_to permit(user, other_affiliation) }
+      end
     end
   end
 
