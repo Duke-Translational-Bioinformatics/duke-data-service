7fb8093123582ef7f06efc0ceb3f1dc8d4fa7d51
Darrin Mann <darrin.mann@duke.edu>
2016-03-24 17:46:34 -0400

    Adding project_admin trait to project_permissions factory

--- spec/factories/project_permissions.rb
+++ spec/factories/project_permissions.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     user
     auth_role 
 
+    trait :project_admin do
+      association :auth_role, factory: [:auth_role, :project_admin]
+    end
+
     trait :deleted_project do
       association :project, factory: [:project, :deleted]
     end
