93491144620228fb11fda1a8cbadab273e9f7a90
Darrin Mann <darrin.mann@duke.edu>
2016-03-25 15:11:54 -0400

    ChunkPolicy requires create_file auth_role permission

--- spec/policies/chunk_policy_spec.rb
+++ spec/policies/chunk_policy_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe ChunkPolicy do
   include_context 'policy declarations'
 
-  let(:project_permission) { FactoryGirl.create(:project_permission) }
+  let(:auth_role) { FactoryGirl.create(:auth_role) }
+  let(:project_permission) { FactoryGirl.create(:project_permission, auth_role: auth_role) }
   let(:upload) { FactoryGirl.create(:upload, project: project_permission.project) }
   let(:chunk) { FactoryGirl.create(:chunk, upload: upload) }
   let(:other_chunk) { FactoryGirl.create(:chunk) }
@@ -14,13 +15,28 @@ describe ChunkPolicy do
   context 'when user has project_permission' do
     let(:user) { project_permission.user }
 
-    describe '.scope' do
-      it { expect(resolved_scope).to include(chunk) }
-      it { expect(resolved_scope).not_to include(other_chunk) }
+    context 'without create_file' do
+      let(:auth_role) { FactoryGirl.create(:auth_role, without_permissions: %w(create_file)) }
+      describe '.scope' do
+        it { expect(resolved_scope).not_to include(chunk) }
+        it { expect(resolved_scope).not_to include(other_chunk) }
+      end
+      permissions :show?, :create?, :update?, :destroy? do
+        it { is_expected.not_to permit(user, chunk) }
+        it { is_expected.not_to permit(user, other_chunk) }
+      end
     end
-    permissions :show?, :create?, :update?, :destroy? do
-      it { is_expected.to permit(user, chunk) }
-      it { is_expected.not_to permit(user, other_chunk) }
+
+    context 'with create_file' do
+      let(:auth_role) { FactoryGirl.create(:auth_role, permissions: %w(create_file)) }
+      describe '.scope' do
+        it { expect(resolved_scope).to include(chunk) }
+        it { expect(resolved_scope).not_to include(other_chunk) }
+      end
+      permissions :show?, :create?, :update?, :destroy? do
+        it { is_expected.to permit(user, chunk) }
+        it { is_expected.not_to permit(user, other_chunk) }
+      end
     end
   end
 
