e5dc948c7e4ea4795038c2a60a5837467d0c9721
Darrin Mann <darrin.mann@duke.edu>
2016-03-31 14:52:34 -0400

    UploadPolicy checks auth_role permissions for authorization.

--- spec/policies/upload_policy_spec.rb
+++ spec/policies/upload_policy_spec.rb
@@ -11,18 +11,15 @@ describe UploadPolicy do
   it_behaves_like 'system_permission can access', :upload
   it_behaves_like 'system_permission can access', :other_upload
 
-  context 'when user has project_permission' do
-    let(:user) { project_permission.user }
+  it_behaves_like 'a user with project_permission', :create_file, allows: [:create?, :update?, :complete?], on: :upload
+  it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :show?], denies: [:complete?], on: :upload
 
-    describe '.scope' do
-      it { expect(resolved_scope).to include(upload) }
-      it { expect(resolved_scope).not_to include(other_upload) }
-    end
-    permissions :show?, :create?, :update?, :destroy? do
-      it { is_expected.to permit(user, upload) }
-      it { is_expected.not_to permit(user, other_upload) }
-    end
-  end
+  it_behaves_like 'a user with project_permission', :create_file, allows: [], denies: [:complete?], on: :other_upload
+  it_behaves_like 'a user with project_permission', :view_project, allows: [], denies: [:complete?], on: :other_upload
+
+  it_behaves_like 'a user without project_permission', [:create_file, :view_project], denies: [:scope, :show?, :create?, :update?, :destroy?, :complete?], on: :upload
+  it_behaves_like 'a user without project_permission', [:create_file, :view_project], denies: [:scope, :show?, :create?, :update?, :destroy?, :complete?], on: :other_upload
+
 
   context 'when user does not have project_permission' do
     let(:user) { FactoryGirl.create(:user) }
