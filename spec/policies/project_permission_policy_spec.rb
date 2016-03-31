8f8e5e69746c8eeacd0bc01b6c11a79e7d4b66cb
Darrin Mann <darrin.mann@duke.edu>
2016-03-31 13:50:42 -0400

    ProjectPermissionPolicy checks auth_role permissions for authorization.

--- spec/policies/project_permission_policy_spec.rb
+++ spec/policies/project_permission_policy_spec.rb
@@ -11,18 +11,17 @@ describe ProjectPermissionPolicy do
   it_behaves_like 'system_permission can access', :other_users_project_permission
   it_behaves_like 'system_permission can access', :other_project_permission
 
-  context 'when user has project_permission' do
-    let(:user) { project_permission.user }
+  it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :show?], on: :project_permission
+  it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?], on: :project_permission
 
-    describe '.scope' do
-      it { expect(resolved_scope).to include(other_users_project_permission) }
-      it { expect(resolved_scope).not_to include(other_project_permission) }
-    end
-    permissions :show?, :create?, :update?, :destroy? do
-      it { is_expected.to permit(user, other_users_project_permission) }
-      it { is_expected.not_to permit(user, other_project_permission) }
-    end
-  end
+  it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :show?], on: :other_users_project_permission
+  it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?, :update?, :destroy?], on: :other_users_project_permission
+
+  it_behaves_like 'a user with project_permission', :view_project, allows: [], on: :other_project_permission
+  it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [], on: :other_project_permission
+
+  it_behaves_like 'a user without project_permission', [:view_project, :manage_project_permissions], denies: [:scope, :show?, :create?, :update?, :destroy?], on: :other_users_project_permission
+  it_behaves_like 'a user without project_permission', [:view_project, :manage_project_permissions], denies: [:scope, :show?, :create?, :update?, :destroy?], on: :other_project_permission
 
   context 'when user does not have project_permission' do
     let(:user) { FactoryGirl.create(:user) }
