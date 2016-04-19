7dde29b456c4c2096d68d37a3e998cb31b2919fb
Jonathan <jsp36@duke.edu>
2016-04-19 16:19:15 -0400

    Change tag to not use polymorphism for now

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,9 +1,9 @@
 class Tag < ActiveRecord::Base
-  belongs_to :taggable, polymorphic: true
+  belongs_to :taggable, class_name: 'DataFile'
+  has_many :project_permissions, through: :taggable
+
 
   validates :label, presence: true
   validates :taggable, presence: true
-
-
 
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe Tag, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:taggable) }
+    it { is_expected.to have_many(:project_permissions).through(:taggable) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:label) }
     it { is_expected.to validate_presence_of(:taggable) }
   end
+
 end
