68fffcc9d2955eb2e7b312f940725b937d6bff40
Darin London <darin.london@duke.edu>
2016-04-19 15:04:32 -0400

    SoftwareAgentSpec expects SoftwareAgent to behave like a graphed model with auto_create: true SoftwareAgent meets expectations
    DDS-362 #resolve

--- app/models/software_agent.rb
+++ app/models/software_agent.rb
@@ -1,9 +1,16 @@
 class SoftwareAgent < ActiveRecord::Base
   audited
+  include Kinded
+  include Graphed
+  after_create :graph_node
 
   belongs_to :creator, class_name: "User"
   has_one :api_key
 
   validates :name, presence: true, unless: :is_deleted
   validates :creator_id, presence: true, unless: :is_deleted
+
+  def graph_node
+    super('Agent')
+  end
 end
