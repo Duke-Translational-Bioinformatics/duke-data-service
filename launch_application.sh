5cdfc72a14786bf442dc6e1580b5e1e7506d8a66
Darin London <darin.london@duke.edu>
2016-04-21 11:48:12 -0400

    authservice needed link and environment for neo4j
    launch_application.sh and circle/launch_application.sh launches neo4j and sleeps before launching everything else to give neo4j server a chance to start accepting connections

--- launch_application.sh
+++ launch_application.sh
@@ -2,6 +2,8 @@
 
 if [ -z $COMPOSE_FILE ]
 then
+  docker-compose up -d neo4j
+  sleep 5
   docker-compose up -d server
   docker-compose -f dc-dev.utils.yml run rake db:migrate
   docker-compose -f dc-dev.utils.yml run rake db:seed
@@ -12,6 +14,8 @@ then
   fi
   docker-compose -f dc-dev.utils.yml run rake storage_provider:create
 else
+  docker-compose up -d neo4j
+  sleep 5
   docker-compose up -d server
   docker-compose run rake db:migrate
   docker-compose run rake db:seed
