02f40dec9f7cbd3a992609dd9ce38883bc55944b
Darin London <darin.london@duke.edu>
2016-04-26 14:33:21 -0400

    created seperate user and software_agent associated activity contexts

--- spec/models/agent_activity_association_spec.rb
+++ spec/models/agent_activity_association_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe AgentActivityAssociation, type: :model do
-  subject { associated_with_relation }
-  let(:associated_with_relation) { FactoryGirl.create(:user_activity_association) }
+  context 'user activity association' do
+    subject { associated_with_relation }
+    let(:associated_with_relation) { FactoryGirl.create(:user_activity_association) }
 
-  it_behaves_like 'an audited model'
-  it_behaves_like 'a graphed relation', auto_create: true do
-    let(:from_model) { associated_with_relation.agent }
-    let(:to_model) { associated_with_relation.activity }
-    let(:rel_type) { 'WasAssociatedWith' }
+    it_behaves_like 'an audited model'
+    it_behaves_like 'a graphed relation', auto_create: true do
+      let(:from_model) { associated_with_relation.agent }
+      let(:to_model) { associated_with_relation.activity }
+      let(:rel_type) { 'WasAssociatedWith' }
+    end
+    describe 'validations' do
+      it { is_expected.to validate_presence_of :agent }
+      it { is_expected.to validate_presence_of :activity }
+    end
   end
-  describe 'validations' do
-    it { is_expected.to validate_presence_of :agent }
-    it { is_expected.to validate_presence_of :activity }
+
+  context 'software_agent activity association' do
+    subject { associated_with_relation }
+    let(:associated_with_relation) { FactoryGirl.create(:software_agent_activity_association) }
+
+    it_behaves_like 'an audited model'
+    it_behaves_like 'a graphed relation', auto_create: true do
+      let(:from_model) { associated_with_relation.agent }
+      let(:to_model) { associated_with_relation.activity }
+      let(:rel_type) { 'WasAssociatedWith' }
+    end
   end
 end
