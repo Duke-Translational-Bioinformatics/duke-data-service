51c58d563f55b5adab89df857bacba1270c5b3d4
Darin London <darin.london@duke.edu>
2016-05-03 14:16:37 -0400

    refactored to seperate create and get requests for graph_node and graph_relation auto_create graphed models and relationships call their create method explicitly and the graph_node and graph_relation simply returns the response of a match nil or not.
    shaves a few minutes off the top of the time

--- app/models/file_version.rb
+++ app/models/file_version.rb
@@ -1,7 +1,7 @@
 class FileVersion < ActiveRecord::Base
   include Kinded
   include Graphed
-  after_create :graph_node
+  after_create :create_graph_node
   after_save :logically_delete_graph_node
   after_destroy :delete_graph_node
 
