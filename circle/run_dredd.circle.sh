8ab2d49af5ae5d7547ec0a0d80d2f6459e76be97
Darin London <darin.london@duke.edu>
2016-05-04 09:29:31 -0400

    push image caching into run_dredd

--- circle/run_dredd.circle.sh
+++ circle/run_dredd.circle.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# cache necessary images
+./docker/circle/cache_docker_image.sh ruby 2.2.2
+./docker/circle/cache_docker_image.sh postgres 9.4
+./docker/circle/cache_docker_image.sh ubuntu 14.04
+./docker/circle/cache_dredd.sh
+./docker/circle/cache_docker_image.sh morrisjobke/docker-swift-onlyone latest
+
+# launch_application
 docker-compose up -d server
 sleep 10
 docker-compose ps
@@ -11,4 +19,6 @@ docker-compose run rake storage_provider:create
 docker-compose run rake api_test_user:create
 docker-compose run rake api_test_user_pool:create
 echo "MY_GENERATED_JWT="`docker-compose run rake api_test_user:create | tail -1` >> dredd.env
+
+# run dredd
 docker-compose run dredd
