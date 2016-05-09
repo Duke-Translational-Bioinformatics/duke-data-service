7ec5d2a427578bc1b1166f85f1936e2588242840
Darin London <darin.london@duke.edu>
2016-05-09 17:00:27 -0400

    UsedProvRelations cannot be updated or indexed
    user can only create, show, and destroy UsedProvRelaton if
      - they are a system user
      OR
      - they have created the activity AND they have visibility to the file_version

---
+++ spec/policies/used_prov_relation_policy_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe UsedProvRelationPolicy do
+  include_context 'policy declarations'
+
+  let(:auth_role) { FactoryGirl.create(:auth_role) }
+  let(:project_permission) { FactoryGirl.create(:project_permission, auth_role: auth_role) }
+  let(:user) { project_permission.user }
+  let(:data_file) { FactoryGirl.create(:data_file, project: project_permission.project) }
+  let(:file_version) { FactoryGirl.create(:file_version, data_file: data_file) }
+  let(:activity) { FactoryGirl.create(:activity, creator: user) }
+  let(:used_prov_relation) { FactoryGirl.create(:used_prov_relation, relatable_from: activity, relatable_to: file_version) }
+  let(:other_used_prov_relation) { FactoryGirl.create(:used_prov_relation) }
+  let(:without_relatable_to_prov_relation) { FactoryGirl.create(:used_prov_relation, relatable_from: activity) }
+  let(:without_relatable_from_prov_relation) { FactoryGirl.create(:used_prov_relation, relatable_to: file_version) }
+
+  it_behaves_like 'system_permission can access', :used_prov_relation, allows: [:scope, :show?, :create?, :destroy?], denies: [:index?, :update?]
+  it_behaves_like 'system_permission can access', :other_used_prov_relation, allows: [:scope, :show?, :create?, :destroy?], denies: [:index?, :update?]
+  it_behaves_like 'system_permission can access', :without_relatable_to_prov_relation, allows: [:scope, :show?, :create?, :destroy?], denies: [:index?, :update?]
+  it_behaves_like 'system_permission can access', :without_relatable_from_prov_relation, allows: [:scope, :show?, :create?, :destroy?], denies: [:index?, :update?]
+
+  context 'user created activity and has visibility to the file_version' do
+    it_behaves_like 'a user with project_permission', :view_project, allows: [:show?, :create?, :destroy?], on: :used_prov_relation
+    it_behaves_like 'a user without project_permission', :view_project, denies: [:show?, :create?, :destroy?], on: :used_prov_relation
+  end
+
+  context 'user did not create activity and does not have visibility to the file_version' do
+    it_behaves_like 'a user with project_permission', :view_project, allows: [], on: :other_used_prov_relation
+    it_behaves_like 'a user without project_permission', :view_project, denies: [:show?, :create?, :destroy?], on: :other_used_prov_relation
+  end
+
+  context 'user created the activity but does not have visbility to the file_version' do
+    it_behaves_like 'a user with project_permission', :view_project, allows: [], on: :without_relatable_to_prov_relation
+    it_behaves_like 'a user without project_permission', :view_project, denies: [:show?, :create?, :destroy?], on: :without_relatable_to_prov_relation
+    end
+
+  context 'user did not create the activity but has visibility to the file_version' do
+    it_behaves_like 'a user with project_permission', :view_project, allows: [], on: :without_relatable_from_prov_relation
+    it_behaves_like 'a user without project_permission', :view_project, denies: [:show?, :create?, :destroy?], on: :without_relatable_from_prov_relation
+  end
+end
