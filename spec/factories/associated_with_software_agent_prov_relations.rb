49df32ce502642cb775f106913011a4dac79eda3
Darin London <darin.london@duke.edu>
2016-05-11 12:04:16 -0400

    created seperate ProvRelation implementations for AssociatedWithUser and AssociatedWithSoftwareAgent each with their own Serializer
    This will allow them to be serialized automatically in list/view responses based on their model-serializer link

---
+++ spec/factories/associated_with_software_agent_prov_relations.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :associated_with_software_agent_prov_relation do
+    association :creator, factory: :user
+    is_deleted false
+    relationship_type { 'was-associated-with' }
+    association :relatable_from, factory: :software_agent
+    association :relatable_to, factory: :activity
+  end
+end
