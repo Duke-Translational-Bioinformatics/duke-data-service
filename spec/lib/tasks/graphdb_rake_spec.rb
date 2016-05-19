4d1da856e9db8ee32a9ec4462af73717a1f324c5
Darin London <darin.london@duke.edu>
2016-05-19 14:07:13 -0400

    graphdb tasks and spec tests   graphdb:build     expected to create graph_nodes for User, SoftwareAgent, Activity, and FileVersion     expected to create AssociatedWithUserProvRelations from all Activities to the creation_audit.user     expected to create AssociatedWithSoftwareAgent from all Activities to the creation_audit.comment["software_agent_id"] SoftwareAgent if present     expected to create AttributedToUserProvRelations from creation_audit.user to all FileVersions     expected to create AttributedToSoftwareAgent from creation_audit.comment["software_agent_id"] SoftwareAgent if present to all FileVersions     expected to create graph_relations for all ProvRelations THIS WILL NOT RUN ON CIRCLECI

--- spec/lib/tasks/graphdb_rake_spec.rb
+++ spec/lib/tasks/graphdb_rake_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-describe "graphdb" do
-  include_context "rake"
-  let(:task_path) { "lib/tasks/graphdb" }
+describe "graphdb", :unless => ENV['CIRCLECI'] do
   let(:current_user) { FactoryGirl.create(:user) }
-
-  it { expect(subject.prerequisites).to  include("environment") }
-
   def annotate_audit(audit, audited_software_agent=nil)
     comment_annotation = {
-      endpoint: "/graphdb"
+      endpoint: "/graphdb",
       action: 'build'
     }
     if audited_software_agent
@@ -18,20 +13,34 @@ describe "graphdb" do
 
     audit_update = {
       request_uuid: SecureRandom.hex,
-      remote_address: '10.10.10.10'
+      remote_address: '10.10.10.10',
+      comment: audit.comment ?
+        audit.comment.merge(comment_annotation) :
+        comment_annotation
     }
-    audit_update[:comment] = audit.comment ?
-      audit.comment.merge(comment_annotation) :
-      comment_annotation
     audit.update(audit_update)
   end
 
-  before do
-    FactoryGirl.create_list(:user, 2).each do |user|
+  before(:all) do
+    Neo4j::Session.query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+    AssociatedWithUserProvRelation.destroy_all
+    AssociatedWithSoftwareAgentProvRelation.destroy_all
+    AttributedToUserProvRelation.destroy_all
+    AttributedToSoftwareAgentProvRelation.destroy_all
+    User.destroy_all
+    SoftwareAgent.destroy_all
+    Activity.destroy_all
+    FileVersion.destroy_all
+    Upload.destroy_all
+    DataFile.destroy_all
+    Audited.audit_class.destroy_all
+
+    #these simulate objects with ProvRelations that need nodes and relationsips
+    FactoryGirl.create_list(:user, 1).each do |user|
       Audited.audit_class.as_user(user) do
-        sa FactoryGirl.create(:software_agent, creator: user)
+        sa = FactoryGirl.create(:software_agent, creator: user)
         annotate_audit(sa.audits.last)
-        user_upload = FactoryGirl.create(:upload, creator: user)
+        user_upload = FactoryGirl.create(:upload, :completed, creator: user)
         annotate_audit(user_upload.audits.last)
 
         user_df = FactoryGirl.create(:data_file, upload: user_upload)
@@ -39,11 +48,9 @@ describe "graphdb" do
 
         user_fv = FactoryGirl.create(:file_version, data_file: user_df)
         annotate_audit(user_fv.audits.last)
-
         a2u = AttributedToUserProvRelation.create(
           creator: user,
           relatable_from: user_fv,
-          relationship_type: 'was-attributed-to',
           relatable_to: user)
         annotate_audit(a2u.audits.last)
 
@@ -53,11 +60,10 @@ describe "graphdb" do
         aWu = AssociatedWithUserProvRelation.create(
           creator: user,
           relatable_from: user,
-          relationship_type: 'was-associated-with',
           relatable_to: user_activity)
         annotate_audit(aWu.audits.last)
 
-        sa_upload = FactoryGirl.create(:upload, creator: user)
+        sa_upload = FactoryGirl.create(:upload, :completed, creator: user)
         annotate_audit(sa_upload.audits.last, sa)
 
         sa_df = FactoryGirl.create(:data_file, upload: sa_upload)
@@ -69,14 +75,12 @@ describe "graphdb" do
         a2u2 = AttributedToUserProvRelation.create(
           creator: user,
           relatable_from: sa_fv,
-          relationship_type: 'was-attributed-to',
           relatable_to: user)
         annotate_audit(a2u2.audits.last, sa)
 
-        a2sa= AttributedToSoftwareAgentProvRelation.create(
+        a2sa = AttributedToSoftwareAgentProvRelation.create(
           creator: user,
           relatable_from: sa_fv,
-          relationship_type: 'was-attributed-to',
           relatable_to: sa)
         annotate_audit(a2sa.audits.last, sa)
 
@@ -85,41 +89,120 @@ describe "graphdb" do
         aWu2 = AssociatedWithUserProvRelation.create(
           creator: user,
           relatable_from: user,
-          relationship_type: 'was-associated-with',
           relatable_to: sa_activity)
         annotate_audit(aWu2.audits.last, sa)
         aWsa = AssociatedWithSoftwareAgentProvRelation.create(
           creator: user,
           relatable_from: sa,
-          relationship_type: 'was-associated-with',
           relatable_to: sa_activity)
         annotate_audit(aWsa.audits.last, sa)
       end
     end
-  end
 
-  describe ":build" do
-    let(:task_name) { 'graphdb:build' }
-    let(:invoke_task) { silence_stream(STDOUT) { subject.invoke } }
+    #these simulate objects that need ProvRelations as well as nodes
+    # and relationships
+    FactoryGirl.create_list(:user, 1).each do |user|
+      Audited.audit_class.as_user(user) do
+        sa = FactoryGirl.create(:software_agent, creator: user)
+        annotate_audit(sa.audits.last)
+        user_upload = FactoryGirl.create(:upload, :completed, creator: user)
+        annotate_audit(user_upload.audits.last)
 
-    it 'should build all graph_nodes and relationships' do
-      Neo4j::Session.query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
-      invoke_task
-      #there should be nodes and relationships
+        user_df = FactoryGirl.create(:data_file, upload: user_upload)
+        annotate_audit(user_df.audits.last)
+
+        user_fv = FactoryGirl.create(:file_version, data_file: user_df)
+        annotate_audit(user_fv.audits.last)
+
+        user_activity = FactoryGirl.create(:activity, creator: user)
+        annotate_audit(user_activity.audits.last)
+
+        sa_upload = FactoryGirl.create(:upload, :completed, creator: user)
+        annotate_audit(sa_upload.audits.last, sa)
+
+        sa_df = FactoryGirl.create(:data_file, upload: sa_upload)
+        annotate_audit(sa_df.audits.last, sa)
+
+        sa_fv = FactoryGirl.create(:file_version, data_file: sa_df)
+        annotate_audit(sa_fv.audits.last, sa)
+
+        sa_activity = FactoryGirl.create(:activity, creator: user)
+        annotate_audit(sa_activity.audits.last, sa)
+      end
     end
   end
 
-  describe ':clean' do
-    let(:task_name) { 'graphdb:clean' }
+  describe "graphdb:build" do
+    include_context "rake"
+    let(:task_name) { "graphdb:build" }
     let(:invoke_task) { silence_stream(STDOUT) { subject.invoke } }
+    it { expect(subject.prerequisites).to  include("environment") }
+
+    before do
+      Neo4j::Session.query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+    end
+
+    it 'should build all graph_nodes, prov_relations, and graph_relations' do
+      invoke_task
+      User.all.each do |user|
+        expect(user.graph_node).to be
+      end
+
+      SoftwareAgent.all.each do |sa|
+        expect(sa.graph_node).to be
+      end
+
+      Activity.all.each do |activity|
+        expect(activity.graph_node).to be
+        creation_audit = activity.audits.where(action: 'create').take
+        associated_with_user = AssociatedWithUserProvRelation.where(
+          relatable_from: creation_audit.user, relatable_to: activity
+        ).take
+        expect(associated_with_user).to be
+        expect(associated_with_user.graph_relation).to be
+        if creation_audit.comment && creation_audit.comment.has_key?( "software_agent_id" )
+          sa = SoftwareAgent.find(creation_audit.comment["software_agent_id"])
+          associated_with_software_agent = AssociatedWithSoftwareAgentProvRelation.where(
+            relatable_from: sa, relatable_to: activity
+          ).take
+          expect(associated_with_software_agent).to be
+          expect(associated_with_software_agent.graph_relation).to be
+        end
+      end
+
+      FileVersion.all.each do |file_version|
+        expect(file_version.graph_node).to be
+        creation_audit = file_version.audits.where(action: 'create').take
+        attributed_to_user = AttributedToUserProvRelation.where(
+          relatable_to: creation_audit.user, relatable_from: file_version
+        ).take
+        expect(attributed_to_user).to be
+        expect(attributed_to_user.graph_relation).to be
+        if creation_audit.comment && creation_audit.comment.has_key?( "software_agent_id" )
+          sa = SoftwareAgent.find(creation_audit.comment["software_agent_id"])
+          attributed_to_software_agent = AttributedToSoftwareAgentProvRelation.where(
+            relatable_to: sa, relatable_from: file_version
+          ).take
+          expect(attributed_to_software_agent).to be
+          expect(attributed_to_software_agent.graph_relation).to be
+        end
+      end
+    end
+  end
+
+  describe 'graphdb:clean' do
+    include_context "rake"
+    let(:task_name) { "graphdb:clean" }
+    let(:invoke_task) { silence_stream(STDOUT) { subject.invoke } }
+    it { expect(subject.prerequisites).to  include("environment") }
 
     it 'should remove all graph nodes and relationships' do
       invoke_task
       expect(
-        Neo4j::Session.query('MATCH (n) RETURN n').pluck(:n).count
+        Neo4j::Session.query.match('n').pluck(:n).count
       ).to eq(0)
       expect(
-        Neo4j::Session.query('MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN r').pluck(:r).count
+        Neo4j::Session.query('MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN r').count
       ).to eq(0)
     end
   end
