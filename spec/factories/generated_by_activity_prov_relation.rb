8948ac961dbbce96517919e6c6bce4885166f62d
Darin London <darin.london@duke.edu>
2016-05-24 09:39:45 -0400

    GeneratedByActivityProvRelation   Spec   SerializerSpec   Serializer   PolicySpec   Policy   autogenerates Graph::WasGeneratedBy
    DDS-394 #resolve

---
+++ spec/factories/generated_by_activity_prov_relation.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :generated_by_activity_prov_relation do
+    association :creator, factory: :user
+    is_deleted false
+    association :relatable_from, factory: :file_version
+    association :relatable_to, factory: :activity
+  end
+end
