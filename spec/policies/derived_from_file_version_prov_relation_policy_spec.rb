9e80aef8f5d3f03e3066ed304469f749583a2e60
Darin London <darin.london@duke.edu>
2016-05-24 21:07:19 -0400

    DerivedFromFileVersionProvRelation   - model   - Graph::WasDerivedFrom   - serializer   - policy autocreates graph relationship
    DDS-416 #resolve

---
+++ spec/policies/derived_from_file_version_prov_relation_policy_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe DerivedFromFileVersionProvRelationPolicy do
+  include_context 'policy declarations'
+
+  let(:auth_role) { FactoryGirl.create(:auth_role, permissions: [:view_project].flatten) }
+  let(:project_permission) { FactoryGirl.create(:project_permission, auth_role: auth_role) }
+  let(:user) { project_permission.user }
+
+  let(:data_file) { FactoryGirl.create(:data_file, project: project_permission.project) }
+  let(:visible_from_file_version) { FactoryGirl.create(:file_version, data_file: data_file) }
+  let(:visible_to_file_version) { FactoryGirl.create(:file_version, data_file: data_file) }
+
+  let(:invisible_from_file_version) { FactoryGirl.create(:file_version) }
+  let(:invisible_to_file_version) { FactoryGirl.create(:file_version, data_file: invisible_from_file_version.data_file) }
+
+  context 'destroy' do
+    let(:prov_relation) { FactoryGirl.create(:derived_from_file_version_prov_relation,
+      relatable_to: visible_to_file_version,
+      creator: user,
+      relatable_from: visible_from_file_version)
+    }
+    let(:other_prov_relation) {
+      FactoryGirl.create(:derived_from_file_version_prov_relation)
+    }
+    it_behaves_like 'a policy for', :user, on: :prov_relation, allows: [:show?, :create?, :destroy?]
+    it_behaves_like 'a policy for', :user, on: :other_prov_relation, allows: []
+  end
+
+  context 'from file_version visible to user' do
+    context 'to file_version visible to user' do
+      let(:prov_relation) { FactoryGirl.create(:derived_from_file_version_prov_relation,
+        relatable_to: visible_to_file_version,
+        creator: user,
+        relatable_from: visible_from_file_version)
+      }
+      it_behaves_like 'system_permission can access', :prov_relation, allows: [:scope, :show?, :create?, :destroy?]
+      it_behaves_like 'a policy for', :user, on: :prov_relation, allows: [:show?, :create?, :destroy?]
+    end
+
+    context 'to file_version not visible to user' do
+      let(:prov_relation) { FactoryGirl.create(:derived_from_file_version_prov_relation,
+        relatable_to: invisible_to_file_version,
+        relatable_from: visible_from_file_version)
+      }
+      it_behaves_like 'system_permission can access', :prov_relation, allows: [:scope, :show?, :create?, :destroy?]
+      it_behaves_like 'a policy for', :user, on: :prov_relation, allows: []
+    end
+  end
+
+  context 'from file_version not visible to user' do
+    context 'to file_version visible to user' do
+      let(:prov_relation) { FactoryGirl.create(:derived_from_file_version_prov_relation,
+        relatable_to: visible_to_file_version,
+        relatable_from: invisible_from_file_version)
+      }
+      it_behaves_like 'system_permission can access', :prov_relation, allows: [:scope, :show?, :create?, :destroy?]
+      it_behaves_like 'a policy for', :user, on: :prov_relation, allows: []
+    end
+
+    context 'to file_version not visible to user' do
+      let(:prov_relation) { FactoryGirl.create(:derived_from_file_version_prov_relation,
+        relatable_to: invisible_to_file_version,
+        relatable_from: invisible_from_file_version)
+      }
+      it_behaves_like 'system_permission can access', :prov_relation, allows: [:scope, :show?, :create?, :destroy?]
+      it_behaves_like 'a policy for', :user, on: :prov_relation, allows: []
+    end
+  end
+end
