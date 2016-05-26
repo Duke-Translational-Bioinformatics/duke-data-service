aa8536662b77f5d67033db5beb48dc07f723f773
Darrin Mann <darrin.mann@duke.edu>
2016-05-26 14:52:13 -0400

    DDS-425 #resolve Validate Fingerprint algorithm is md5, sha256, or sha1

--- app/models/fingerprint.rb
+++ app/models/fingerprint.rb
@@ -4,5 +4,9 @@ class Fingerprint < ActiveRecord::Base
 
   validates :upload_id, presence: true
   validates :value, presence: true
-  validates :algorithm, presence: true
+  validates :algorithm, presence: true, inclusion: {in: :available_algorithms}
+
+  def available_algorithms
+    %w{md5 sha256 sha1}
+  end
 end
--- spec/models/fingerprint_spec.rb
+++ spec/models/fingerprint_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Fingerprint, type: :model do
   subject { FactoryGirl.create(:fingerprint) }
+  let(:algorithms) { %w{md5 sha256 sha1} }
 
   it_behaves_like 'an audited model'
 
@@ -13,5 +14,11 @@ RSpec.describe Fingerprint, type: :model do
     it { is_expected.to validate_presence_of(:upload_id) }
     it { is_expected.to validate_presence_of(:value) }
     it { is_expected.to validate_presence_of(:algorithm) }
+    it { is_expected.to validate_inclusion_of(:algorithm).in_array(algorithms) }
+  end
+
+  describe '#available_algorithms' do
+    it { is_expected.to respond_to(:available_algorithms) }
+    it { expect(subject.available_algorithms).to eq(algorithms)}
   end
 end
