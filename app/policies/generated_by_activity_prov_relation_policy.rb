22128b36b60dc5a3c51a1747acbf27ea705264e7
Darin London <darin.london@duke.edu>
2016-06-09 17:06:16 -0400

    GeneratedByActivityProvRelationPolicySpec expects GeneratedByActivityProvRelationPolicy to grant :scope to users if they can view the Activity or the FileVersion
    GeneratedByActivityProvRelationPolicy meets spec

--- app/policies/generated_by_activity_prov_relation_policy.rb
+++ app/policies/generated_by_activity_prov_relation_policy.rb
@@ -26,7 +26,16 @@ class GeneratedByActivityProvRelationPolicy < ApplicationPolicy
       if user.system_permission
         scope
       else
-        scope.none
+        prov_relation_scope = scope.where(creator: user)
+        prov_relation_scope = prov_relation_scope.union(
+          GeneratedByActivityProvRelation.where(
+            relatable_from_id: policy_scope(FileVersion.all)
+        ))
+        prov_relation_scope = prov_relation_scope.union(
+          GeneratedByActivityProvRelation.where(
+            relatable_to_id: Activity.where(creator: user)
+        ))
+        prov_relation_scope
       end
     end
   end
