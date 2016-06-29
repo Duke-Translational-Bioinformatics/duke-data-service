ecb6f029596a1d63d2065859a84588683c17a9e7
benneely <nigelneely@gmail.com>
2016-06-29 08:09:13 -0400

    in the middle of provenance_activities.py

--- docker/builds/dredd/dredd_scripts/14_children_search.py
+++ docker/builds/dredd/dredd_scripts/14_children_search.py
@@ -10,8 +10,29 @@ utils = imp.load_source("utils",os.path.join(os.getcwd(),'utils.py'))
 ###############################################################################
 #         Children Search
 ###############################################################################
-############################################################################### 
+###############################################################################
 @hooks.before("Search Children > Search project children > Search project children")
-@hooks.before("Search Children > Search folder children > Search folder children")
 def justPass14_1(transaction):
-    utils.pass_this_endpoint(transaction)
+    name = str(uuid.uuid4())
+    description = "Created by dredd under: Projects > Projects collection > Create project"
+    neww = utils.create_a_project(transaction,name,description)
+    proj_id = neww['id']
+    upload_id = utils.upload_a_file(proj_id,True)
+    file_id = utils.create_a_file(proj_id,upload_id)
+    url = transaction['fullPath']
+    temp = str(url).replace('ca29f7df-33ca-46dd-a015-92c46fdb6fd1',proj_id)
+    temp = temp.split('?',1)[0]
+    transaction['fullPath'] = temp
+@hooks.before("Search Children > Search folder children > Search folder children")
+def justPass214_1(transaction):
+    name = str(uuid.uuid4())
+    description = "Created by dredd under: Projects > Projects collection > Create project"
+    neww = utils.create_a_project(transaction,name,description)
+    proj_id = neww ['id']
+    upload_id = utils.upload_a_file(proj_id,True)
+    file_id = utils.create_a_file(proj_id,upload_id)
+    folder_id = utils.create_a_folder(proj_id,'justaname')
+    url = transaction['fullPath']
+    temp = str(url).replace('ca29f7df-33ca-46dd-a015-92c46fdb6fd1',folder_id)
+    temp = temp.split('?',1)[0]
+    transaction['fullPath'] = temp
--- docker/builds/dredd/dredd_scripts/15_provenance_activities.py
+++ docker/builds/dredd/dredd_scripts/15_provenance_activities.py
@@ -3,6 +3,7 @@ import imp
 import os
 import json
 import uuid
+import pprint
 #if you want to import another module for use in this workflow
 utils = imp.load_source("utils",os.path.join(os.getcwd(),'utils.py'))
 
@@ -10,11 +11,18 @@ utils = imp.load_source("utils",os.path.join(os.getcwd(),'utils.py'))
 ###############################################################################
 #           Provenance Activities
 ###############################################################################
-############################################################################### 
-@hooks.before("Provenance Activities > Activities collection > NOT_IMPLEMENTED Create activity")
-@hooks.before("Provenance Activities > Activities collection > NOT_IMPLEMENTED List activities")
-@hooks.before("Provenance Activities > Activities instance > NOT_IMPLEMENTED View activity")
-@hooks.before("Provenance Activities > Activities instance > NOT_IMPLEMENTED Update activity")
-@hooks.before("Provenance Activities > Activities instance > NOT_IMPLEMENTED Delete activity")
+###############################################################################
+@hooks.before("Provenance Activities > Activities collection > Create activity")
+def pass_this_a15_1(transaction):
+    pass
+@hooks.after("Provenance Activities > Activities collection > Create activity")
+def get_prov_activity(transaction):
+    pprint.pprint(transaction.keys())
+    pprint.pprint(transaction[u'real'])
+@hooks.before("Provenance Activities > Activities collection > List activities")
+@hooks.before("Provenance Activities > Activities instance > View activity")
+@hooks.before("Provenance Activities > Activities instance > Update activity")
+@hooks.before("Provenance Activities > Activities instance > Delete activity")
 def justPass15_1(transaction):
+    print(transaction['name'])
     utils.pass_this_endpoint(transaction)
