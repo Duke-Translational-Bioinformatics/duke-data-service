e9463e335b022d0f5f4f99bfd8ed4e317ceabc13
Darin London <darin.london@duke.edu>
2016-07-26 10:50:33 -0400

    includes Graphed::Model specs specify that these behave like graphed models

--- app/models/graph/agent.rb
+++ app/models/graph/agent.rb
@@ -1,5 +1,7 @@
 class Graph::Agent
   include Neo4j::ActiveNode
+  include Graphed::Model
+
   property :model_id, index: :exact
   property :model_kind, index: :exact
   self.mapped_label_name = 'Agent'
