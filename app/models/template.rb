1782f80333e3a4ff979489827c5eb86665b133d1
Darrin Mann <darrin.mann@duke.edu>
2016-08-25 12:46:03 -0400

    Initial validations for Template

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,9 @@
 class Template < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
+
+  validates :name, presence: true,
+    uniqueness: {case_sensitive: false},
+    format: {with: /\A[a-z0-9_]*\z/i}
+  validates :label, presence: true
+  validates :creator, presence: true
 end
