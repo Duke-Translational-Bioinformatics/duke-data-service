d3f991c23734ef575640d166099b427278914b27
Darrin Mann <darrin.mann@duke.edu>
2016-08-25 12:19:53 -0400

    Adding creator association to Template

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,3 @@
 class Template < ActiveRecord::Base
+  belongs_to :creator, class_name: 'User'
 end
--- spec/factories/templates.rb
+++ spec/factories/templates.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name { Faker::Name.first_name }
     label { "#{Faker::App.name}_#{rand(10**3)}" }
     description { Faker::Hacker.say_something_smart }
+    association :creator, factory: :user
     is_deprecated false
   end
 end
