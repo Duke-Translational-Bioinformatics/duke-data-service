56f110dd9e0de9e5b20af12375144d4e31410243
Darrin Mann <darrin.mann@duke.edu>
2016-08-25 12:17:11 -0400

    Correcting errant Faker calls

--- spec/factories/templates.rb
+++ spec/factories/templates.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :template do
-    name { Faker::Name.first }
+    name { Faker::Name.first_name }
     label { "#{Faker::App.name}_#{rand(10**3)}" }
-    description { Faker::App.say_something_smart }
+    description { Faker::Hacker.say_something_smart }
     is_deprecated false
   end
 end
