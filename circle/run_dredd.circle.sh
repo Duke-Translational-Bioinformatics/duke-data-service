df3eab3376884e1f31b8a202d4ead0956fd68e0c
Darin London <darin.london@duke.edu>
2016-08-26 15:24:12 -0400

    this is debugging

--- circle/run_dredd.circle.sh
+++ circle/run_dredd.circle.sh
@@ -8,17 +8,19 @@
 ./docker/circle/cache_docker_image.sh morrisjobke/docker-swift-onlyone latest
 
 # launch_application
+echo "COMPOSE_FILE ${COMPOSE_FILE}"
 docker-compose up -d server
 sleep 10
 docker-compose ps
 curl http://swift.local:12345/info
-docker-compose run rake db:migrate
-docker-compose run rake db:seed
-docker-compose run authservice
-docker-compose run rake storage_provider:create
-docker-compose run rake api_test_user:create
-docker-compose run rake api_test_user_pool:create
+docker-compose run rake db:migrate > /dev/null 2>&1
+docker-compose run rake db:seed > /dev/null 2>&1
+docker-compose run authservice > /dev/null 2>&1
+docker-compose run rake storage_provider:create > /dev/null 2>&1
+docker-compose run rake api_test_user:create > /dev/null 2>&1
+docker-compose run rake api_test_user_pool:create > /dev/null 2>&1
 MY_GENERATED_JWT=$(docker-compose run rake api_test_user:create | tail -1)
+echo "GOT ${MY_GENERATED_JWT}"
 
 # run dredd
-docker-compose run -e MY_GENERATED_JWT -e "HOST_NAME=http://dds.host:3000/api/v1" dredd
+docker-compose run -e MY_GENERATED_JWT -e "HOST_NAME=http://dds.host:3000/api/v1" curl dds.host:3000/api/v1/app/status
