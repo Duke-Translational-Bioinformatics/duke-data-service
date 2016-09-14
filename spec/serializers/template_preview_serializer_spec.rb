1bab0abdb7a1e13c62d287fe78067a256f7ec3d7
Darrin Mann <darrin.mann@duke.edu>
2016-09-14 16:05:23 -0400

    Removing label from TemplatePreviewSerializer

--- spec/serializers/template_preview_serializer_spec.rb
+++ spec/serializers/template_preview_serializer_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe TemplatePreviewSerializer, type: :serializer do
   let(:resource) { FactoryGirl.create(:template) }
   let(:expected_attributes) {{
     'id' => resource.id,
-    'name' => resource.name,
-    'label' => resource.label
+    'name' => resource.name
   }}
 
   it_behaves_like 'a json serializer' do
