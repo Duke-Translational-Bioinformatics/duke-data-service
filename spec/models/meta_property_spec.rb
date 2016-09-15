08b278f4ce3f6377cda42e5693201134565e11d8
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-09-15 14:53:14 -0400

    Added callback for set_property_from_key and validation for key attribute

--- spec/models/meta_property_spec.rb
+++ spec/models/meta_property_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe MetaProperty, type: :model do
+  let(:meta_template) { FactoryGirl.create(:meta_template) }
+  let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
+  let(:other_property) { FactoryGirl.create(:property) }
 
   it_behaves_like 'an audited model'
 
@@ -9,23 +12,40 @@ RSpec.describe MetaProperty, type: :model do
     it { is_expected.to belong_to(:property) }
   end
 
+  describe 'callbacks' do
+    it { is_expected.to callback(:set_property_from_key).before(:validation) }
+  end
+
   describe 'validations' do
     let!(:existing_tag_for_uniqueness_validation) { FactoryGirl.create(:meta_property) }
+
     it { is_expected.to validate_presence_of(:meta_template) }
     it { is_expected.to validate_presence_of(:property) }
     it { is_expected.to validate_presence_of(:value) }
 
     it { is_expected.to validate_uniqueness_of(:property).scoped_to(:meta_template_id).case_insensitive }
+
+    context 'when a meta_template is set' do
+      subject { FactoryGirl.build(:meta_property, meta_template: meta_template) }
+
+      it { is_expected.to allow_value(nil).for(:key) }
+      it { is_expected.to allow_value(property.key).for(:key) }
+      it { is_expected.not_to allow_value(other_property.key).for(:key) }
+    end
+
+    context 'when a meta_template is nil' do
+      subject { FactoryGirl.build(:meta_property, meta_template: nil) }
+
+      it { is_expected.to allow_value(nil).for(:key) }
+      it { is_expected.not_to allow_value(property.key).for(:key) }
+      it { is_expected.not_to allow_value(other_property.key).for(:key) }
+    end
   end
 
   it { is_expected.to respond_to(:key) }
   it { is_expected.to respond_to(:key=).with(1).argument }
 
   describe '#set_property_from_key' do
-    let(:meta_template) { FactoryGirl.create(:meta_template) }
-    let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
-    let(:other_property) { FactoryGirl.create(:property) }
-
     it { is_expected.to respond_to(:set_property_from_key) }
 
     context 'called' do
