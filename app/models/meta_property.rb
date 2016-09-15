a626c3d6c60c67fa073b3a91cf39629c90958e8e
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-09-15 13:58:52 -0400

    Create key atribute and set_property_from_key method

--- app/models/meta_property.rb
+++ app/models/meta_property.rb
@@ -1,5 +1,7 @@
 class MetaProperty < ActiveRecord::Base
   include RequestAudited
+  attr_accessor :key
+
   audited
   belongs_to :meta_template
   belongs_to :property
@@ -8,4 +10,12 @@ class MetaProperty < ActiveRecord::Base
     uniqueness: {scope: [:meta_template_id], case_sensitive: false}
   validates :meta_template, presence: true
   validates :value, presence: true
+
+  def set_property_from_key
+    if meta_template && meta_template.template
+      self.property = meta_template.template.properties.where(key: key).first
+    else
+      self.property = nil
+    end
+  end
 end
--- spec/models/meta_property_spec.rb
+++ spec/models/meta_property_spec.rb
@@ -17,4 +17,50 @@ RSpec.describe MetaProperty, type: :model do
 
     it { is_expected.to validate_uniqueness_of(:property).scoped_to(:meta_template_id).case_insensitive }
   end
+
+  it { is_expected.to respond_to(:key) }
+  it { is_expected.to respond_to(:key=).with(1).argument }
+
+  describe '#set_property_from_key' do
+    let(:meta_template) { FactoryGirl.create(:meta_template) }
+    let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
+    let(:other_property) { FactoryGirl.create(:property) }
+
+    it { is_expected.to respond_to(:set_property_from_key) }
+
+    context 'called' do
+      before { expect{subject.set_property_from_key}.not_to raise_error }
+
+      context 'when key is nil' do
+        subject { FactoryGirl.build(:meta_property, meta_template: meta_template, key: nil) }
+
+        it { expect(subject.key).to be_nil }
+        it { expect(subject.property).to be_nil }
+      end
+
+      context 'when meta_template is nil' do
+        subject { FactoryGirl.build(:meta_property, meta_template: nil, key: property.key) }
+
+        it { expect(subject.key).to eq(property.key) }
+        it { expect(subject.meta_template).to be_nil }
+        it { expect(subject.property).to be_nil }
+      end
+
+      context 'when key is a property from another template' do
+        subject { FactoryGirl.build(:meta_property, meta_template: meta_template, key: other_property.key) }
+
+        it { expect(subject.key).to eq(other_property.key) }
+        it { expect(subject.meta_template).to eq(meta_template) }
+        it { expect(subject.property).to be_nil }
+      end
+
+      context 'when key is a property from the assigned template' do
+        subject { FactoryGirl.build(:meta_property, meta_template: meta_template, key: property.key) }
+
+        it { expect(subject.key).to eq(property.key) }
+        it { expect(subject.meta_template).to eq(meta_template) }
+        it { expect(subject.property).to eq(property) }
+      end
+    end
+  end
 end
