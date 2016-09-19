f089aee7c452fb3653caaa8d0834d06a3c62897b
Darrin Mann <darrin.mann@duke.edu>
2016-09-19 15:37:47 -0400

    Stubbing out the rspec requests for metatemplate api

--- spec/requests/meta_templates_api_spec.rb
+++ spec/requests/meta_templates_api_spec.rb
@@ -26,10 +26,19 @@ describe DDS::V1::MetaTemplatesAPI do
   let!(:resource_permission) { project_permission }
   let(:resource_stub) { meta_template_stub }
 
+  let(:file_id) { data_file.id }
+  let(:resource_kind) { data_file.kind }
+
   describe 'MetaTemplates collection' do
+    let(:url) { "/api/v1/meta/#{resource_kind}/#{file_id}" }
+
+    describe 'GET' do
+      subject { get(url, nil, headers) }
+    end
+  end
+
+  describe 'Object Metadata Instance' do
     let(:url) { "/api/v1/meta/#{resource_kind}/#{file_id}/#{template_id}" }
-    let(:file_id) { data_file.id }
-    let(:resource_kind) { data_file.kind }
     let(:template_id) { template.id }
 
     describe 'POST' do
@@ -91,6 +100,30 @@ describe DDS::V1::MetaTemplatesAPI do
         let(:payload_property_value) { '' }
         it_behaves_like 'a validated resource'
       end
+    end
+
+    describe 'GET' do
+      subject { get(url, nil, headers) }
+    end
+
+    describe 'PUT' do
+      subject { put(url, payload.to_json, headers) }
+      let(:called_action) { 'PUT' }
+      let!(:payload) {{
+        properties: [
+          {
+            "key": payload_property_key,
+            "value": payload_property_value
+          }
+        ]
+      }}
+      let(:payload_property_key) { property.key }
+      let(:payload_property_value) { meta_property_stub.value }
+    end
+
+    describe 'DELETE' do
+      subject { delete(url, nil, headers) }
+      let(:called_action) { 'DELETE' }
     end
   end
 end
