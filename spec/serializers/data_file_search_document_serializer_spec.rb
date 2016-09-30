ad4a4db5ebd33757df731f9f69f7234e6f8184e1
Darin London <darin.london@duke.edu>
2016-09-30 10:58:06 -0400

    moved 'tagged document' and 'metadata annotated document' shared examples from DataFileSearchDocumentSerializerSpec to search_shared_examples so other tests can use them
    FolderSearchDocumentSerializerSpec expects FolderSearchdocumentSerializer
    json to behave like a metadata annotated document'
    
    FolderSearchDocumentSerializer meets spec

--- spec/serializers/data_file_search_document_serializer_spec.rb
+++ spec/serializers/data_file_search_document_serializer_spec.rb
@@ -1,47 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe DataFileSearchDocumentSerializer, type: :serializer do
-  shared_examples 'a tagged document' do
-    it 'should have tags with only labels and values' do
-      expect(tag).to be_persisted
-      resource.reload
-      expect(resource.tags).not_to be_empty
-      is_expected.to have_key 'tags'
-      expect(subject['tags']).not_to be_empty
-      tag_document = subject['tags']
-      expect(tag_document).to be_a Array
-      resource.tags.each do |tag|
-        expect(tag_document).to include({"label" => tag.label})
-      end
-    end
-  end
-
-  shared_examples 'a metadata annotated document' do
-    it 'should have expected meta section' do
-      expect(meta_template).to be_persisted
-      expect(property).to be_persisted
-      expect(meta_property).to be_persisted
-      resource.reload
-      meta_template.reload
-      meta_property.reload
-
-      expect(resource.meta_templates).not_to be_empty
-      is_expected.to have_key 'meta'
-      expect(subject['meta']).not_to be_empty
-      metadata = subject['meta']
-      resource.meta_templates.each do |meta_template|
-        expect(meta_template.meta_properties).not_to be_empty
-        expect(metadata).to have_key meta_template.template.name
-        meta_template_document = metadata[meta_template.template.name]
-        expect(meta_template_document).to be_a Hash
-        meta_template.meta_properties.each do |prop|
-          expect(meta_template_document).to have_key prop.property.key
-          expect(meta_template_document[prop.property.key]).to eq(prop.value)
-        end
-      end
-    end
-  end
-
   let(:resource) { FactoryGirl.create(:data_file) }
   let(:expected_file_attributes) {{
     'id' => resource.id,
--- spec/serializers/folder_search_document_serializer_spec.rb
+++ spec/serializers/folder_search_document_serializer_spec.rb
@@ -1,38 +1,6 @@
 RSpec.describe FolderSearchDocumentSerializer, type: :serializer do
-  shared_examples 'a tagged document' do
-    it 'should have tags with only labes and values' do
-      expect(resource.tags).to exist
-      is_expected.to have_key 'tags'
-      expect(subject['tags']).not_to be_empty
-      tag_document = subject['tags']
-      expect(tag_document).to be_a Array
-      resource.tags.each do |tag|
-        expect(tag_document).to include({label: tag.label})
-      end
-    end
-  end
-
-  shared_examples 'a metadata annotated document' do
-    it 'should have expected metadata section' do
-      expect(resource.meta_templates).to exist
-      is_expected.to have_key 'metadata'
-      expect(subject['metadata']).not_to be_empty
-      metadata = subject['metadata']
-      resource.meta_templates.each do |meta_template|
-        expect(meta_template.meta_properties).to exist
-        expect(metadata).to have_key meta_template.name
-        meta_template_document = metadata[meta_template.name]
-        expect(meta_template_document).to be_a Hash
-        meta_template.meta_properties.each do |prop|
-          expect(meta_template_document).to have_key prop.key
-          expect(meta_template_document[prop.key]).to eq(prop.value)
-        end
-      end
-    end
-  end
-
   let(:resource) { FactoryGirl.create(:data_file) }
-  let(:expected_file_attributes) {{
+  let(:expected_folder_attributes) {{
     'id' => resource.id,
     'name' => resource.name,
     'is_deleted' => resource.is_deleted?,
@@ -42,6 +10,18 @@ RSpec.describe FolderSearchDocumentSerializer, type: :serializer do
   }}
 
   it_behaves_like 'a json serializer' do
-    it { is_expected.to include(expected_file_attributes) }
+    it { is_expected.to include(expected_folder_attributes) }
+  end
+
+  context 'with meta_templates' do
+    let(:meta_template) { FactoryGirl.create(:meta_template, templatable: resource) }
+    let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
+    let(:meta_property){ FactoryGirl.create(:meta_property, meta_template: meta_template, property: property) }
+    include_context 'elasticsearch prep', [:meta_template, :property, :meta_property], [:resource]
+
+    it_behaves_like 'a json serializer' do
+      it { is_expected.to include(expected_folder_attributes) }
+      it_behaves_like 'a metadata annotated document'
+    end
   end
 end
