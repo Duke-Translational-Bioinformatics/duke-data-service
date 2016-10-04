c633d0d164be22027dbdf64cd2c4d9fd41cb11fe
Darin London <darin.london@duke.edu>
2016-10-04 14:37:14 -0400

    refactored the expectations of a ProvenanceGraphSerializer into a shared_example in the test itself (there are no other tests currently that use it, but if they do, then it can be moved easily)
    updated the original test into a 'SearchProvenanceGraph' context using the new shared example
    
    added a 'WasGeneratedByProvenanceGraph' context to test that this ProvenanceGraph implementation
    works with the ProvenanceGraphSerializer

--- spec/serializers/provenance_graph_serializer_spec.rb
+++ spec/serializers/provenance_graph_serializer_spec.rb
@@ -1,45 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe ProvenanceGraphSerializer, type: :serializer do
+  shared_examples 'a ProvenanceGraphSerializer' do |expected_object_node_syms:, expected_object_relationship_syms:|
+    let(:expected_object_nodes) { expected_object_node_syms.map{|enodesym| send(enodesym) }.flatten }
+    let(:expected_object_relationships) { expected_object_relationship_syms.map{|ersym| send(ersym) }.flatten }
+
+    it_behaves_like 'a has_many association with', :nodes, ProvenanceGraphNodeSerializer
+    it_behaves_like 'a has_many association with', :relationships, ProvenanceGraphRelationshipSerializer
+
+    it_behaves_like 'a json serializer' do
+      it 'should have expected keys and values' do
+        is_expected.to have_key('nodes')
+        is_expected.to have_key('relationships')
+        expected_nodes = expected_object_nodes.map{|enode|
+          g = ProvenanceGraphNode.new(enode.graph_node)
+          g.properties = enode
+          JSON.parse(ProvenanceGraphNodeSerializer.new(g).to_json)
+        }.flatten
+        expect(subject["nodes"]).to match_array(expected_nodes)
+
+        expected_relationships = expected_object_relationships.map{|expected_object_relationship|
+          expected_relationship = ProvenanceGraphRelationship.new(expected_object_relationship.graph_relation)
+          expected_relationship.properties = expected_object_relationship
+          JSON.parse(
+            ProvenanceGraphRelationshipSerializer.new(expected_relationship).to_json
+          )
+        }.flatten
+        expect(subject["relationships"]).to match_array(expected_relationships)
+      end
+    end
+  end
+
   let(:policy_scope) { Proc.new {|scope| scope } }
 
-  # (activity)-(used)->(focus)
-  let!(:focus) {
-    FactoryGirl.create(:file_version, label: "FOCUS")
-  }
+  context 'SearchProvenanceGraph' do
+    # (activity)-(used)->(focus)
+    let!(:focus) {
+      FactoryGirl.create(:file_version, label: "FOCUS")
+    }
 
-  let!(:activity) { FactoryGirl.create(:activity, name: "ACTIVITY") }
-  let!(:activity_used_focus) {
-    FactoryGirl.create(:used_prov_relation,
-      relatable_from: activity,
-      relatable_to: focus
-    )
-  }
-  let(:resource) {
-    ProvenanceGraph.new(focus: focus, policy_scope: policy_scope)
-  }
+    let!(:activity) { FactoryGirl.create(:activity, name: "ACTIVITY") }
+    let!(:activity_used_focus) {
+      FactoryGirl.create(:used_prov_relation,
+        relatable_from: activity,
+        relatable_to: focus
+      )
+    }
+    let(:resource) {
+      SearchProvenanceGraph.new(focus: focus, policy_scope: policy_scope)
+    }
 
-  it_behaves_like 'a has_many association with', :nodes, ProvenanceGraphNodeSerializer
-  it_behaves_like 'a has_many association with', :relationships, ProvenanceGraphRelationshipSerializer
+    it_behaves_like 'a ProvenanceGraphSerializer', expected_object_node_syms: [:focus, :activity],
+      expected_object_relationship_syms: [:activity_used_focus]
+  end
 
-  it_behaves_like 'a json serializer' do
-    it 'should have expected keys and values' do
-      is_expected.to have_key('nodes')
-      is_expected.to have_key('relationships')
-      expected_nodes = [focus, activity].map{|enode|
-        g = ProvenanceGraphNode.new(enode.graph_node)
-        g.properties = enode
-        JSON.parse(ProvenanceGraphNodeSerializer.new(g).to_json)
-      }.flatten
-
-      expected_relationship = ProvenanceGraphRelationship.new(activity_used_focus.graph_relation)
-      expected_relationship.properties = activity_used_focus
-
-      expect(subject["nodes"]).to match_array(expected_nodes)
-      expect(subject["relationships"]).to match_array([
-        JSON.parse(
-          ProvenanceGraphRelationshipSerializer.new(expected_relationship).to_json
-        ) ] )
-    end
+  context 'WasGeneratedByProvenanceGraph' do
+    # (fv1ga)-[generated]->(fv1)
+    let!(:fv1) { FactoryGirl.create(:file_version, label: "FV1") }
+    let!(:fv1ga) { FactoryGirl.create(:activity, name: "FV1GA") }
+    let!(:fv1ga_generated_fv1) {
+      FactoryGirl.create(:generated_by_activity_prov_relation,
+        relatable_from: fv1,
+        relatable_to: fv1ga
+      )
+    }
+    let!(:file_versions) { [ fv1.id ] }
+    let(:resource) {
+      WasGeneratedByProvenanceGraph.new(
+        file_versions: file_versions,
+        policy_scope: policy_scope
+      )
+    }
+    it_behaves_like 'a ProvenanceGraphSerializer', expected_object_node_syms: [:fv1, :fv1ga],
+      expected_object_relationship_syms: [:fv1ga_generated_fv1]
   end
 end
