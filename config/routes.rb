a32bd01b8f8a95799d0fee861d6cb794a19df903
Darrin Mann <darrin.mann@duke.edu>
2016-10-10 15:03:56 -0400

    Cherry picking the apidocs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount DDS::Base, at: '/'
   get "apiexplorer" => "swaggerui#index"
+  get "apidocs" => "apidocs#index"
   root to: redirect("/portal", status: 302)
 end
