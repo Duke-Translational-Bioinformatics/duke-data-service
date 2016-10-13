e41833700f58a568ad30a958611204d505697b56
Darin London <darin.london@duke.edu>
2016-10-13 15:02:01 -0400

    ProvenanceGraphNode and ProvenanceGraphRelationship  both store the properties on initialization, but allow the restricted flag  (default false), to be set to true. This makes it possible for the serializers  to use the RestrictedObjectSerializer when applicable.

--- app/models/provenance_graph_relationship.rb
+++ app/models/provenance_graph_relationship.rb
@@ -2,17 +2,22 @@ class ProvenanceGraphRelationship
   include ActiveModel::Serialization
   include Comparable
 
-  attr_reader :id, :type, :start_node, :end_node
-  attr_accessor :properties
+  attr_reader :id, :type, :start_node, :end_node, :properties
+  attr_accessor :restricted
 
   def initialize(relationship)
     @id = relationship.model_id
     @type = relationship.type
     @start_node = relationship.from_node.model_id
     @end_node = relationship.to_node.model_id
-    @properties = nil
+    @properties = relationship.graphed_model
+    @restricted = false
   end
 
+  def is_restricted?
+    self.restricted
+  end
+  
   def <=>(other_provenance)
     [self.id, self.type, self.start_node, self.end_node, self.properties] <=> [other_provenance.id, other_provenance.type, other_provenance.start_node, other_provenance.end_node, other_provenance.properties]
   end
