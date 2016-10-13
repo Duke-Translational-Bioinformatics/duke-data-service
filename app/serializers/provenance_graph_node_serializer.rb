79a1604d16839d8f2c519d7fcb2d91a10ff2f983
Darin London <darin.london@duke.edu>
2016-10-13 15:09:54 -0400

    tests for differences in restricted and not restricted Nodes and Relationships.
    Restricted nodes use the RestrictedObjectSerializer, while restricted relationships
    return null properties.

--- app/serializers/provenance_graph_node_serializer.rb
+++ app/serializers/provenance_graph_node_serializer.rb
@@ -2,14 +2,10 @@ class ProvenanceGraphNodeSerializer < ActiveModel::Serializer
   attributes :id, :labels, :properties
 
   def properties
-    if object.properties
-      ActiveModel::Serializer.serializer_for(object.properties).new(object.properties)
+    if object.is_restricted?
+      RestrictedObjectSerializer.new(object.properties)
     else
-      {
-        id: object.node.model_id,
-        kind: object.node.model_kind,
-        is_deleted: object.node.is_deleted ? "true" : "false"
-      }
+      ActiveModel::Serializer.serializer_for(object.properties).new(object.properties)
     end
   end
 end
