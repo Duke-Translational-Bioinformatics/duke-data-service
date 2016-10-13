e32619f8f71ac88a14c9627bb72fd97fcabb5b8d
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-10-13 18:58:20 -0400

    Added GET list project transfer endpoint

--- spec/requests/project_transfers_api_spec.rb
+++ spec/requests/project_transfers_api_spec.rb
@@ -4,7 +4,7 @@ describe DDS::V1::ProjectTransfersAPI do
   include_context 'with authentication'
   let(:project) { FactoryGirl.create(:project) }
   let(:other_project) { FactoryGirl.create(:project) }
-  let(:project_transfer) { FactoryGirl.create(:project_transfer) }
+  let(:project_transfer) { FactoryGirl.create(:project_transfer, project: project) }
   let(:to_user) { FactoryGirl.create(:user) }
   let(:other_project_transfer) { FactoryGirl.create(:project_transfer) }
   let(:project_transfer_stub) { FactoryGirl.create(:project_transfer) }
@@ -68,6 +68,30 @@ describe DDS::V1::ProjectTransfersAPI do
       end
       it_behaves_like 'a validated resource' do
         let(:payload_to_user_id) { "doesNotExist" }
+      end
+    end
+
+    describe 'GET' do
+      subject { get(url, nil, headers) }
+
+      it_behaves_like 'an authenticated resource'
+      it_behaves_like 'a software_agent accessible resource'
+      it_behaves_like 'a listable resource' do
+        let!(:project_transfer_from) { FactoryGirl.create(:project_transfer, from_user: current_user)}
+        let!(:project_transfer_user) { FactoryGirl.create(:project_transfer_user, to_user: current_user)}
+        let(:expected_list_length) { expected_resources.length }
+        let!(:expected_resources) { [
+          project_transfer
+        ]}
+        let!(:unexpected_resources) { [
+          other_project_transfer,
+          project_transfer_from,
+          project_transfer_user.project_transfer
+        ] }
+      end
+      it_behaves_like 'an identified resource' do
+        let(:project_id) { "doesNotExist" }
+        let(:resource_class) { Project }
       end
     end
   end
