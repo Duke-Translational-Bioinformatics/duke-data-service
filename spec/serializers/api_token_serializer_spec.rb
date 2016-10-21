b9a0e413224d520f84406b74e1381bb907e39db0
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-10-21 08:21:37 -0400

    Refactor api_token_serializer_spec

--- spec/serializers/api_token_serializer_spec.rb
+++ spec/serializers/api_token_serializer_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ApiTokenSerializer, type: :serializer do
   let(:user) { FactoryGirl.create(:user) }
+  let(:expected_attributes) {{
+    'api_token' => resource.api_token,
+    'expires_on' => resource.expires_on,
+    'time_to_live' => resource.time_to_live
+  }}
 
   context 'with UserAuthenticationService Authenticated User' do
     let(:auth_service) { FactoryGirl.create(:authentication_service) }
@@ -14,14 +19,7 @@ RSpec.describe ApiTokenSerializer, type: :serializer do
       ApiToken.new(user: user, user_authentication_service: user_authentication_service)
     }
     it_behaves_like 'a json serializer' do
-      it 'should have expected keys and values' do
-        is_expected.to have_key('api_token')
-        is_expected.to have_key('expires_on')
-        is_expected.to have_key('time_to_live')
-        expect(subject['api_token']).to be
-        expect(subject['expires_on']).to be
-        expect(subject['time_to_live']).to be
-      end
+      it { is_expected.to include(expected_attributes) }
     end
   end
 end
