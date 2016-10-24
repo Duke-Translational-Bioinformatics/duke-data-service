7e207d8fef694c3ad52e05c9fd91dc10ba941452
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-10-24 13:51:27 -0400

    Refactor user_serializer_spec

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -21,29 +21,6 @@ RSpec.describe UserSerializer, type: :serializer do
 
   it_behaves_like 'a json serializer' do
     it { is_expected.to include(expected_attributes) }
-    it 'should have expected keys and values' do
-      is_expected.to have_key('id')
-      is_expected.to have_key('username')
-      is_expected.to have_key('full_name')
-      is_expected.to have_key('first_name')
-      is_expected.to have_key('last_name')
-      is_expected.to have_key('email')
-      is_expected.to have_key('auth_provider')
-      is_expected.to have_key('last_login_on')
-      is_expected.to have_key('agent')
-      expect(subject['id']).to eq(resource.id)
-      expect(subject['username']).to eq(resource.username)
-      expect(subject['full_name']).to eq(resource.display_name)
-      expect(subject['first_name']).to eq(resource.first_name)
-      expect(subject['last_name']).to eq(resource.last_name)
-      expect(subject['email']).to eq(resource.email)
-      expect(subject['auth_provider']).to have_key('uid')
-      expect(subject['auth_provider']).to have_key('source')
-      expect(subject['auth_provider']['uid']).to eq(user_authentication_service.uid)
-      expect(subject['auth_provider']['source']).to eq(user_authentication_service.authentication_service.name)
-      expect(subject['last_login_on'].to_json).to eq(resource.last_login_at.to_json)
-      expect(subject['agent']).to be_nil
-    end
     it_behaves_like 'a serializer with a serialized audit'
 
     context 'with current_software_agent' do
