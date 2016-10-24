05e5450736400147837d52bddad447732c14019b
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-10-24 14:21:26 -0400

    Refactor folder_serializer_spec

--- spec/serializers/folder_serializer_spec.rb
+++ spec/serializers/folder_serializer_spec.rb
@@ -5,27 +5,31 @@ RSpec.describe FolderSerializer, type: :serializer do
   let(:is_logically_deleted) { true }
   let(:child_folder) { FactoryGirl.create(:folder, :with_parent) }
   let(:root_folder) { FactoryGirl.create(:folder, :root) }
-  let(:expected_attributes_with_parent) {{
-    'id' => resource.id,
-    'parent' => { 'kind' => resource.parent.kind, 'id' => resource.parent.id },
-    'name' => resource.name,
-    'is_deleted' => resource.is_deleted
-  }}
-  let(:expected_attributes_without_parent) {{
-    'parent' => { 'kind' => resource.project.kind, 'id' => resource.project.id }
-  }}
-  
+
   it_behaves_like 'a has_one association with', :project, ProjectPreviewSerializer
   it_behaves_like 'a has_many association with', :ancestors, AncestorSerializer
 
   it_behaves_like 'a json serializer' do
     context 'with a parent' do
-      it { is_expected.to include(expected_attributes_with_parent) }
+      let(:expected_attributes) {{
+        'id' => resource.id,
+        'parent' => { 'kind' => resource.parent.kind,
+                      'id' => resource.parent.id
+                    },
+        'name' => resource.name,
+        'is_deleted' => resource.is_deleted
+      }}
+      it { is_expected.to include(expected_attributes) }
     end
 
     context 'without a parent' do
       let(:resource) { root_folder }
-      it { is_expected.to include(expected_attributes_without_parent) }
+      let(:expected_attributes) {{
+        'parent' => { 'kind' => resource.project.kind,
+                      'id' => resource.project.id
+                    }
+      }}
+      it { is_expected.to include(expected_attributes) }
     end
   end
 end
