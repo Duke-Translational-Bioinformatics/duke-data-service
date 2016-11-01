158426710b10b542f541e72eae16de141d0e3a54
Darrin Mann <darrin.mann@duke.edu>
2016-11-01 10:43:14 -0400

    Changing parallelize_test_suite.sh to its previous state, even though it is ignored

--- circle/parallelize_test_suite.sh
+++ circle/parallelize_test_suite.sh
@@ -2,15 +2,9 @@
 
 case $CIRCLE_NODE_INDEX in
   0)
-    bundle exec rspec spec/[m]*
+    bundle exec rspec
     ;;
   1)
-    bundle exec rspec spec/[p]*
-    ;;
-  2)
-    bundle exec rspec spec/[r]*
-    ;;
-  4)
-    bundle exec rspec spec/[^mpr]*
+    ./circle/run_dredd.circle.sh
     ;;
 esac
