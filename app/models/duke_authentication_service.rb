2805940320db0de3ceaa9633ff1eae17a3b45093
Darin London <darin.london@duke.edu>
2016-11-07 15:51:26 -0500

    DukeAuthenticationServiceSpec expects DukeAuthenticationService to implement get_user_for_access_token - returns a User with a user.current_user_authentication_service set - user returned is a new user, with a newly built user_authentication_service if the user has not used the DukeAuthenticationService before - raises a JWT::VerificationError if the token is not signed with the Rails secret

--- app/models/duke_authentication_service.rb
+++ app/models/duke_authentication_service.rb
@@ -1,2 +1,28 @@
 class DukeAuthenticationService < AuthenticationService
+  def get_user_for_access_token(encoded_access_token)
+    access_token = JWT.decode(encoded_access_token, Rails.application.secrets.secret_key_base)[0]
+    user_authentication_service = user_authentication_services.where(uid: access_token['uid']).first
+    if user_authentication_service
+      user = user_authentication_service.user
+      user.current_user_authenticaiton_service = user_authentication_service
+      user
+    else
+      new_user = User.new(
+        id: SecureRandom.uuid,
+        username: access_token['uid'],
+        etag: SecureRandom.hex,
+        email: access_token['email'],
+        display_name: access_token['display_name'],
+        first_name: access_token['first_name'],
+        last_login_at: DateTime.now,
+        last_name: access_token['last_name']
+      )
+      user_authentication_service = new_user.user_authentication_services.build(
+        uid: access_token['uid'],
+        authentication_service: self
+      )
+      new_user.current_user_authenticaiton_service = user_authentication_service
+      new_user
+    end
+  end
 end
--- spec/models/duke_authentication_service_spec.rb
+++ spec/models/duke_authentication_service_spec.rb
@@ -3,4 +3,94 @@ require 'rails_helper'
 RSpec.describe DukeAuthenticationService, type: :model do
   subject { FactoryGirl.create(:duke_authentication_service) }
   it_behaves_like 'an authentication service'
+  context 'get_user_for_access_token' do
+    context 'with valid token' do
+      context 'for first time user' do
+        let(:new_user) { FactoryGirl.build(:user) }
+        let (:user_token) {
+          {
+            'service_id' => subject.id,
+            'uid' => SecureRandom.uuid,
+            'display_name' => new_user.display_name,
+            'first_name' => new_user.first_name,
+            'last_name' => new_user.last_name,
+            'email' => new_user.email
+          }
+        }
+        let (:access_token) {
+          JWT.encode(user_token, Rails.application.secrets.secret_key_base)
+        }
+
+        it 'should return an unpersisted user with an unpersisted user_authentication_service assigned to user.current_user_authenticaiton_service' do
+          returned_user = subject.get_user_for_access_token(access_token)
+          expect(returned_user).not_to be_persisted
+          expect(returned_user.current_user_authenticaiton_service).not_to be_nil
+          expect(returned_user.current_user_authenticaiton_service).not_to be_persisted
+          expect(returned_user.current_user_authenticaiton_service.authentication_service_id).to eq(subject.id)
+        end
+      end
+
+      context 'for existing user' do
+        let(:current_user) { FactoryGirl.create(:user) }
+        let(:user_auth) {
+          FactoryGirl.create(:user_authentication_service,
+          authentication_service: subject,
+          user: current_user)
+        }
+        let (:user_token) {
+          {
+            'service_id' => subject.id,
+            'uid' => user_auth.uid,
+            'display_name' => current_user.display_name,
+            'first_name' => current_user.first_name,
+            'last_name' => current_user.last_name,
+            'email' => current_user.email
+          }
+        }
+        let (:access_token) {
+          expect(user_auth).to be_persisted
+          expect(current_user).to be_persisted
+          JWT.encode(user_token, Rails.application.secrets.secret_key_base)
+        }
+
+        it 'should return a persisted user with the user_authentication_service assigned to user.current_user_authenticaiton_service' do
+          expect(user_auth).to be_persisted
+          expect(current_user).to be_persisted
+          returned_user = subject.get_user_for_access_token(access_token)
+          expect(returned_user).to be_persisted
+          expect(returned_user.current_user_authenticaiton_service).not_to be_nil
+          expect(returned_user.current_user_authenticaiton_service.id).to eq(user_auth.id)
+        end
+      end
+    end
+
+    context 'with token not signed with our secret' do
+      let(:current_user) { FactoryGirl.create(:user) }
+      let(:user_auth) {
+        FactoryGirl.create(:user_authentication_service,
+        authentication_service: subject,
+        user: current_user)
+      }
+      let (:user_token) {
+        {
+          'service_id' => subject.id,
+          'uid' => user_auth.uid,
+          'display_name' => current_user.display_name,
+          'first_name' => current_user.first_name,
+          'last_name' => current_user.last_name,
+          'email' => current_user.email
+        }
+      }
+      let (:wrong_secret_access_token) {
+        JWT.encode(user_token, 'WrongSecret')
+      }
+      it {
+        expect(user_auth).to be_persisted
+        expect(current_user).to be_persisted
+        expect {
+          subject.get_user_for_access_token(wrong_secret_access_token)
+        }.to raise_error(JWT::VerificationError)
+      }
+    end
+  end
 end
