bae6f85ecacd46a9b46ee70d939bba791a459db1
Darin London <darin.london@duke.edu>
2016-11-08 11:08:29 -0500

    standardized get_user_for_access_token expectations and moved them to the authentication_service_shared_examples 'an authentication service' tests

--- spec/models/duke_authentication_service_spec.rb
+++ spec/models/duke_authentication_service_spec.rb
@@ -54,49 +54,4 @@ RSpec.describe DukeAuthenticationService, type: :model do
   }
 
   it_behaves_like 'an authentication service'
-
-  context 'get_user_for_access_token' do
-    context 'with valid token' do
-      context 'for first time user' do
-        it 'should return an unpersisted user with an unpersisted user_authentication_service assigned to user.current_user_authenticaiton_service' do
-          returned_user = subject.get_user_for_access_token(first_time_user_access_token)
-          expect(returned_user).not_to be_persisted
-          expect(returned_user.current_user_authenticaiton_service).not_to be_nil
-          expect(returned_user.current_user_authenticaiton_service).not_to be_persisted
-          expect(returned_user.current_user_authenticaiton_service.authentication_service_id).to eq(subject.id)
-        end
-      end
-
-      context 'for existing user' do
-        context 'already authenticated with this authentication service' do
-          it 'should return a persisted user with a persisted user_authentication_service assigned to user.current_user_authenticaiton_service' do
-            returned_user = subject.get_user_for_access_token(existing_user_access_token)
-            expect(returned_user).to be_persisted
-            expect(returned_user.id).to eq(existing_user.id)
-            expect(returned_user.current_user_authenticaiton_service).not_to be_nil
-            expect(returned_user.current_user_authenticaiton_service).to be_persisted
-            expect(returned_user.current_user_authenticaiton_service.id).to eq(existing_user_auth.id)
-          end
-        end
-
-        context 'not authenticated with this authentication service' do
-          it 'should return a persisted user with the an unpersisted user_authentication_service assigned to user.current_user_authenticaiton_service' do
-            returned_user = subject.get_user_for_access_token(existing_first_authenticating_access_token)
-            expect(returned_user).to be_persisted
-            expect(returned_user.id).to eq(existing_user.id)
-            expect(returned_user.current_user_authenticaiton_service).not_to be_nil
-            expect(returned_user.current_user_authenticaiton_service).not_to be_persisted
-          end
-        end
-      end
-    end
-
-    context 'with invalid token' do
-      it {
-        expect {
-          subject.get_user_for_access_token(invalid_access_token)
-        }.to raise_error(InvalidAccessTokenException)
-      }
-    end
-  end
 end
