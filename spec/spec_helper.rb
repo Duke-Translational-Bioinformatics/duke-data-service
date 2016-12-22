33b1fdbbd52ab9434e2e812bc856cbcd18f07b17
Jonathan Parrish <jonathan.parrish@duke.edu>
2016-12-22 16:05:08 -0500

    ommited too much

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,9 @@ if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
 end
+SimpleCov.start
+
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
