a69ead72ffe2a7bca5a3cbf9f99d727c1095851d
Darin London <darin.london@duke.edu>
2016-12-22 11:53:18 -0500

    stitching together a single html view of the api_docs requires a little more complex processing of the individual api_docs files.
    bin/gen_apiary.sh now must be run in an apiary docker image container, processes each api_docs/*apib
      file based on the following logic:
       - api_docs/apiary.apib
         - processed with aglio to stdout
         - passed through bin/parse_apiary.js
         - written to app/views/apidocs/index.html.erb
       - api_docs/* (not apiary.apib)
         - processed with aglio to stdout
         - passed through bin/apiary_section.js
         - written to app/views/apidocs/_${name}.html for erb partial rendering
    
    docker-compose.dev.yml redefines endpoint for the genapiary service to run bin/gen_apiary.sh
    
    bin/parse_apiary.js: node script that
      - takes stdin from aglio output (e.g. html)
      - writes line as-is if it is not a paragraph with render in the text
      - uses htmlparser2 to parse the line if it is a paragraph with render in the text to produce replace the line
        with a div that uses ERB to render the named partial
    
    bin/apiary_section.js: node script that uses cheerio to print only the <section class="resource-group"> entries
    
    api_docs/apiary.apib reformatted to support the use of the render paragraphs to facilitate creation of the resulting index.html.erb
    using aglio and parse_apiary.js
    
    YOU MUST docker-compose build apiary to run this correctly

--- bin/gen_apiary.sh
+++ bin/gen_apiary.sh
@@ -4,12 +4,13 @@ for apifile in `ls -1 api_docs`
 do
   if [ ${apifile} == 'apiary.apib' ]
   then
-    target='index.html'
+    target='index.html.erb'
+    echo "generating ${target}"
+    aglio -i api_docs/${apifile} -o - | bin/parse_apiary.js > app/views/apidocs/${target}
   else
     target=`echo ${apifile} | sed 's/apib/html/'`
+    echo "generating ${target}"
+    aglio -i api_docs/${apifile} -o - | bin/apiary_section.js > app/views/apidocs/_${target}
   fi
-  echo "generating app/views/apidocs/${target}"
-  docker-compose run genapiary -i api_docs/${apifile} -o app/views/apidocs/${target}
 done
-docker-compose down
 echo "FIN"
