920c7dc647f4246ac6fdfd93baec85b964b64d00
Darin London <darin.london@duke.edu>
2017-01-10 10:19:34 -0500

    working toward using ProjectPreviewSerializer in Search::DataFileSerializer

--- spec/serializers/project_preview_serializer_spec.rb
+++ spec/serializers/project_preview_serializer_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe ProjectPreviewSerializer, type: :serializer do
   let(:resource) { FactoryGirl.create(:project) }
   let(:expected_attributes) {{
-    'id' => resource.id
+    'id' => resource.id,
+    'name' => resource.name
   }}
 
   it_behaves_like 'a json serializer' do
