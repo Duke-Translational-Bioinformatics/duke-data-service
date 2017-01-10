583afc92e27b327465998476e71bfef63cc7ad7b
Darin London <darin.london@duke.edu>
2017-01-10 10:08:48 -0500

    removed unused let variable

--- spec/serializers/data_file_serializer_spec.rb
+++ spec/serializers/data_file_serializer_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe DataFileSerializer, type: :serializer do
-  let(:root_file) { FactoryGirl.create(:data_file, :root) }
   let(:child_file) { FactoryGirl.create(:data_file, :with_parent) }
   it_behaves_like 'a serialized DataFile', :child_file
 end
