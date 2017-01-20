378029d48aa93c62e067194e4a9db994e966f800
Darrin Mann <darrin.mann@duke.edu>
2017-01-20 11:58:59 -0500

    Setting puma worker_timeout via ENV["WORKER_TIMEOUT"]

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
+worker_timeout = Integer(ENV['WORKER_TIMEOUT']) if ENV['WORKER_TIMEOUT']
 
 preload_app!
 
