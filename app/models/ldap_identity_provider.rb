9f3c4cd36099cfcfb1d222dd0b6d18123f89d29d
Darin London <darin.london@duke.edu>
2017-01-26 09:31:15 -0500

    paired down the code for affiliates

--- app/models/ldap_identity_provider.rb
+++ app/models/ldap_identity_provider.rb
@@ -3,35 +3,27 @@ require 'net/ldap'
 class LdapIdentityProvider < IdentityProvider
   validates :ldap_base, presence: true
 
-  def affiliates(auth_provider, full_name_contains=nil)
-    users = []
-    return users unless full_name_contains && full_name_contains.length > 3
-    ldap = Net::LDAP.new(
+  def affiliates(authentication_service, full_name_contains=nil)
+    return [] unless full_name_contains && full_name_contains.length > 3
+    Net::LDAP.new(
         host: host,
         port: port,
         base: ldap_base
-    )
-    filter = Net::LDAP::Filter.construct("displayName=*#{full_name_contains}*")
-    entries = ldap.search(
-      filter: filter,
+    ).search(
+      filter: Net::LDAP::Filter.construct("displayName=*#{full_name_contains}*"),
       attributes: %w(uid duDukeID sn givenName mail displayName)
-    )
-    entries.each do |entry|
-      if entry.attribute_names.include?(:uid) #we never want anyone without a uid
-        user = User.new(
-          username: entry.uid.first,
-          first_name: entry.givenName.first,
-          last_name: entry.sn.first,
-          email: entry.mail.first,
-          display_name: entry.displayName.first
-        )
-        user.user_authentication_services.build(
-          uid: user.username,
-          authentication_service: auth_provider
-        )
-        users << user
-      end
-    end
-    users
+    ).reject {|entry| !entry.attribute_names.include?(:uid)}.map { |entry|
+      User.new(
+        username: entry.uid.first,
+        first_name: entry.givenName.first,
+        last_name: entry.sn.first,
+        email: entry.mail.first,
+        display_name: entry.displayName.first,
+        user_authentication_services_attributes: [{
+          uid: entry.uid.first,
+          authentication_service: authentication_service
+        }] #http://billpatrianakos.me/blog/2013/09/29/rails-tricky-error-no-implicit-conversion-from-symbol-to-integer/
+      )
+    }
   end
 end
