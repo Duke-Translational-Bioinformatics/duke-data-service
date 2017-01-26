b663fb971187f65d5b6b96b1d1eb180a483fa847
Darin London <darin.london@duke.edu>
2017-01-26 10:01:11 -0500

    implemented affiliate(authentication_service, uid) refactored to share ldap_search among both affiliates and affiliate

--- app/models/ldap_identity_provider.rb
+++ app/models/ldap_identity_provider.rb
@@ -3,14 +3,28 @@ require 'net/ldap'
 class LdapIdentityProvider < IdentityProvider
   validates :ldap_base, presence: true
 
+  def affiliate(authentication_service, uid)
+    ldap_search(
+      authentication_service,
+      Net::LDAP::Filter.construct("uid=#{uid}")
+    ).first
+  end
+
   def affiliates(authentication_service, full_name_contains=nil)
     return [] unless full_name_contains && full_name_contains.length > 3
+    ldap_search(
+      authentication_service,
+      Net::LDAP::Filter.construct("displayName=*#{full_name_contains}*")
+    )
+  end
+
+  def ldap_search(authentication_service, ldap_filter)
     Net::LDAP.new(
         host: host,
         port: port,
         base: ldap_base
     ).search(
-      filter: Net::LDAP::Filter.construct("displayName=*#{full_name_contains}*"),
+      filter: ldap_filter,
       attributes: %w(uid duDukeID sn givenName mail displayName)
     ).reject {|entry| !entry.attribute_names.include?(:uid)}.map { |entry|
       User.new(
