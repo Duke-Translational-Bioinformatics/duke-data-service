76ed808236233ab4bdf19e4a4f27ccc79f6840e8
Darin London <darin.london@duke.edu>
2017-01-27 14:13:59 -0500

    speling

--- app/serializers/authentication_service_preview_serializer.rb
+++ app/serializers/authentication_service_preview_serializer.rb
@@ -1,7 +1,7 @@
 class AuthenticationServicePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :name, :affilate_search_supported
+  attributes :id, :name, :is_affiliate_search_supported
 
-  def affiliate_search_supported
+  def is_affiliate_search_supported
     object.identity_provider ? true : false
   end
 end
