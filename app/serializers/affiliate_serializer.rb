d18e81c04d7156156a1dc732c850f9f693ae627a
Darin London <darin.london@duke.edu>
2017-01-31 10:17:10 -0500

    AffiliateAuthenticationServiceSerializer is similar to the AuthenticationServicePreviewSerializer, but does not include the supports_identity_search field

--- app/serializers/affiliate_serializer.rb
+++ app/serializers/affiliate_serializer.rb
@@ -9,12 +9,12 @@ class AffiliateSerializer < ActiveModel::Serializer
   def uid
     object.username
   end
-  
+
   def full_name
     object.display_name
   end
 
   def auth_provider
-    AuthenticationServicePreviewSerializer.new(object.user_authentication_services.first.authentication_service)
+    AffiliateAuthenticationServiceSerializer.new(object.user_authentication_services.first.authentication_service)
   end
 end
