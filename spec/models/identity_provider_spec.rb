26ee07bd9bf39613ce02d08abbfc8117f0c43df3
Darin London <darin.london@duke.edu>
2017-02-06 17:13:02 -0500

    uses standard for naming instance metthod tests, implements interface methods that raise NotImplemented

--- spec/models/identity_provider_spec.rb
+++ spec/models/identity_provider_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe IdentityProvider, type: :model do
     it { is_expected.to validate_presence_of :host }
     it { is_expected.to validate_presence_of :port }
   end
+
+  describe 'interface' do
+    describe '#affiliates' do
+      it {
+        is_expected.to respond_to(:affiliates)
+        expect{ subject.affiliates }.to raise_error(NotImplementedError)
+        expect{ subject.affiliates('foo') }.to raise_error(NotImplementedError)
+      }
+    end
+
+    describe '#affiliate' do
+      it {
+        is_expected.to respond_to(:affiliate).with(1).argument
+        expect{ subject.affiliate('foo') }.to raise_error(NotImplementedError)
+      }
+    end
+  end
 end
