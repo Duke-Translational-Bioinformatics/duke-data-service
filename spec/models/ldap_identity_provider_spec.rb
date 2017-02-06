277af5037370329db5dc3fd495d72a497e4e6c13
Darin London <darin.london@duke.edu>
2017-02-06 17:13:39 -0500

    ldap_search is private asserts that LdapIdentityProvider is_an IdentityProvider no longer needs to test that it throws argument errors when calling affiliate without an argument use standard naming convention for tests of instance methods

--- spec/models/ldap_identity_provider_spec.rb
+++ spec/models/ldap_identity_provider_spec.rb
@@ -6,37 +6,26 @@ RSpec.describe LdapIdentityProvider, type: :model do
   let(:auth_provider) { FactoryGirl.create(:openid_authentication_service, :with_ldap_identity_provider) }
   let(:test_user) { FactoryGirl.attributes_for(:user) }
 
+  it { is_expected.to be_an IdentityProvider }
+
   describe 'validations' do
     it { is_expected.to validate_presence_of :ldap_base }
   end
 
-  describe 'affiliate' do
-    it { is_expected.to respond_to(:affiliate) }
-    context 'without uid' do
-      it {
-        expect{
-          subject.affiliate
-        }.to raise_error(ArgumentError)
-      }
-    end
+  describe '#affiliate' do
+    let(:ldap_returns) { [test_user] }
+    include_context 'mocked ldap', returns: :ldap_returns
+    subject { auth_provider.identity_provider.affiliate(test_user[:username]) }
 
-    context 'with uid and authentication_service' do
-      let(:ldap_returns) { [test_user] }
-      include_context 'mocked ldap', returns: :ldap_returns
-      subject { auth_provider.identity_provider.affiliate(test_user[:username]) }
-
-      it {
-        is_expected.to be
-        is_expected.to be_a User
-        is_expected.not_to be_persisted
-        expect(subject.display_name).to eq test_user[:display_name]
-      }
-    end
+    it {
+      is_expected.to be
+      is_expected.to be_a User
+      is_expected.not_to be_persisted
+      expect(subject.display_name).to eq test_user[:display_name]
+    }
   end
 
-  describe 'affiliates' do
-    it { is_expected.to respond_to(:affiliates) }
-
+  describe '#affiliates' do
     context 'full_name_contains' do
       context 'not provided' do
         subject { auth_provider.identity_provider.affiliates }
