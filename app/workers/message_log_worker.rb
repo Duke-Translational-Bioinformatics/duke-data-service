2d7edf7ac7c575bff7ca265784fc0602a7a93041
Darrin Mann <darrin.mann@duke.edu>
2017-03-03 15:24:38 -0500

    Expecting message_log queue and respond to work_with_params

--- app/workers/message_log_worker.rb
+++ app/workers/message_log_worker.rb
@@ -1,3 +1,7 @@
 class MessageLogWorker
   include Sneakers::Worker
+  from_queue 'message_log'
+
+  def work_with_params(msg, delivery_info, metadata)
+  end
 end
--- spec/workers/message_log_worker_spec.rb
+++ spec/workers/message_log_worker_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe MessageLogWorker do
+  let(:gateway_exchange_name) { Sneakers::CONFIG[:exchange] }
+  let(:queue_name) { 'message_log' }
+
   it { expect(described_class).to include(Sneakers::Worker) }
+  it { expect(subject.queue.name).to eq(queue_name) }
+  it { expect(subject.opts[:exchange]).to eq(gateway_exchange_name) }
+  it { expect(subject.opts[:exchange_options][:type]).to eq(:fanout) }
+  it { expect(subject.opts[:exchange_options][:durable]).to be_truthy }
+
+  it { is_expected.not_to respond_to(:work) }
+  it { is_expected.to respond_to(:work_with_params).with(3).arguments }
+
+  describe '#work_with_params' do
+  end
 end
