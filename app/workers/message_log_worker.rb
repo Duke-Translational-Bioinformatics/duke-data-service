2b4506817a903be6e13f6075b164c7d692f4fe4d
Darrin Mann <darrin.mann@duke.edu>
2017-03-07 11:49:09 -0500

    MessageLogWorker#work_with_params sends serialized parameters to Rails.logger.info

--- app/workers/message_log_worker.rb
+++ app/workers/message_log_worker.rb
@@ -3,5 +3,13 @@ class MessageLogWorker
   from_queue 'message_log'
 
   def work_with_params(msg, delivery_info, metadata)
+    Rails.logger.info({
+      MESSAGE_LOG: {
+        message: msg,
+        delivery_info: delivery_info,
+        metadata: metadata
+      }
+    }.to_json)
+    ack!
   end
 end
--- spec/workers/message_log_worker_spec.rb
+++ spec/workers/message_log_worker_spec.rb
@@ -14,5 +14,27 @@ RSpec.describe MessageLogWorker do
   it { is_expected.to respond_to(:work_with_params).with(3).arguments }
 
   describe '#work_with_params' do
+    let(:message) { Faker::Lorem.words(5) }
+    let(:delivery_info) { {exchange: Faker::Internet.slug, id: Faker::Number.digit} }
+    let(:metadata) { :baz }
+    let(:method_call) { subject.work_with_params(message, delivery_info, metadata) }
+    let(:ack) { subject.ack! }
+    it { expect(ack).not_to be_nil }
+    it { expect(method_call).to eq ack }
+
+    context 'calls Rails.logger' do
+      let(:log_message) {{
+        MESSAGE_LOG: {
+          message: message,
+          delivery_info: delivery_info,
+          metadata: metadata
+        }
+      }.to_json}
+      before do
+        allow(Rails.logger).to receive(:info)
+        expect{method_call}.not_to raise_error
+      end
+      it { expect(Rails.logger).to have_received(:info).with(log_message) }
+    end
   end
 end
