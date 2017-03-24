5075566bac67d336dfee9ce4b9910032b7317c03
Darin London <darin.london@duke.edu>
2017-03-24 12:11:33 -0400

    folder needed a child now that manage_children only submits ChilDeletionJobs for objects with children

--- spec/jobs/child_deletion_job_spec.rb
+++ spec/jobs/child_deletion_job_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ChildDeletionJob, type: :job do
+  before do
+    expect(folder_child).to be_persisted
+  end
   context 'project' do
     let(:project) { FactoryGirl.create(:project) }
     let(:root_folder) { FactoryGirl.create(:folder, :root, project: project) }
+    let(:folder_child) { FactoryGirl.create(:data_file, parent: root_folder) }
     let(:root_file) { FactoryGirl.create(:data_file, :root, project: project) }
 
     it_behaves_like 'a ChildDeletionJob', :project, :root_folder, :root_file
@@ -12,6 +16,7 @@ RSpec.describe ChildDeletionJob, type: :job do
   context 'folder' do
     let(:folder) { FactoryGirl.create(:folder) }
     let(:sub_folder) { FactoryGirl.create(:folder, parent: folder) }
+    let(:folder_child) { FactoryGirl.create(:data_file, parent: sub_folder) }
     let(:sub_file) { FactoryGirl.create(:data_file, parent: folder) }
 
     it_behaves_like 'a ChildDeletionJob', :folder, :sub_folder, :sub_file
