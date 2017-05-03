b61363bec8df5f100ffe05d4dfbb82c495ce8245
Darin London <darin.london@duke.edu>
2017-05-03 18:22:17 -0400

    refactored to serialize search documents according to DDS-826 api_design specification

--- spec/serializers/search/folder_serializer_spec.rb
+++ spec/serializers/search/folder_serializer_spec.rb
@@ -1,28 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Search::FolderSerializer, type: :serializer do
-  let(:extra_searchable_attributes) {{
-    'created_at' => folder.created_at.as_json,
-    'updated_at' => folder.updated_at.as_json,
-    'label' => folder.label
-  }}
 
-  context 'with_parent' do
-    let(:folder) { FactoryGirl.create(:folder, :with_parent) }
-    it_behaves_like 'a serialized Folder', :folder, with_parent: true do
-      it_behaves_like 'a has_one association with', :creator, Search::UserSummarySerializer
-      it_behaves_like 'a json serializer' do
-        it { is_expected.to include(extra_searchable_attributes) }
-      end
-    end
-  end
+  let(:folder) { FactoryGirl.create(:folder) }
+  it_behaves_like 'a serialized Folder', :folder do
+    it_behaves_like 'a json serializer'
 
-  context 'without_parent' do
-    let(:folder) { FactoryGirl.create(:folder, :root) }
-    it_behaves_like 'a serialized Folder', :folder do
-      it_behaves_like 'a has_one association with', :creator, Search::UserSummarySerializer
+    context 'with meta_templates' do
+      let(:meta_template) { FactoryGirl.create(:meta_template, templatable: data_file) }
+      let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
+      let(:meta_property){ FactoryGirl.create(:meta_property, meta_template: meta_template, property: property) }
+      include_context 'elasticsearch prep', [:meta_template, :property, :meta_property], [:data_file]
+
       it_behaves_like 'a json serializer' do
-        it { is_expected.to include(extra_searchable_attributes) }
+        it_behaves_like 'a metadata annotated document'
       end
     end
   end
