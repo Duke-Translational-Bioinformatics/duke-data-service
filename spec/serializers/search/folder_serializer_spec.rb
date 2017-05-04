ea77c98e05ea07183a5471353e13740d21a5df40
Darin London <darin.london@duke.edu>
2017-05-04 11:41:57 -0400

    Folders are not currently meta_template templatable.

--- spec/serializers/search/folder_serializer_spec.rb
+++ spec/serializers/search/folder_serializer_spec.rb
@@ -5,16 +5,5 @@ RSpec.describe Search::FolderSerializer, type: :serializer do
   let(:folder) { FactoryGirl.create(:folder) }
   it_behaves_like 'a serialized Folder', :folder do
     it_behaves_like 'a json serializer'
-
-    context 'with meta_templates' do
-      let(:meta_template) { FactoryGirl.create(:meta_template, templatable: data_file) }
-      let(:property) { FactoryGirl.create(:property, template: meta_template.template) }
-      let(:meta_property){ FactoryGirl.create(:meta_property, meta_template: meta_template, property: property) }
-      include_context 'elasticsearch prep', [:meta_template, :property, :meta_property], [:data_file]
-
-      it_behaves_like 'a json serializer' do
-        it_behaves_like 'a metadata annotated document'
-      end
-    end
   end
 end
