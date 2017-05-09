9f1b8658b79e0c3617fe26ccbd63a39e126f1df3
Darrin Mann <darrin.mann@duke.edu>
2017-05-09 12:05:27 -0400

    Organize method specific tests in describe blocks

--- spec/support/search_shared_examples.rb
+++ spec/support/search_shared_examples.rb
@@ -93,35 +93,40 @@ shared_examples 'an Elasticsearch::Model' do |resource_search_serializer_sym: :s
   it { expect(described_class).to include(Elasticsearch::Model) }
   it { expect(described_class).not_to include(Elasticsearch::Model::Callbacks) }
 
-  it { is_expected.to respond_to(:create_elasticsearch_index) }
-  it { is_expected.to respond_to(:update_elasticsearch_index) }
+  describe '#create_elasticsearch_index' do
+    it { is_expected.to respond_to(:create_elasticsearch_index) }
+    it {
+      is_expected.to callback(:create_elasticsearch_index).after(:create)
+    }
+    it {
+      expect(ElasticsearchIndexJob).to receive(:initialize_job).with(
+        subject
+      ).and_return(job_transaction)
+      expect {
+        subject.create_elasticsearch_index
+      }.to have_enqueued_job(ElasticsearchIndexJob).with(job_transaction, subject)
+    }
+  end
 
-  it {
-    is_expected.to callback(:create_elasticsearch_index).after(:create)
-  }
-  it {
-    is_expected.to callback(:update_elasticsearch_index).after(:update)
-  }
+  describe '#update_elasticsearch_index' do
+    it { is_expected.to respond_to(:update_elasticsearch_index) }
+    it {
+      is_expected.to callback(:update_elasticsearch_index).after(:update)
+    }
+    it {
+      expect(ElasticsearchIndexJob).to receive(:initialize_job).with(
+        subject
+      ).and_return(job_transaction)
+      expect {
+        subject.update_elasticsearch_index
+      }.to have_enqueued_job(ElasticsearchIndexJob).with(job_transaction, subject, update: true)
+    }
+  end
 
-  it { is_expected.to respond_to 'as_indexed_json' }
-  it { expect(subject.as_indexed_json).to eq(resource_search_serializer.new(subject).as_json) }
-
-  it {
-    expect(ElasticsearchIndexJob).to receive(:initialize_job).with(
-      subject
-    ).and_return(job_transaction)
-    expect {
-      subject.create_elasticsearch_index
-    }.to have_enqueued_job(ElasticsearchIndexJob).with(job_transaction, subject)
-  }
-  it {
-    expect(ElasticsearchIndexJob).to receive(:initialize_job).with(
-      subject
-    ).and_return(job_transaction)
-    expect {
-      subject.update_elasticsearch_index
-    }.to have_enqueued_job(ElasticsearchIndexJob).with(job_transaction, subject, update: true)
-  }
+  describe '#as_indexed_json' do
+    it { is_expected.to respond_to 'as_indexed_json' }
+    it { expect(subject.as_indexed_json).to eq(resource_search_serializer.new(subject).as_json) }
+  end
 end
 
 shared_examples 'an Elasticsearch index mapping model' do |expected_property_mappings_sym: :property_mappings|
