5d8618135a6ae8acecad8bac0145e545fdc584a6
Darrin Mann <darrin.mann@duke.edu>
2017-05-19 13:49:16 -0400

    shared_examples to test elasticsearch indexing

--- spec/support/search_shared_examples.rb
+++ spec/support/search_shared_examples.rb
@@ -148,3 +148,33 @@ shared_examples 'an Elasticsearch index mapping model' do |expected_property_map
     end
   }
 end
+
+shared_examples 'an elasticsearch indexer' do |method_sym: :method|
+  let(:elasticsearch_client) { Elasticsearch::Model.client }
+  let(:existing_documents) { elasticsearch_client.search["hits"]["hits"] }
+  let(:new_documents) { elasticsearch_client.search["hits"]["hits"] - existing_documents }
+  before do
+    expect{ existing_documents }.not_to raise_error
+    expect{ send(method_sym) }.not_to raise_error
+    expect{ elasticsearch_client.indices.flush }.not_to raise_error
+    expect{ new_documents }.not_to raise_error
+    expect(new_documents).not_to be_empty
+  end
+  after do
+    new_documents.each do |d|
+      elasticsearch_client.delete(
+        id: d["_id"],
+        index: d["_index"],
+        type: d["_type"]
+      )
+    end
+  end
+end
+
+shared_context 'with a single document indexed' do
+  let(:document) { new_documents.first }
+  before do
+    expect(new_documents.length).to eq(1)
+    expect(document).not_to be_nil
+  end
+end
