13500dc962c0c96132db60b7051826cbc7b674c5
Darin London <darin.london@duke.edu>
2017-05-23 10:14:46 -0400

    reverted to previous version to retaion deprecated functionality for the /search endpoint. These objects will be renamed 'DeprecatedElasticsearchResponse' in a subsequent commit to ease cleanup when we finally remove the endpoint.

--- app/serializers/elasticsearch_response_serializer.rb
+++ app/serializers/elasticsearch_response_serializer.rb
@@ -1,8 +1,3 @@
 class ElasticsearchResponseSerializer < ActiveModel::Serializer
-  attributes :results, :aggs
-
-  def filter(keys)
-    keys.delete(:aggs) if object.aggs.nil?
-    keys
-  end
+  has_many :results
 end
