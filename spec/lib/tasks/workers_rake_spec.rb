7ccfae18e355227db1a13cd0780e4280a6d2cc6b
Darin London <darin.london@duke.edu>
2017-06-22 12:34:11 -0400

    removing redundant tests

--- spec/lib/tasks/workers_rake_spec.rb
+++ spec/lib/tasks/workers_rake_spec.rb
@@ -3,51 +3,6 @@ require 'sneakers/runner'
 Dir[Rails.root.join('app/jobs/*.rb')].each { |f| require f }
 
 describe "workers" do
-  describe 'workers:message_logger:run' do
-    include_context "rake"
-    let(:task_name) { "workers:message_logger:run" }
-    let(:expected_job_class) { MessageLogWorker }
-
-    it { expect(subject.prerequisites).to  include("environment") }
-    it_behaves_like 'a queued worker', :expected_job_class
-  end
-
-  describe 'workers:initialize_project_storage:run' do
-    include_context "rake"
-    let(:task_name) { "workers:initialize_project_storage:run" }
-    let(:expected_job_class) { ProjectStorageProviderInitializationJob }
-
-    it { expect(subject.prerequisites).to  include("environment") }
-    it_behaves_like 'a queued job worker', :expected_job_class
-  end
-
-  describe 'workers:delete_children:run' do
-    include_context "rake"
-    let(:task_name) { "workers:delete_children:run" }
-    let(:expected_job_class) { ChildDeletionJob }
-
-    it { expect(subject.prerequisites).to  include("environment") }
-    it_behaves_like 'a queued job worker', :expected_job_class
-  end
-
-  describe 'workers:index_documents:run' do
-    include_context "rake"
-    let(:task_name) { "workers:index_documents:run" }
-    let(:expected_job_class) { ElasticsearchIndexJob }
-
-    it { expect(subject.prerequisites).to  include("environment") }
-    it_behaves_like 'a queued job worker', :expected_job_class
-  end
-
-  describe 'workers:complete_upload:run' do
-    include_context "rake"
-    let(:task_name) { "workers:complete_upload:run" }
-    let(:expected_job_class) { UploadCompletionJob }
-
-    it { expect(subject.prerequisites).to  include("environment") }
-    it_behaves_like 'a queued job worker', :expected_job_class
-  end
-
   JobsRunner.workers_registry.each do |worker_key, worker_class|
     describe "workers:#{worker_key}:run" do
       include_context "rake"
