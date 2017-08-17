25d7293c0a6619aa29ce4fc28ede5aa7d53c3df0
Darrin Mann <darrin.mann@duke.edu>
2017-08-17 17:10:06 -0400

    Users can update/delete their own project permissions when another project_admin exists for the project

--- spec/policies/project_permission_policy_spec.rb
+++ spec/policies/project_permission_policy_spec.rb
@@ -14,6 +14,28 @@ describe ProjectPermissionPolicy do
   it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :index?, :show?], on: :project_permission
   it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?], on: :project_permission
 
+  context 'when another :manage_project_permissions exists' do
+    let!(:another_project_admin) { FactoryGirl.create(:project_permission, :project_admin, project: project_permission.project) }
+
+    it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :index?, :show?], on: :project_permission
+    it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?, :update?, :destroy?], on: :project_permission
+  end
+
+  context 'when a non manager exists' do
+    let(:non_admin_role) { FactoryGirl.create(:auth_role, without_permissions: [:manage_project_permissions]) }
+    let!(:a_non_admin) { FactoryGirl.create(:project_permission, auth_role: non_admin_role, project: project_permission.project) }
+
+    it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :index?, :show?], on: :project_permission
+    it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?], on: :project_permission
+  end
+
+  context 'when a :manage_project_permissions exists in different project' do
+    let!(:another_project_admin) { FactoryGirl.create(:project_permission, :project_admin) }
+
+    it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :index?, :show?], on: :project_permission
+    it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?], on: :project_permission
+  end
+
   it_behaves_like 'a user with project_permission', :view_project, allows: [:scope, :index?, :show?], on: :other_users_project_permission
   it_behaves_like 'a user with project_permission', :manage_project_permissions, allows: [:create?, :update?, :destroy?], on: :other_users_project_permission
 
