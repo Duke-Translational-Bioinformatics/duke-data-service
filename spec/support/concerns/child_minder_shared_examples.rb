b3411181a4b4ff5f46352c787ac04fc33ac9b191
Darin London <darin.london@duke.edu>
2017-08-30 16:08:46 -0400

    factored out shared_contexts that are common to both types of ChildMinders

--- spec/support/concerns/child_minder_shared_examples.rb
+++ spec/support/concerns/child_minder_shared_examples.rb
@@ -1,11 +1,10 @@
-shared_examples 'a ChildMinder' do |resource_factory,
+shared_context 'any ChildMinder' do |resource_factory,
   valid_child_file_sym,
   invalid_child_file_sym,
   child_folder_sym|
   let(:valid_child_file) { send(valid_child_file_sym) }
   let(:invalid_child_file) { send(invalid_child_file_sym) }
   let(:child_folder) { send(child_folder_sym) }
-  include_context 'with job runner', ChildPurgationJob
 
   it {
     expect(described_class).to include(ChildMinder)
@@ -35,28 +34,82 @@ shared_examples 'a ChildMinder' do |resource_factory,
     end
   end
 
-  describe '#manage_children' do
+  describe '#purge_children' do
+    it { is_expected.not_to respond_to(:purge_children).with(0).arguments }
+    it { is_expected.to respond_to(:purge_children).with(1).argument }
+    context 'called', :vcr do
+      let(:job_transaction) { ChildPurgationJob.initialize_job(subject) }
+      let(:child_job_transaction) { ChildPurgationJob.initialize_job(child_folder) }
+      let(:child_folder_file) { FactoryGirl.create(:data_file, parent: child_folder)}
+      let(:page) { 1 }
+
+      before do
+        expect(child_folder).to be_persisted
+        expect(child_folder_file).to be_persisted
+        @old_max = Rails.application.config.max_children_per_job
+        Rails.application.config.max_children_per_job = subject.children.count + child_folder.children.count
+      end
+
+      after do
+        Rails.application.config.max_children_per_job = @old_max
+      end
+
+      it {
+        expect(child_folder.is_deleted?).to be_falsey
+        expect(child_folder.is_purged?).to be_falsey
+        expect(valid_child_file.is_deleted?).to be_falsey
+        expect(valid_child_file.is_purged?).to be_falsey
+        subject.current_transaction = job_transaction
+        expect(ChildPurgationJob).to receive(:initialize_job)
+          .with(child_folder)
+          .and_return(child_job_transaction)
+        expect(ChildPurgationJob).to receive(:perform_later)
+          .with(child_job_transaction, child_folder, page).and_call_original
+        subject.purge_children(page)
+        expect(child_folder.reload).to be_truthy
+        expect(child_folder.is_deleted?).to be_truthy
+        expect(child_folder.is_purged?).to be_truthy
+        expect(valid_child_file.reload).to be_truthy
+        expect(valid_child_file.is_deleted?).to be_truthy
+        expect(valid_child_file.is_purged?).to be_truthy
+      }
+    end
+  end
+end
+
+shared_context 'common manage_children expectations' do
+  it {
+    is_expected.to respond_to(:manage_children)
+  }
+
+  describe 'callbacks' do
     it {
-      is_expected.to respond_to(:manage_children)
+      is_expected.to callback(:manage_children).around(:update)
     }
+  end
 
-    describe 'callbacks' do
-      it {
-        is_expected.to callback(:manage_children).around(:update)
-      }
-    end
+  context 'when is_deleted not changed' do
+    it {
+      expect(subject.is_deleted_changed?).to be_falsey
+      yield_called = false
+      expect(ChildPurgationJob).not_to receive(:perform_later)
+      subject.manage_children do
+        yield_called = true
+      end
+      expect(yield_called).to be_truthy
+    }
+  end
+end
 
-    context 'when is_deleted not changed' do
-      it {
-        expect(subject.is_deleted_changed?).to be_falsey
-        yield_called = false
-        expect(ChildPurgationJob).not_to receive(:perform_later)
-        subject.manage_children do
-          yield_called = true
-        end
-        expect(yield_called).to be_truthy
-      }
-    end
+shared_examples 'a ChildMinder' do |resource_factory,
+  valid_child_file_sym,
+  invalid_child_file_sym,
+  child_folder_sym|
+  include_context 'with job runner', ChildPurgationJob
+  include_context 'any ChildMinder', resource_factory, valid_child_file_sym, invalid_child_file_sym, child_folder_sym
+
+  describe '#manage_children' do
+    include_context 'common manage_children expectations'
 
     context 'when is_deleted changed from false to true' do
       context 'has_children? true' do
@@ -135,97 +188,17 @@ shared_examples 'a ChildMinder' do |resource_factory,
       }
     end
   end
-
-  describe '#purge_children' do
-    it { is_expected.not_to respond_to(:purge_children).with(0).arguments }
-    it { is_expected.to respond_to(:purge_children).with(1).argument }
-    context 'called', :vcr do
-      let(:job_transaction) { ChildPurgationJob.initialize_job(subject) }
-      let(:child_job_transaction) { ChildPurgationJob.initialize_job(child_folder) }
-      let(:child_folder_file) { FactoryGirl.create(:data_file, parent: child_folder)}
-      let(:page) { 1 }
-
-      before do
-        expect(child_folder).to be_persisted
-        expect(child_folder_file).to be_persisted
-        @old_max = Rails.application.config.max_children_per_job
-        Rails.application.config.max_children_per_job = subject.children.count + child_folder.children.count
-      end
-
-      after do
-        Rails.application.config.max_children_per_job = @old_max
-      end
-
-      it {
-        expect(child_folder.is_deleted?).to be_falsey
-        expect(child_folder.is_purged?).to be_falsey
-        expect(valid_child_file.is_deleted?).to be_falsey
-        expect(valid_child_file.is_purged?).to be_falsey
-        subject.current_transaction = job_transaction
-        expect(ChildPurgationJob).to receive(:initialize_job)
-          .with(child_folder)
-          .and_return(child_job_transaction)
-        expect(ChildPurgationJob).to receive(:perform_later)
-          .with(child_job_transaction, child_folder, page).and_call_original
-        subject.purge_children(page)
-        expect(child_folder.reload).to be_truthy
-        expect(child_folder.is_deleted?).to be_truthy
-        expect(child_folder.is_purged?).to be_truthy
-        expect(valid_child_file.reload).to be_truthy
-        expect(valid_child_file.is_deleted?).to be_truthy
-        expect(valid_child_file.is_purged?).to be_truthy
-      }
-    end
-  end
 end
 
 shared_examples 'a Trashable ChildMinder' do |resource_factory,
  valid_child_file_sym,
  invalid_child_file_sym,
  child_folder_sym|
- let(:valid_child_file) { send(valid_child_file_sym) }
- let(:invalid_child_file) { send(invalid_child_file_sym) }
- let(:child_folder) { send(child_folder_sym) }
  include_context 'with job runner', ChildDeletionJob
-
- it {
-   expect(described_class).to include(ChildMinder)
-   is_expected.to respond_to(:children)
- }
-
- describe '#has_children?' do
-   it { is_expected.to respond_to(:has_children?) }
-
-   context 'without children' do
-     subject { FactoryGirl.create(resource_factory) }
-     it { expect(subject.children.count).to eq(0) }
-     it { expect(subject.has_children?).to be_falsey }
-   end
-
-   context 'with children' do
-     before do
-       expect(child_folder).to be_persisted
-       expect(child_folder.is_deleted?).to be_falsey
-       expect(valid_child_file).to be_persisted
-       expect(valid_child_file.is_deleted?).to be_falsey
-       expect(invalid_child_file).to be_persisted
-       expect(invalid_child_file.is_deleted?).to be_falsey
-     end
-     it { expect(subject.children.count).to be > 0 }
-     it { expect(subject.has_children?).to be_truthy }
-   end
- end
+ include_context 'any ChildMinder', resource_factory, valid_child_file_sym, invalid_child_file_sym, child_folder_sym
 
  describe '#manage_children' do
-   it {
-     is_expected.to respond_to(:manage_children)
-   }
-
-   describe 'callbacks' do
-     it {
-       is_expected.to callback(:manage_children).around(:update)
-     }
-   end
+   include_context 'common manage_children expectations'
 
    context 'when is_deleted, is_purged not changed' do
      it {
@@ -501,48 +474,6 @@ shared_examples 'a Trashable ChildMinder' do |resource_factory,
        expect(child_folder.is_deleted?).to be_falsey
        expect(valid_child_file.reload).to be_truthy
        expect(valid_child_file.is_deleted?).to be_falsey
-     }
-   end
- end
-
- describe '#purge_children' do
-   it { is_expected.not_to respond_to(:purge_children).with(0).arguments }
-   it { is_expected.to respond_to(:purge_children).with(1).argument }
-   context 'called', :vcr do
-     let(:job_transaction) { ChildPurgationJob.initialize_job(subject) }
-     let(:child_job_transaction) { ChildPurgationJob.initialize_job(child_folder) }
-     let(:child_folder_file) { FactoryGirl.create(:data_file, parent: child_folder)}
-     let(:page) { 1 }
-
-     before do
-       expect(child_folder).to be_persisted
-       expect(child_folder_file).to be_persisted
-       @old_max = Rails.application.config.max_children_per_job
-       Rails.application.config.max_children_per_job = subject.children.count + child_folder.children.count
-     end
-
-     after do
-       Rails.application.config.max_children_per_job = @old_max
-     end
-
-     it {
-       expect(child_folder.is_deleted?).to be_falsey
-       expect(child_folder.is_purged?).to be_falsey
-       expect(valid_child_file.is_deleted?).to be_falsey
-       expect(valid_child_file.is_purged?).to be_falsey
-       subject.current_transaction = job_transaction
-       expect(ChildPurgationJob).to receive(:initialize_job)
-         .with(child_folder)
-         .and_return(child_job_transaction)
-       expect(ChildPurgationJob).to receive(:perform_later)
-         .with(child_job_transaction, child_folder, page).and_call_original
-       subject.purge_children(page)
-       expect(child_folder.reload).to be_truthy
-       expect(child_folder.is_deleted?).to be_truthy
-       expect(child_folder.is_purged?).to be_truthy
-       expect(valid_child_file.reload).to be_truthy
-       expect(valid_child_file.is_deleted?).to be_truthy
-       expect(valid_child_file.is_purged?).to be_truthy
      }
    end
  end
