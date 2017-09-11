e2b46e2779cdb340ecac800a076ba0944e15d965
Darin London <darin.london@duke.edu>
2017-09-11 17:02:50 -0400

    storage_provider now supports   - chunk_max_number: integer   - chunk_max_size_bytes:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622204323) do
+ActiveRecord::Schema.define(version: 20170911204850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -264,6 +264,8 @@ ActiveRecord::Schema.define(version: 20170622204323) do
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
     t.string   "chunk_hash_algorithm", default: "md5"
+    t.integer  "chunk_max_number"
+    t.integer  "chunk_max_size_bytes"
   end
 
   create_table "system_permissions", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
