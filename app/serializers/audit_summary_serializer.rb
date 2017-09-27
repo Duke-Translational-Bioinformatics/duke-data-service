56ffdf5933e4d27811da77f1c72412d69df9bef5
Darrin Mann <darrin.mann@duke.edu>
2017-09-27 14:35:07 -0400

    Refactor audit.user serialization into private #audit_user_info_hash method

--- app/serializers/audit_summary_serializer.rb
+++ app/serializers/audit_summary_serializer.rb
@@ -5,70 +5,19 @@ module AuditSummarySerializer
     object.audits.each do |audit|
       if audit[:action] == "create"
         audit_summary[:created_on] = audit.created_at
-        if audit.user
-          audit_summary[:created_by] = audit.user.audited_user_info
-          if audit&.comment&.has_key?("software_agent_id")
-            creation_agent = SoftwareAgent.find(audit.comment["software_agent_id"])
-            audit_summary[:created_by].merge!(
-              {
-                "agent": {
-                  "id": creation_agent.id,
-                  "name": creation_agent.name
-                }
-              }
-            )
-          end
-        end
+        audit_summary[:created_by] = audit_user_info_hash(audit)
       elsif audit[:action] == "update"
         audit_summary[:last_updated_on] = audit.created_at
-        if audit.user
-          audit_summary[:last_updated_by] = audit.user.audited_user_info
-          if audit&.comment&.has_key?("software_agent_id")
-            creation_agent = SoftwareAgent.find(audit.comment["software_agent_id"])
-            audit_summary[:last_updated_by].merge!(
-              {
-                "agent": {
-                  "id": creation_agent.id,
-                  "name": creation_agent.name
-                }
-              }
-            )
-          end
-        end
+        audit_summary[:last_updated_by] = audit_user_info_hash(audit)
+
         if audit&.comment["action"] == 'DELETE' && 
             object.respond_to?('is_deleted') && object.is_deleted
           audit_summary[:deleted_on] = audit.created_at
-          if audit.user
-            audit_summary[:deleted_by] = audit.user.audited_user_info
-            if audit&.comment&.has_key?("software_agent_id")
-              creation_agent = SoftwareAgent.find(audit.comment["software_agent_id"])
-              audit_summary[:deleted_by].merge!(
-                {
-                  "agent": {
-                    "id": creation_agent.id,
-                    "name": creation_agent.name
-                  }
-                }
-              )
-            end
-          end
+          audit_summary[:deleted_by] = audit_user_info_hash(audit)
         end
       elsif audit[:action] == "destroy"
         audit_summary[:deleted_on] = audit.created_at
-        if audit.user
-          audit_summary[:deleted_by] = audit.user.audited_user_info
-          if audit&.comment&.has_key?("software_agent_id")
-            creation_agent = SoftwareAgent.find(audit.comment["software_agent_id"])
-            audit_summary[:deleted_by].merge!(
-              {
-                "agent": {
-                  "id": creation_agent.id,
-                  "name": creation_agent.name
-                }
-              }
-            )
-          end
-        end
+        audit_summary[:deleted_by] = audit_user_info_hash(audit)
       end
     end
 
@@ -80,5 +29,26 @@ module AuditSummarySerializer
       deleted_on: nil,
       deleted_by: nil
     }.merge(audit_summary)
+  end
+
+private
+
+  def audit_user_info_hash(audit)
+    user_info = nil
+    if audit.user
+      user_info = audit.user.audited_user_info
+      if audit&.comment&.has_key?("software_agent_id")
+        creation_agent = SoftwareAgent.find(audit.comment["software_agent_id"])
+        user_info.merge!(
+          {
+            "agent": {
+              "id": creation_agent.id,
+              "name": creation_agent.name
+            }
+          }
+        )
+      end
+    end
+    user_info
   end
 end
