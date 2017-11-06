aaabf03602e1480985133d20fd615c8b1c461d95
Darrin Mann <darrin.mann@duke.edu>
2017-11-06 15:29:52 -0500

    Fix graph model test misuse of #create_graph_relation

--- spec/models/graph_was_invalidated_by_spec.rb
+++ spec/models/graph_was_invalidated_by_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Graph::WasInvalidatedBy do
-  subject { FactoryGirl.create(:invalidated_by_activity_prov_relation).create_graph_relation }
+  let(:resource) { FactoryGirl.create(:invalidated_by_activity_prov_relation) }
+  before(:example) { resource.create_graph_relation }
+  subject { resource.graph_model_object }
   it_behaves_like 'a graphed model'
 end
