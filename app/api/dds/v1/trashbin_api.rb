092a6c19549241dad3f216d1f6e7e8852b7c9566
Darin London <darin.london@duke.edu>
2017-11-08 14:14:48 -0500

    TrashbinAPISpec expects TrashbinAPI to implement:   - get /api/v1/trashbin/:object_kind/:object_id   - put /api/v1/trashbin/:object_kind/:object_id/restore   - put /api/v1/trashbin/:object_kind/:object_id/purge According to api_design/DDS-829-trash_bin.md TrashbinAPI meets spec

--- app/api/dds/v1/trashbin_api.rb
+++ app/api/dds/v1/trashbin_api.rb
@@ -1,6 +1,129 @@
 module DDS
   module V1
     class TrashbinAPI < Grape::API
+      desc 'View Trashbin Item details' do
+        detail 'Show Details of a Trashbin Item.'
+        named 'show trashbin item'
+        failure [
+          [200, 'Success'],
+          [401, 'Unauthorized'],
+          [403, 'Forbidden'],
+          [404, 'Item Does not Exist'],
+          [404, 'Object kind not supported']
+        ]
+      end
+      params do
+        requires :object_kind, type: String, desc: 'Object Kind'
+        requires :object_id, type: String, desc: 'Object UUID'
+      end
+      get '/trashbin/:object_kind/:object_id', root: false do
+        authenticate!
+        object_kind = KindnessFactory.by_kind(params[:object_kind])
+        trashed_object = object_kind.find_by!(id: params[:object_id], is_deleted: true, is_purged: false)
+        authorize trashed_object, :show?
+        trashed_object
+      end
+
+      desc 'Restore a Trashbin Item' do
+        detail 'Restores the item, and any children, to an undeleted status to the specified parent folder or project.'
+        named 'restore trashbin item'
+        failure [
+          [200, 'Success'],
+          [404, 'Parent object does not exist or is itself in the trashbin'],
+          [401, 'Unauthorized'],
+          [403, 'Forbidden'],
+          [404, 'Object or Parent kind not supported'],
+          [404, 'Object not found in trash bin'],
+          [404, 'Parent object does not exist or is itself in the trashbin']
+        ]
+      end
+      params do
+        requires :object_kind, type: String, desc: 'Object Kind'
+        requires :object_id, type: String, desc: 'Object UUID'
+        optional :parent, desc: "Parent. Required unless restoring a FileVersion", type: Hash do
+          optional :kind, type: String, desc: 'Parent Kind'
+          optional :id, type: String, desc: 'Parent UUID'
+        end
+      end
+      put '/trashbin/:object_kind/:object_id/restore', root: false do
+        authenticate!
+        parent_params = declared(params, {include_missing: false}, [:parent])
+        object_kind = KindnessFactory.by_kind(params[:object_kind])
+        purge_object = object_kind.find_by!(id: params[:object_id], is_deleted: true)
+        unless purge_object.class.include? Restorable
+          error_json = {
+            "error" => "404",
+            "code" => "not_provided",
+            "reason" => "#{purge_object.class.to_s} Not Found",
+            "suggestion" => "#{purge_object.kind} is not Restorable"
+          }
+          error!(error_json, 404)
+        end
+        unless purge_object.is_purged?
+          purge_object.is_deleted = false
+          if purge_object.is_a?(FileVersion)
+            if purge_object.data_file.is_deleted?
+              error_json = {
+                "error" => "404",
+                "code" => "not_provided",
+                "reason" => "DataFile Not Found",
+                "suggestion" => "Restore #{purge_object.data_file.kind} #{purge_object.data_file_id}"
+              }
+              error!(error_json, 404)
+            end
+          else
+            parent_kind = KindnessFactory.by_kind(parent_params[:parent][:kind])
+            purge_object.parent = hide_logically_deleted parent_kind.find(parent_params[:parent][:id])
+          end
+          authorize purge_object, :restore?
+          purge_object.save
+        end
+        purge_object
+      end
+
+      desc 'Purge Trashbin Item' do
+        detail 'Purges the item and any children, and permenantly removes any stored files from the storage_provider. If a FileVersion is restored, the parent is optional, otherwise it is required.'
+        named 'purge trashbin item'
+        failure [
+          [200, 'Successfully Purged'],
+          [401, 'Unauthorized'],
+          [403, 'Forbidden'],
+          [404, 'Item Does not Exist'],
+          [404, 'Object kind not supported']
+        ]
+      end
+      params do
+        requires :object_kind, type: String, desc: 'Object Kind'
+        requires :object_id, type: String, desc: 'Object UUID'
+      end
+      put '/trashbin/:object_kind/:object_id/purge', root: false do
+        authenticate!
+        object_kind = KindnessFactory.by_kind(params[:object_kind])
+        purge_object = object_kind.find_by!(id: params[:object_id], is_deleted: true)
+        unless purge_object.class.include? Restorable
+          error_json = {
+            "error" => "404",
+            "code" => "not_provided",
+            "reason" => "#{purge_object.class.to_s} Not Found",
+            "suggestion" => "#{purge_object.kind} is not Purgable"
+          }
+          error!(error_json, 404)
+        end
+        if purge_object.is_a?(FileVersion)
+          error_json = {
+            "error" => "404",
+            "code" => "not_provided",
+            "reason" => "#{purge_object.class.to_s} Not Found",
+            "suggestion" => "#{purge_object.kind} is not Purgable"
+          }
+          error!(error_json, 404)
+        end
+        authorize purge_object, :destroy?
+        unless purge_object.is_purged?
+          purge_object.update(is_deleted: true, is_purged: true)
+        end
+        body false
+      end
     end
   end
 end
--- spec/requests/trashbin_api_spec.rb
+++ spec/requests/trashbin_api_spec.rb
@@ -9,9 +9,14 @@ describe DDS::V1::TrashbinAPI do
   let(:trashed_resource) { FactoryGirl.create(:data_file, :deleted, project: project, upload: upload) }
   let(:untrashed_resource) { FactoryGirl.create(:data_file, project: project, upload: upload) }
   let(:purged_resource) { FactoryGirl.create(:data_file, :purged, project: project, upload: upload) }
+  let(:project_permission) { FactoryGirl.create(:project_permission, :project_admin, user: current_user, project: project) }
+  let!(:resource_permission) { project_permission }
 
-  describe '/trashbin/{object_kind}/{object_id}' do
+  describe 'GET /trashbin/{object_kind}/{object_id}' do
     let(:url) { "/api/v1/trashbin/#{resource_kind}/#{resource_id}" }
+    let(:resource) { trashed_resource }
+    let(:resource_class) { DataFile }
+    let(:resource_serializer) { DataFileSerializer }
     let(:resource_id) { trashed_resource.id }
     let(:resource_kind) { trashed_resource.kind }
     let(:payload) {{}}
@@ -42,11 +47,14 @@ describe DDS::V1::TrashbinAPI do
   end
 
   describe 'PUT /trashbin/{object_kind}/{object_id}/restore' do
-    let(:url) { "/api/v1/trashbin/#{resource_kind}/#{resource_id}" }
+    let(:url) { "/api/v1/trashbin/#{resource_kind}/#{resource_id}/restore" }
     subject { put(url, params: payload.to_json, headers: headers) }
     let(:called_action) { 'PUT' }
     let(:parent_kind) { parent_folder.kind }
     let(:parent_id) { parent_folder.id }
+    let(:resource) { trashed_resource }
+    let(:resource_class) { DataFile }
+    let(:resource_serializer) { DataFileSerializer }
     let(:resource_kind) { trashed_resource.kind }
     let(:resource_id) { trashed_resource.id }
 
@@ -66,16 +74,14 @@ describe DDS::V1::TrashbinAPI do
     end
 
     it_behaves_like 'an identified resource' do
-      let(:resource_id) { purged_resource.id }
-    end
-
-    it_behaves_like 'an identified resource' do
       let(:parent_id) { "doesNotExist" }
+      let(:resource_class) { Folder }
     end
 
     it_behaves_like 'an identified resource' do
+      let(:resource_class) { Folder }
       before do
-        parent.update_columns(is_deleted: true)
+        parent_folder.update_columns(is_deleted: true)
       end
     end
 
@@ -85,15 +91,18 @@ describe DDS::V1::TrashbinAPI do
 
     it_behaves_like 'a kinded resource' do
       let(:parent_kind) { 'invalid-kind' }
+      let(:resource_kind) { 'invalid-kind' }
     end
 
     it_behaves_like 'an authenticated resource'
     it_behaves_like 'an authorized resource'
-    it_behaves_like 'an annotate_audits endpoint'
+    it_behaves_like 'an annotate_audits endpoint' do
+      let(:expected_audits) { 3 }
+    end
 
     it_behaves_like 'an updatable resource' do
       it 'restores the object to the requested parent' do
-        is_expected.to eq(201)
+        is_expected.to eq(expected_response_status)
         trashed_resource.reload
         expect(trashed_resource.is_deleted).to be_falsey
         expect(trashed_resource.parent.id).to eq(parent_id)
@@ -101,13 +110,15 @@ describe DDS::V1::TrashbinAPI do
 
       it_behaves_like 'a software_agent accessible resource' do
         it 'restores the object to the requested parent' do
-          is_expected.to eq(200)
+          is_expected.to eq(expected_response_status)
           trashed_resource.reload
           expect(trashed_resource.is_deleted).to be_falsey
           expect(trashed_resource.parent.id).to eq(parent_id)
         end
 
-        it_behaves_like 'an annotate_audits endpoint'
+        it_behaves_like 'an annotate_audits endpoint' do
+            let(:expected_audits) { 2 }
+        end
       end
     end
 
@@ -116,13 +127,20 @@ describe DDS::V1::TrashbinAPI do
         let(:file_version) {
           fv = untrashed_resource.file_versions.first
           fv.update_columns(is_deleted: true)
+          fv.reload
+          fv
         }
+        let(:resource) { file_version }
+        let(:resource_id) { file_version.id }
         let(:resource_kind) { file_version.kind }
+        let(:resource_class) { FileVersion }
+        let(:resource_serializer) { FileVersionSerializer }
         let(:payload) {{}}
         it_behaves_like 'an updatable resource' do
           it 'restores the object' do
             expect(file_version.is_deleted).to be_truthy
-            is_expected.to eq(201)
+            expect(file_version.data_file.is_deleted).to be_falsey
+            is_expected.to eq(expected_response_status)
             file_version.reload
             expect(file_version.is_deleted).to be_falsey
           end
@@ -138,40 +156,49 @@ describe DDS::V1::TrashbinAPI do
         let(:resource_kind) { file_version.kind }
         let(:resource_id) { file_version.id }
 
-        it_behaves_like 'an identified resource'
-      end
-
-      context 'data_file with multiple deleted file_versions'
-      let(:deleted_file_versions) { FactoryGirl.create_list(:file_version, 3, :deleted, data_file: trashed_resource) }
-
-        it_behaves_like 'an updatable resource' do
-          it 'restores the file and all file_versions to the requested parent' do
-            deleted_file_versions.each do |fv|
-              expect(fv.is_deleted).to be_truthy
-            end
-            is_expected.to eq(201)
-            trashed_resource.reload
-            expect(trashed_resource.is_deleted).to be_falsey
-            expect(trashed_resource.parent.id).to eq(parent_id)
-            deleted_file_versions.each do |fv|
-              expect(fv.is_deleted).to be_falsey
-            end
-          end
+        it_behaves_like 'an identified resource' do
+          let(:expected_suggestion) { "Restore #{file_version.data_file.kind} #{file_version.data_file_id}" }
         end
       end
+    end
+
+    context 'object is not Restorable' do
+      let(:resource) { project }
+      let(:resource_id) { project.id }
+      let(:resource_kind) { project.kind }
+      let(:resource_class) { Project }
+
+      before do
+        resource.update_columns(is_deleted: true)
+      end
+      it_behaves_like 'an identified resource' do
+        let(:expected_suggestion) { "#{project.kind} is not Restorable" }
+      end
+    end
+
+    context 'already purged object' do
+      let(:resource) { purged_resource }
+      let(:resource_id) { purged_resource.id }
+      let(:resource_kind) { purged_resource.kind }
+      let(:resource_class) { purged_resource.class }
+
+      it_behaves_like 'a viewable resource'
     end
   end
 
   describe 'PUT /trashbin/{object_kind}/{object_id}/purge' do
-    let(:url) { "/api/v1/trashbin/#{resource_kind}/#{resource_id}" }
+    let(:url) { "/api/v1/trashbin/#{resource_kind}/#{resource_id}/purge" }
     subject { put(url, params: payload.to_json, headers: headers) }
     let(:called_action) { 'PUT' }
     let(:trashed_file_version) {
       fv = trashed_resource.file_versions.first
       fv.update_columns(is_deleted: true)
+      fv
     }
+    let(:resource) { trashed_resource }
     let(:resource_kind) { trashed_resource.kind }
     let(:resource_id) { trashed_resource.id }
+    let(:resource_class) { trashed_resource.class }
     let(:payload) {{}}
 
     it_behaves_like 'an identified resource' do
@@ -183,12 +210,10 @@ describe DDS::V1::TrashbinAPI do
     end
 
     it_behaves_like 'an identified resource' do
-      let(:resource_id) { purged_resource.id }
-    end
-
-    it_behaves_like 'an identified resource' do
       let(:resource_id) { trashed_file_version.id }
+      let(:resource_class) { trashed_file_version.class }
       let(:resource_kind) { trashed_file_version.kind }
+      let(:expected_suggestion) { "#{trashed_file_version.kind} is not Purgable" }
     end
 
     it_behaves_like 'a kinded resource' do
@@ -197,23 +222,54 @@ describe DDS::V1::TrashbinAPI do
 
     it_behaves_like 'an authenticated resource'
     it_behaves_like 'an authorized resource'
-    it_behaves_like 'an annotate_audits endpoint'
+    it_behaves_like 'an annotate_audits endpoint' do
+      let(:expected_response_status) { 204 }
+    end
 
-    it_behaves_like 'an updatable resource' do
+    it 'purges the object' do
+      is_expected.to eq(204)
+      trashed_resource.reload
+      expect(trashed_resource.is_purged).to be_truthy
+    end
+
+    it_behaves_like 'a software_agent accessible resource' do
+      let(:expected_response_status) { 204 }
       it 'purges the object' do
-        is_expected.to eq(201)
+        is_expected.to eq(204)
         trashed_resource.reload
-        expect(trashed_resource.is_purged).to be_truty
+        expect(trashed_resource.is_purged).to be_truthy
       end
 
-      it_behaves_like 'a software_agent accessible resource' do
-        it 'purges the object' do
-          is_expected.to eq(201)
-          trashed_resource.reload
-          expect(trashed_resource.is_purged).to be_truty
-        end
+      it_behaves_like 'an annotate_audits endpoint' do
+        let(:expected_response_status) { 204 }
+      end
+    end
 
-        it_behaves_like 'an annotate_audits endpoint'
+    context 'object is not Purgable' do
+      let(:resource) { project }
+      let(:resource_id) { project.id }
+      let(:resource_kind) { project.kind }
+      let(:resource_class) { Project }
+
+      before do
+        resource.update_columns(is_deleted: true)
+      end
+      it_behaves_like 'an identified resource' do
+        let(:expected_suggestion) { "#{project.kind} is not Purgable" }
+      end
+    end
+
+    context 'already purged object' do
+      let(:resource) { purged_resource }
+      let(:resource_id) { purged_resource.id }
+      let(:resource_kind) { purged_resource.kind }
+      let(:resource_class) { purged_resource.class }
+
+      it 'should return an empty 204 response' do
+        is_expected.to eq(204)
+        expect(response.status).to eq(204)
+        expect(response.body).not_to eq('null')
+        expect(response.body).to be
       end
     end
   end
