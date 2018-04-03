fec29ea9c759a5580524767c335164d44e748ee0
Darin London <darin.london@duke.edu>
2018-04-03 07:52:46 -0400

    made changes based on Code Review suggestions

--- spec/support/requests_shared_examples.rb
+++ spec/support/requests_shared_examples.rb
@@ -224,17 +224,26 @@ shared_examples 'a sorted index resource' do |expected_last_item_sym|
     expect(response_json).to have_key('results')
     returned_results = response_json['results']
     expect(returned_results).to be_a(Array)
+    expect(returned_results).not_to be_empty
     last_date = nil
     returned_results.each do |this_result|
       this_result_object = KindnessFactory.by_kind(this_result["kind"]).find(this_result["id"])
-      if last_date.nil?
-        last_date = this_result_object.send(sort_column)
-      elsif sort_order == "asc"
+      last_date = this_result_object.send(sort_column) if last_date.nil?
+
+      if sort_order == "asc"
         expect(this_result_object.send(sort_column)).to be >= last_date
       else
         expect(this_result_object.send(sort_column)).to be <= last_date
       end
+      last_date = this_result_object.send(sort_column)
     end
+  end
+
+  it 'should end with the expected last item' do
+    is_expected.to eq(expected_response_status)
+    response_json = JSON.parse(response.body)
+    expect(response_json).to have_key('results')
+    returned_results = response_json['results']
     expect(returned_results.last["id"]).to eq(expected_last_item.id)
   end
 end
