ae122b3747a4e231b9624e6f024f8383dc168888
Darrin Mann <darrin.mann@duke.edu>
2018-07-24 18:17:26 -0400

    JobTransaction methods to help with cleanup task

--- app/models/job_transaction.rb
+++ app/models/job_transaction.rb
@@ -5,4 +5,19 @@ class JobTransaction < ActiveRecord::Base
   validates :key, presence: true
   validates :request_id, presence: true
   validates :state, presence: true
+
+  def self.oldest_completed_at
+    unscope(:order).order(:created_at).where(state: 'complete').first&.created_at
+  end
+
+  def self.delete_all_complete_by_request_id
+    created_before = Time.now
+    where(request_id: select(:request_id).where(state: 'complete')).where('created_at < ?', created_before).delete_all
+  end
+
+#months_ago = ((Time.now - j.created_at) / 1.month).floor
+#months_ago.downto(2).each do |m|
+#  del_num = JobTransaction.where(request_id: JobTransaction.select(:request_id).where(state: 'complete')).where('created_at < ?', Time.now - m.months).delete_all
+#  puts "deleted #{del_num} from #{m} months ago"
+#end
 end
--- spec/models/job_transaction_spec.rb
+++ spec/models/job_transaction_spec.rb
@@ -13,4 +13,49 @@ RSpec.describe JobTransaction, type: :model do
       is_expected.to validate_presence_of(:state)
     }
   end
+
+  describe '.oldest_completed_at' do
+    let(:oldest_completed_at) { described_class.oldest_completed_at }
+    it { expect(described_class).to respond_to(:oldest_completed_at) }
+    it { expect(oldest_completed_at).to be_nil }
+
+    context 'with multiple completed' do
+      let(:complete_jobs) { FactoryBot.create_list(:job_transaction, 3, state: 'complete') }
+      let(:complete_times) { complete_jobs.collect {|j| j.created_at} }
+      before(:each) do
+        expect(complete_jobs).to be_a Array
+        complete_jobs.each {|j| expect(j.reload).to be_truthy}
+      end
+      it { expect(complete_times.uniq).to eq complete_times }
+      it { expect(complete_times.sort).to eq complete_times }
+      it { expect(oldest_completed_at).to eq complete_times.first }
+    end
+  end
+
+  describe '.delete_all_complete_by_request_id' do
+    let(:delete_all_complete_by_request_id) { described_class.delete_all_complete_by_request_id }
+    it { expect(described_class).to respond_to(:delete_all_complete_by_request_id) }
+    it { expect(delete_all_complete_by_request_id).to eq 0 }
+
+    context 'with multiple completed' do
+      let(:complete_jobs) { FactoryBot.create_list(:job_transaction, 3, state: 'complete') }
+      before(:each) do
+        expect(complete_jobs).to be_a Array
+      end
+      it { expect{delete_all_complete_by_request_id}.to change{JobTransaction.count}.by(-3) }
+    end
+
+    context 'with multiple completed and referencing request ids' do
+      let(:initial_jobs) { FactoryBot.create_list(:job_transaction, 3) }
+      let(:complete_jobs) do
+        initial_jobs.collect do |i|
+          FactoryBot.create(:job_transaction, state: 'complete', request_id: i.request_id)
+        end
+      end
+      before(:each) do
+        expect(complete_jobs).to be_a Array
+      end
+      it { expect{delete_all_complete_by_request_id}.to change{JobTransaction.count}.by(-6) }
+    end
+  end
 end
