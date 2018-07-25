c62984a3395a202286e86520f507ba3c2593c567
Darrin Mann <darrin.mann@duke.edu>
2018-07-25 14:36:01 -0400

    Loop from the oldest completed month forward

--- lib/tasks/job_transaction.rake
+++ lib/tasks/job_transaction.rake
@@ -1,5 +1,13 @@
 namespace :job_transaction do
   desc 'Removes completed JobTransactions older than a month'
   task clean_up: :environment do
+    if oldest = JobTransaction.oldest_completed_at
+      months_ago = ((Time.now - oldest) / 1.month).floor
+      months_ago.downto(1).each do |m|
+        puts "Delete from #{m} #{'month'.pluralize(m)} ago."
+      end
+    else
+      puts "No completed JobTransactions found."
+    end
   end
 end
--- spec/lib/tasks/job_transaction_rake_spec.rb
+++ spec/lib/tasks/job_transaction_rake_spec.rb
@@ -3,5 +3,18 @@ require 'rails_helper'
 describe 'job_transaction:clean_up' do
   include_context "rake"
 
-  it { expect {invoke_task}.not_to raise_error }
+  let(:oldest_completed_at) { nil }
+  before(:each) do
+    expect(JobTransaction).to receive(:oldest_completed_at).and_return(oldest_completed_at)
+  end
+
+  it { invoke_task(expected_stdout: /No completed JobTransactions found./) }
+
+  context 'when oldest completed from 4 months ago' do
+    let(:oldest_completed_at) { Time.now - 4.month - 1.day }
+    it { invoke_task(expected_stdout: /Delete from 4 months ago./) }
+    it { invoke_task(expected_stdout: /Delete from 3 months ago./) }
+    it { invoke_task(expected_stdout: /Delete from 2 months ago./) }
+    it { invoke_task(expected_stdout: /Delete from 1 month ago./) }
+  end
 end
