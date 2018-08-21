fdb4d3723bc2f198a8899fd33b72e99ff0b57e38
Darin London <darin.london@duke.edu>
2018-08-21 12:59:05 -0400

    uses saved_changes? as a guard clause on the after_save instead of in the method

--- app/models/project_permission.rb
+++ app/models/project_permission.rb
@@ -2,7 +2,7 @@ class ProjectPermission < ActiveRecord::Base
   include RequestAudited
   default_scope { order('created_at DESC') }
   audited
-  after_save :update_project_etag
+  after_save :update_project_etag, if: :saved_changes?
   after_destroy :update_project_etag
 
   belongs_to :user
@@ -17,12 +17,10 @@ class ProjectPermission < ActiveRecord::Base
   private
 
   def update_project_etag
-    if saved_changes?
-      last_audit = self.audits.last
-      new_comment = last_audit.comment ? last_audit.comment.merge({raised_by_audit: last_audit.id}) : {raised_by_audit: last_audit.id}
-      self.project.update(etag: SecureRandom.hex, audit_comment: new_comment)
-      last_parent_audit = self.project.audits.last
-      last_parent_audit.update(request_uuid: last_audit.request_uuid)
-    end
+    last_audit = self.audits.last
+    new_comment = last_audit.comment ? last_audit.comment.merge({raised_by_audit: last_audit.id}) : {raised_by_audit: last_audit.id}
+    self.project.update(etag: SecureRandom.hex, audit_comment: new_comment)
+    last_parent_audit = self.project.audits.last
+    last_parent_audit.update(request_uuid: last_audit.request_uuid)
   end
 end
