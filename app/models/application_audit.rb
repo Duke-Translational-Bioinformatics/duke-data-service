7f0c662a61656484c67a1d23736eacbb2911bf32
Darrin Mann <darrin.mann@duke.edu>
2018-08-21 18:25:30 -0400

    Set audit.comment[:software_agent_id] to current_user.current_software_agent before_create

--- app/models/application_audit.rb
+++ app/models/application_audit.rb
@@ -1,4 +1,8 @@
 class ApplicationAudit < Audited::Audit
+  before_create do
+    self.comment ||= {}
+    self.comment[:software_agent_id] = user&.current_software_agent&.id
+  end
   def self.store_current_user(current_user)
     ::Audited.store[:audited_user] = current_user
   end
--- spec/models/application_audit_spec.rb
+++ spec/models/application_audit_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe ApplicationAudit, type: :model do
     expect(Audited.store.keys).to eq original_store_keys
   end
 
+  it { expect{subject.save}.not_to raise_error }
+
   describe '.store_current_user' do
     it { expect(described_class).to respond_to(:store_current_user).with(1).argument }
     it { expect(subject.user).to be_nil }
@@ -17,12 +19,28 @@ RSpec.describe ApplicationAudit, type: :model do
       before(:each) do
         expect{ described_class.store_current_user(current_user) }.not_to raise_error
       end
-      let(:current_user) { FactoryBot.create(:user) }
+      let(:current_user) { FactoryBot.create(:user, :save_without_auditing) }
       it { expect(subject.user).to be_nil }
 
       context 'after subject#save' do
         before(:each) { expect(subject.save).to be_truthy }
         it { expect(subject.user).to eq current_user }
+      end
+    end
+
+    context 'when called with user.current_software_agent set' do
+      before(:each) do
+        expect{ described_class.store_current_user(current_user) }.not_to raise_error
+      end
+      let(:current_agent) { FactoryBot.create(:software_agent, :save_without_auditing) }
+      let(:current_user) { FactoryBot.create(:user, :save_without_auditing, current_software_agent: current_agent) }
+      it { expect(subject.user).to be_nil }
+
+      context 'after subject#save' do
+        before(:each) { expect(subject.save).to be_truthy }
+        it { expect(subject.user).to eq current_user }
+        it { expect(subject.comment).to be_a Hash }
+        it { expect(subject.comment).to include('software_agent_id' => current_agent.id) }
       end
     end
   end
