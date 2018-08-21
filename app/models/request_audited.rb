ba4bbcec15f566b8b6904f675d0824a32e99249c
Darrin Mann <darrin.mann@duke.edu>
2018-08-21 18:38:48 -0400

    Replacing RequestAudited current_user logic with ApplicationAudit.store_current_user

--- app/models/request_audited.rb
+++ app/models/request_audited.rb
@@ -1,17 +1,7 @@
 module RequestAudited
   def around_audit
-    current_user = Audited.store[:current_user]
     audit_attributes = Audited.store[:audit_attributes]
-    if current_user
-      if current_user.current_software_agent
-        audit_attributes[:comment][:software_agent_id] = current_user.current_software_agent.id 
-      end
-      Audited.audit_class.as_user(current_user) do
-        yield
-      end
-    else
-      yield
-    end
+    yield
     if audit_attributes
       audit = audits.last
       audit_attributes[:comment] = (audit.comment || {}).merge(audit_attributes[:comment])
