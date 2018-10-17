d8f693595163ef6e03579b97cbed89f47504bdb5
Darrin Mann <darrin.mann@duke.edu>
2018-10-17 10:55:54 -0400

    Implement IdentityProvider#affiliates using keyword arguments

--- spec/models/identity_provider_spec.rb
+++ spec/models/identity_provider_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe IdentityProvider, type: :model do
   describe 'interface' do
     describe '#affiliates' do
       it { is_expected.to respond_to(:affiliates).with(0).arguments }
-      it { is_expected.to respond_to(:affiliates).with(1).argument }
+      it { is_expected.to respond_to(:affiliates).with_keywords(:full_name_contains) }
       it { expect{ subject.affiliates }.to raise_error(NotImplementedError) }
-      it { expect{ subject.affiliates('foo') }.to raise_error(NotImplementedError) }
+      it { expect{ subject.affiliates(full_name_contains: 'foo') }.to raise_error(NotImplementedError) }
     end
 
     describe '#affiliate' do
