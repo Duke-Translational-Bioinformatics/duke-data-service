98726a10cb66b944a28be9d4e1223e58702ea7c9
Darrin Mann <darrin.mann@duke.edu>
2018-10-25 16:41:38 -0400

    Refactoring LdapIdentityProvider#ldap_search a bit

--- app/models/ldap_identity_provider.rb
+++ app/models/ldap_identity_provider.rb
@@ -5,28 +5,31 @@ class LdapIdentityProvider < IdentityProvider
 
   def affiliate(uid)
     ldap_search(
-      Net::LDAP::Filter.construct("uid=#{uid}")
+      filter: {username: uid}
     ).first
   end
 
   def affiliates(full_name_contains=nil)
     return [] unless full_name_contains && full_name_contains.length >= 3
     ldap_search(
-      Net::LDAP::Filter.construct("displayName=*#{full_name_contains}*")
+      filter: {full_name_contains: full_name_contains}
     )
   end
 
-  private
+  def ldap_filter(filter_hash)
+    filter_str = nil
+    if val = filter_hash[:username]
+      filter_str = "uid=#{val}"
+    elsif val = filter_hash[:full_name_contains]
+      filter_str = "displayName=*#{val}*"
+    end
+    Net::LDAP::Filter.construct(filter_str) if filter_str
+  end
 
-  def ldap_search(ldap_filter)
-    ldap = Net::LDAP.new(
-        host: host,
-        port: port,
-        base: ldap_base
-    )
+  def ldap_search(filter:)
     results = []
     success = ldap.search(
-      filter: ldap_filter,
+      filter: ldap_filter(filter),
       attributes: %w(uid duDukeID sn givenName mail displayName),
       size: 500,
       return_results: false
@@ -43,5 +46,15 @@ class LdapIdentityProvider < IdentityProvider
     end
     logger.warn "#{ldap.get_operation_result.inspect} results may have been truncated" unless success
     results
+  end
+
+  private
+
+  def ldap
+    @ldap ||= Net::LDAP.new(
+        host: host,
+        port: port,
+        base: ldap_base
+    )
   end
 end
