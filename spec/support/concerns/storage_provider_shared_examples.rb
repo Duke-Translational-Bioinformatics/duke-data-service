5d52c338ac73ca0c57eb2bc95e293913339178b8
Darin London <darin.london@duke.edu>
2018-10-31 16:33:19 -0400

    refactored 'mock all Uploads StorageProvider' to use StorageProvider Double context

--- spec/support/concerns/storage_provider_shared_examples.rb
+++ spec/support/concerns/storage_provider_shared_examples.rb
@@ -1,7 +1,7 @@
 shared_context 'mock all Uploads StorageProvider' do
   # use this when uploads get created by other factories, and
   # need their storage_provider mocked
-  let(:mocked_upload_storage_provider) { instance_double("StorageProvider") }
+  include_context 'StorageProvider Double'
   let(:mock_storage_provider_attributes) { FactoryBot.attributes_for(:storage_provider) }
   let(:mock_chunk_max_number) { mock_storage_provider_attributes[:chunk_max_number] }
   let(:mock_chunk_max_size_bytes) { mock_storage_provider_attributes[:chunk_max_size_bytes] }
@@ -12,25 +12,25 @@ shared_context 'mock all Uploads StorageProvider' do
 
   before do
     allow_any_instance_of(Upload).to receive(:storage_provider)
-      .and_return(mocked_upload_storage_provider)
-    allow(mocked_upload_storage_provider).to receive(:max_chunked_upload_size)
+      .and_return(mocked_storage_provider)
+    allow(mocked_storage_provider).to receive(:max_chunked_upload_size)
       .and_return(mock_max_chunked_upload_size)
-    allow(mocked_upload_storage_provider).to receive(:chunk_max_exceeded?)
+    allow(mocked_storage_provider).to receive(:chunk_max_exceeded?)
       .and_return(expected_chunk_max_exceeded)
-    allow(mocked_upload_storage_provider).to receive(:endpoint)
+    allow(mocked_storage_provider).to receive(:endpoint)
       .and_return(expected_endpoint)
-    allow(mocked_upload_storage_provider).to receive(:download_url) do |upload,filename=nil|
+    allow(mocked_storage_provider).to receive(:download_url) do |upload,filename=nil|
       filename ||= upload.name
       "#{Faker::Internet.url}/#{URI.encode(filename)}"
     end
-    allow(mocked_upload_storage_provider).to receive(:read_attribute_for_serialization)
+    allow(mocked_storage_provider).to receive(:read_attribute_for_serialization)
      .with(:id)
      .and_return(mock_storage_provider_id)
     mock_storage_provider_attributes.keys.each do |k|
-      allow(mocked_upload_storage_provider).to receive(k.to_sym)
+      allow(mocked_storage_provider).to receive(k.to_sym)
         .and_return(mock_storage_provider_attributes[k])
 
-      allow(mocked_upload_storage_provider).to receive(:read_attribute_for_serialization)
+      allow(mocked_storage_provider).to receive(:read_attribute_for_serialization)
        .with(k.to_sym)
        .and_return(mock_storage_provider_attributes[k])
     end
