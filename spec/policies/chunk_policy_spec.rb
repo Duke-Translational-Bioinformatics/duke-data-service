ca9b50aea601c0f6fc740ab7731408ef20f938f7
Darin London <darin.london@duke.edu>
2018-11-02 08:37:19 -0400

    need 'mock all Uploads StorageProvider' context to pass

--- spec/policies/chunk_policy_spec.rb
+++ spec/policies/chunk_policy_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 describe ChunkPolicy do
   include_context 'policy declarations'
+  include_context 'mock all Uploads StorageProvider'
 
   let(:auth_role) { FactoryBot.create(:auth_role) }
   let(:project_permission) { FactoryBot.create(:project_permission, auth_role: auth_role) }
-  let(:upload) { FactoryBot.create(:upload, project: project_permission.project) }
-  let(:chunk) { FactoryBot.create(:chunk, upload: upload) }
-  let(:other_chunk) { FactoryBot.create(:chunk) }
+  let(:upload) { FactoryBot.create(:upload, :skip_validation, project: project_permission.project) }
+  let(:chunk) { FactoryBot.create(:chunk, :skip_validation, upload: upload) }
+  let(:other_chunk) { FactoryBot.create(:chunk, :skip_validation) }
 
   it_behaves_like 'system_permission can access', :chunk
   it_behaves_like 'system_permission can access', :other_chunk
