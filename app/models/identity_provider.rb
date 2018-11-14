40e02f70b246994b10df462d5728e58d85131673
Darrin Mann <darrin.mann@duke.edu>
2018-11-14 11:22:23 -0500

    Adding offset and limit attrs to IdentityProvider in preparation for pagination

--- app/models/identity_provider.rb
+++ app/models/identity_provider.rb
@@ -2,6 +2,8 @@ class IdentityProvider < ActiveRecord::Base
   validates :host, presence: true
   validates :port, presence: true
 
+  attr_accessor :affiliates_offset, :affiliates_limit
+
   def affiliates(full_name_contains: nil, username: nil, email: nil)
     raise NotImplementedError
   end
