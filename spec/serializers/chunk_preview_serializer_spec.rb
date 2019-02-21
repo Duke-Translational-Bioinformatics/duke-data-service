c3fc0c2cc1a69123c082b00286f7d3290accefe2
Darrin Mann <darrin.mann@duke.edu>
2019-02-21 11:54:11 -0500

    Chunk#chunked_upload association change. Passing serializer specs

--- spec/serializers/chunk_preview_serializer_spec.rb
+++ spec/serializers/chunk_preview_serializer_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe ChunkPreviewSerializer, type: :serializer do
   include_context 'mocked StorageProvider'
   include_context 'mocked StorageProvider Interface'
-  let(:upload) { FactoryBot.create(:upload, :with_chunks, storage_provider: mocked_storage_provider) }
-  let(:resource) { upload.chunks.first }
+  let(:chunked_upload) { FactoryBot.create(:chunked_upload, :with_chunks, storage_provider: mocked_storage_provider) }
+  let(:resource) { chunked_upload.chunks.first }
 
   let(:expected_attributes) {{
     'number' => resource.number,
