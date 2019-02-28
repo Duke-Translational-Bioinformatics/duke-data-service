62dfcc87d2c133262c32b797b93e7c49f64d765d
Darrin Mann <darrin.mann@duke.edu>
2019-02-28 11:43:28 -0500

    Scaffolding of NonChunkedUpload methods

--- app/models/non_chunked_upload.rb
+++ app/models/non_chunked_upload.rb
@@ -1,2 +1,21 @@
 class NonChunkedUpload < Upload
+  validates :size, numericality:  {
+    less_than: :max_size_bytes,
+    message: ->(object, data) do
+      "File size is currently not supported - maximum size is #{object.max_size_bytes}"
+    end
+  }
+
+  def max_size_bytes
+    1
+  end
+
+  def purge_storage
+  end
+
+  def complete
+  end
+
+  def complete_and_validate_integrity
+  end
 end
--- spec/models/non_chunked_upload_spec.rb
+++ spec/models/non_chunked_upload_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe NonChunkedUpload, type: :model do
   it { is_expected.to be_an Upload }
+
+  # Validations
+  it { is_expected.to validate_numericality_of(:size)
+    .is_less_than(subject.max_size_bytes)
+    .with_message("File size is currently not supported - maximum size is #{subject.max_size_bytes}") }
+
+  # Instance methods
+  it { is_expected.to respond_to :max_size_bytes }
+  describe '#max_size_bytes' do
+    it { expect(subject.max_size_bytes).to be_a Integer }
+  end
+
+  it { is_expected.to respond_to :purge_storage }
+  it { is_expected.to respond_to :complete }
+  it { is_expected.to respond_to :complete_and_validate_integrity }
 end
