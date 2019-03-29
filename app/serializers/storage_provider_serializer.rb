373484a405549c201cccf58e85b59da07b792825
Darrin Mann <darrin.mann@duke.edu>
2019-03-29 14:50:59 -0400

    StorageProviderSerializer is_default attribute

--- app/serializers/storage_provider_serializer.rb
+++ app/serializers/storage_provider_serializer.rb
@@ -1,5 +1,5 @@
 class StorageProviderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :is_deprecated, :chunk_hash_algorithm,
+  attributes :id, :name, :description, :is_deprecated, :is_default, :chunk_hash_algorithm,
   :chunk_max_number, :chunk_max_size_bytes
 
   def name
