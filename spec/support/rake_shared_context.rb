1ce76752c395623d946bfe497e171ae17c1ce07a
Darrin Mann <darrin.mann@duke.edu>
2019-04-11 10:54:15 -0400

    Enables coverage for rake tasks

--- spec/support/rake_shared_context.rb
+++ spec/support/rake_shared_context.rb
@@ -1,9 +1,8 @@
 require "rake"
 
 shared_context "rake" do
-  let(:rake) { Rake::Application.new }
+  let(:rake) { Rake.application }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
   subject { rake[task_name] }
 
   def invoke_task(expected_stdout: //, expected_stderr: //)
@@ -14,15 +13,9 @@ shared_context "rake" do
     }.to output(expected_stdout).to_stdout
   end
 
-  def loaded_files_excluding_current_rake_file
-    $".reject {|file| file == Rails.root.join("#{task_path}.rake").to_s }
-  end
-
-  before do
-    Rake.application = rake
-    Rake.application.rake_require(task_path, [Rails.root.to_s], loaded_files_excluding_current_rake_file)
-
-    Rake::Task.define_task(:environment)
+  # This allows tasks to be invoked more than once.
+  after do
+    subject.reenable
   end
 end
 
