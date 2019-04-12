61ba5b58c8a8d65e8e4ac65bbbf28b0b49a53438
Darrin Mann <darrin.mann@duke.edu>
2019-04-12 11:26:44 -0400

    Folder is not templatable. Removing code uses meta_templates with folders.

--- app/serializers/search/folder_serializer.rb
+++ app/serializers/search/folder_serializer.rb
@@ -1,13 +1,3 @@
 class Search::FolderSerializer < FolderSerializer
-  attributes :kind, :id, :parent, :name, :is_deleted, :audit,
-  :meta
-
-  def meta
-    object.meta_templates.each_with_object({}) do |meta_template, metadata|
-      metadata[meta_template.template.name] = {}
-      meta_template.meta_properties.each_with_object(metadata[meta_template.template.name]) do |prop, meta_prop|
-        meta_prop[prop.property.key] = prop.value
-      end
-    end
-  end
+  attributes :kind, :id, :parent, :name, :is_deleted, :audit
 end
--- spec/serializers/search/folder_serializer_spec.rb
+++ spec/serializers/search/folder_serializer_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Search::FolderSerializer, type: :serializer do
-
   let(:folder) { FactoryBot.create(:folder) }
+
   it_behaves_like 'a serialized Folder', :folder do
     it_behaves_like 'a json serializer'
   end
