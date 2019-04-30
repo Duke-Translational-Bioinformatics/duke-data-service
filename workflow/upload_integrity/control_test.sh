e3e22fcc53715e4558854aa71d2e86d6ab302de0
Darrin Mann <darrin.mann@duke.edu>
2019-04-30 17:38:10 -0400

    Use Upload info to test chunk md5s

--- workflow/upload_integrity/control_test.sh
+++ workflow/upload_integrity/control_test.sh
@@ -46,6 +46,22 @@ resp=$(dds_curl GET "/files/${file_id}/url")
 echo "${resp}" | jq
 download_data ${download_location} ${resp}
 
+echo Get Upload info: ${upload_id}
+resp=$(dds_curl GET "/uploads/${upload_id}")
+echo "${resp}" | jq
+
+offset=0
+for i in $(echo "${resp}" | jq -r '.chunks | sort_by(.number)[] | [(.number | tostring), (.size | tostring), .hash.value] | join(",")'); do
+  chunk_number=$(echo $i | cut -d, -f1)
+  chunk_size=$(echo $i | cut -d, -f2)
+  expected_md5=$(echo $i | cut -d, -f3)
+  download_md5=`dd skip=${offset} count=${chunk_size} if=${download_location} of=/dev/stdout bs=1 status=none | md5sum | cut -f1 -d' '`
+  echo Testing md5 for chunk number ${chunk_number} of size ${chunk_size}
+  echo Expected: ${expected_md5}
+  echo Actual: ${expected_md5}
+  offset=$((offset + chunk_size))
+done
+
 downloaded_file_size=`wc -c ${upload_location} | cut -f1 -d' '`
 downloaded_file_md5=`md5sum ${upload_location} | cut -f1 -d' '`
 
