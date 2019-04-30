86021ca7fc1490759c959473165bd493fbc25212
Darrin Mann <darrin.mann@duke.edu>
2019-04-30 15:29:36 -0400

    Breaking things up into separate files

---
+++ workflow/upload_integrity/includes/common_usage.sh
@@ -0,0 +1,27 @@
+# Common usage for upload integrity test scripts
+
+usage() {
+  echo "usage: $0 data_dir"
+  echo 'Requires environment variables: DDS_URL DDS_AUTH_TOKEN DDS_PROJECT_ID'
+  exit 1
+}
+data_dir=$1
+if [ -z ${data_dir} ]
+then
+  usage
+fi
+dds_url=$DDS_URL
+if [ -z $dds_url ]
+then
+  usage
+fi
+auth_token=$DDS_AUTH_TOKEN
+if [ -z $auth_token ]
+then
+  usage
+fi
+project_id=$DDS_PROJECT_ID
+if [ -z $project_id ]
+then
+  usage
+fi
---
+++ workflow/upload_integrity/includes/curl_functions.sh
@@ -0,0 +1,58 @@
+# Curl functions for DDS
+which jq > /dev/null
+if [ $? -gt 0 ]
+then
+  echo "install jq https://stedolan.github.io/jq/"
+  exit 1
+fi
+
+dds_curl() {
+  unset success
+  until [ "${success}" = "yes" ]; do
+    if [ -z $3 ]
+    then
+      curl_resp=`curl -k -s -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: ${auth_token}" -X $1 "${dds_url}/api/v1/$2"`
+    else
+      curl_resp=`curl -k -s -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: ${auth_token}" -X $1 -d $3 "${dds_url}/api/v1/$2"`
+    fi
+    if [ $? -gt 0 ]
+    then
+      echo "Problem!" >&2
+      exit 1
+    fi
+    error=`echo ${curl_resp} | jq '.error'`
+    if [ ${error} = null ]
+    then
+      echo ${curl_resp} # | jq
+      success='yes'
+    else
+      error_code=`echo ${curl_resp} | jq '.code'`
+      echo "error_code = ${error_code}" >&2
+      if [ "${error_code}" = '"resource_not_consistent"' ]
+      then
+        echo 'waiting...' >&2
+        sleep 1
+      else
+        echo ${curl_resp} | jq >&2
+        echo "Problem!" >&2
+        exit 1
+      fi
+    fi
+  done
+}
+
+upload_data() {
+  file=$1
+  host=`echo $2 | jq -r '.host'`
+  put_url=`echo $2 | jq -r '.url'`
+  echo "Uploading data from ${file} to ${host}${put_url}"
+  curl -k -s -T ${file} "${host}${put_url}"
+}
+
+download_data() {
+  file=$1
+  host=`echo $2 | jq -r '.host'`
+  put_url=`echo $2 | jq -r '.url'`
+  echo "Downloading data from ${host}${put_url} to ${file}"
+  curl -k -s -o ${file} "${host}${put_url}"
+}
