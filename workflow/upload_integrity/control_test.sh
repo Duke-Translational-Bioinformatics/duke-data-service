ad37e680e629a40ce5a0237c08575985794affbf
Darrin Mann <darrin.mann@duke.edu>
2019-05-01 15:37:31 -0400

    Move test table logic into functions

--- workflow/upload_integrity/control_test.sh
+++ workflow/upload_integrity/control_test.sh
@@ -5,26 +5,10 @@ source includes/integrity_functions.sh
 source includes/common_usage.sh
 source includes/default_variables.sh
 
+echo '# Upload without errors'
+
 source includes/run_upload_workflow.sh
 
-echo Test chunks:
-echo "Chunk #	| Size  | Expected MD5                     | Actual MD5"
-echo "-------	| ----- | ------------                     | ----------"
-offset=0
-for chunk in $(echo "${upload_get_resp}" | jq --compact-output '.chunks | sort_by(.number)[]'); do
-  chunk_number=$(echo "${chunk}" | jq -r '.number')
-  chunk_size=$(echo "${chunk}" | jq -r '.size')
-  expected_md5=$(echo "${chunk}" | jq -r '.hash.value')
-  actual_md5=$(chunk_md5sum "${chunk}" ${download_location})
-  echo "${chunk_number}	| ${chunk_size}	| ${expected_md5} | ${actual_md5}"
-  offset=$((offset + chunk_size))
-done
-
-downloaded_file_size=`wc -c ${upload_location} | cut -f1 -d' '`
-downloaded_file_md5=`md5sum ${upload_location} | cut -f1 -d' '`
-
-echo Test total file:
-echo "         | Size	| MD5"
-echo "-------- | ----	| ---"
-echo "Expected | ${upload_size}	| ${upload_md5}"
-echo "Actual   | ${downloaded_file_size}	| ${downloaded_file_md5}"
+echo "${upload_get_resp}" | jq '.status'
+test_chunks_table "${upload_get_resp}" "${download_location}"
+test_file_table "${upload_get_resp}" "${download_location}"
--- workflow/upload_integrity/includes/integrity_functions.sh
+++ workflow/upload_integrity/includes/integrity_functions.sh
@@ -1,9 +1,41 @@
 # Functions for checking data integrity
 
 chunk_md5sum() {
-  chunk=$1
-  input_file=$2
-  chunk_number=$(echo "${chunk}" | jq -r '.number')
-  chunk_size=$(echo "${chunk}1" | jq -r '.size')
+  offset=$1
+  chunk_size=$2
+  input_file=$3
+  echo "chunk_md5sum '${offset}' '${chunk_size}' '${input_file}'" >&2
   dd skip=${offset} count=${chunk_size} if=${input_file} of=/dev/stdout bs=1 status=none | md5sum | cut -f1 -d' '
+}
+
+test_chunks_table() {
+  upload=$1
+  file_location=$2
+  echo Test chunks:
+  echo "Chunk #	| Size  | Expected MD5                     | Actual MD5"
+  echo "-------	| ----- | ------------                     | ----------"
+  offset=0
+  for chunk in $(echo "${upload}" | jq --compact-output '.chunks | sort_by(.number)[]'); do
+    chunk_number=$(echo "${chunk}" | jq -r '.number')
+    chunk_size=$(echo "${chunk}" | jq -r '.size')
+    expected_md5=$(echo "${chunk}" | jq -r '.hash.value')
+    actual_md5=$(chunk_md5sum ${offset} ${chunk_size} ${file_location})
+    echo "${chunk_number}	| ${chunk_size}	| ${expected_md5} | ${actual_md5}"
+    offset=$((offset + chunk_size))
+  done
+}
+
+test_file_table() {
+  upload=$1
+  file_location=$2
+  expected_file_size=$(echo "${upload}" | jq -r '.size')
+  expected_file_md5=$(echo "${upload}" | jq -r '.hashes[0].value')
+  downloaded_file_size=`wc -c ${file_location} | cut -f1 -d' '`
+  downloaded_file_md5=`md5sum ${file_location} | cut -f1 -d' '`
+
+  echo Test total file:
+  echo "         | Size	| MD5"
+  echo "-------- | ----	| ---"
+  echo "Expected | ${expected_file_size}	| ${expected_file_md5}"
+  echo "Actual   | ${downloaded_file_size}	| ${downloaded_file_md5}"
 }
