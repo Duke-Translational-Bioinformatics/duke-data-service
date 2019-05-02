f9988886ef5db7c95ab4c194c057b3c19dc534e7
Darrin Mann <darrin.mann@duke.edu>
2019-05-02 11:26:28 -0400

    Print new Chunk 1 payload

--- workflow/upload_integrity/missing_chunk_test.sh
+++ workflow/upload_integrity/missing_chunk_test.sh
@@ -8,7 +8,7 @@ source includes/default_variables.sh
 echo '# Upload missing chunk 1'
 
 chunk_1_payload='{"number":"2","size":"'${chunk_1_size}'","hash":{"value":"'${chunk_1_md5}'","algorithm":"md5"}}'
-echo "Chunk 1 payload: ${upload_complete_payload}"
+echo "Chunk 1 payload: ${chunk_1_payload}"
 echo '(This chunk will be overwritten when Chunk 2 is submitted.)'
 
 source includes/run_upload_workflow.sh
