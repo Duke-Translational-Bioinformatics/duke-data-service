42d42d7b47a393856536d974d3de246c72bee8f8
Darin London <darin.london@duke.edu>
2019-05-10 08:08:34 -0400

    no longer being used in any Dockerfile

--- docker/includes/install_ssl_cert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p /etc/pki/tls /etc/pki/tls/private /etc/pki/tls/certs
-/usr/bin/openssl req -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/localhost.key -config /root/installs/cert_config -out /etc/pki/tls/certs/localhost.csr
-/usr/bin/openssl x509 -req -days 36500 -in /etc/pki/tls/certs/localhost.csr -signkey /etc/pki/tls/private/localhost.key -out /etc/pki/tls/certs/localhost.crt
