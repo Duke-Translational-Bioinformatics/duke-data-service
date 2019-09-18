0baf191eac0f0c683f126fadac77f523aec30d09
Darrin Mann <darrin.mann@duke.edu>
2019-09-18 09:38:05 -0400

    Models are not compatible with belongs_to_required_by_default options

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module DukeDataService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    config.active_record.belongs_to_required_by_default = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
