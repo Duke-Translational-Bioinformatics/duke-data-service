5e0c2b419e605a42673c85dd82aad63482278a4a
Darrin Mann <darrin.mann@duke.edu>
2019-09-18 09:36:42 -0400

    Merge existing config with rails app:update scaffolding

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -9,9 +9,13 @@ require 'neo4j/railtie'
 
 module DukeDataService
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,19 +1,72 @@
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = ENV['RAILS_CACHE_CLASSES'].present?
+
+  # Do not eager load code on boot.
   config.eager_load = ENV['RAILS_EAGER_LOAD'].present?
+
+  # Show full error reports.
   config.consider_all_requests_local       = ENV['RAILS_CONSIDER_REQUESTS_LOCAL'].present?
-  config.action_controller.perform_caching = ENV['RAILS_PERFORM_CACHING'].present?
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = ENV['RAILS_PERFORM_CACHING'].present?
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier
-  config.assets.compile = ENV['RAILS_COMPILE_ASSETS'].present?
-  config.assets.digest = ENV['RAILS_DIGEST_ASSETS'].present?
-  config.assets.debug = ENV['RAILS_DEBUG_ASSETS'].present?
-  config.assets.raise_runtime_errors = ENV['RAILS_ERRORS_ASSETS'].present?
+
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
+
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = ENV['RAILS_MAILER_ERRORS'].present?
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
   config.force_ssl = true
 
   # :debug :info :warn :error :fatal :unknown (0-5)
   config.log_level = ENV['RAILS_LOG_LEVEL'] || :debug
+
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = ENV['RAILS_COMPILE_ASSETS'].present?
+  config.assets.digest = ENV['RAILS_DIGEST_ASSETS'].present?
+  config.assets.raise_runtime_errors = ENV['RAILS_ERRORS_ASSETS'].present?
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = ENV['RAILS_DEBUG_ASSETS'].present?
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
