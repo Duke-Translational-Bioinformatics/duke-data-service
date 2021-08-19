bb8c35a768c652c67d3acbf7f01274d37c1f69e8
Darrin Mann <darrin.mann@duke.edu>
2021-08-19 10:57:20 -0400

    Rake task to create a SingleBucketS3StorageProvider

--- spec/lib/tasks/storage_provider_rake_spec.rb
+++ spec/lib/tasks/storage_provider_rake_spec.rb
@@ -164,5 +164,95 @@ describe "storage_provider" do
         end
       end
     end
+
+    context 'STORAGE_PROVIDER_TYPE single_bucket_s3' do
+      context 'missing required SINGLE BUCKET S3 ENV' do
+        include_context 'with env_override'
+        let(:env_override) { {
+          'STORAGE_PROVIDER_TYPE' => 'single_bucket_s3'
+        } }
+        it {
+          invoke_task(expected_stderr: /YOU DO NOT HAVE YOUR SINGLE BUCKET S3 ENVIRONMENT VARIABLES SET/)
+        }
+      end
+
+      context 'with required SINGLE BUCKET S3 ENV' do
+        let(:storage_provider_attributes) {
+          FactoryBot.attributes_for(:single_bucket_s3_storage_provider)
+        }
+        include_context 'with env_override'
+
+        context 'invalid SingleBucketS3StorageProvider' do
+          let(:env_override) {
+            {
+              'STORAGE_PROVIDER_TYPE' => 'single_bucket_s3',
+              'SINGLE_BUCKET_S3_DISPLAY_NAME' => storage_provider_attributes[:display_name],
+              'SINGLE_BUCKET_S3_DESCRIPTION' => storage_provider_attributes[:description],
+              'SINGLE_BUCKET_S3_ACCT' => storage_provider_attributes[:name],
+              'SINGLE_BUCKET_S3_CHUNK_HASH_ALGORITHM' => storage_provider_attributes[:chunk_hash_algorithm]
+            }
+          }
+          it {
+            invoke_task(expected_stderr: /Validation Error.*/)
+          }
+        end
+
+        context 'valid SingleBucketS3StorageProvider' do
+          before(:example) do
+            allow_any_instance_of(SingleBucketS3StorageProvider).to receive(:configure) do
+              $stderr.puts 'SingleBucketS3StorageProvider#configure called'
+            end
+          end
+          let(:env_override) {
+            {
+              'STORAGE_PROVIDER_TYPE' => 'single_bucket_s3',
+              'SINGLE_BUCKET_S3_DISPLAY_NAME' => storage_provider_attributes[:display_name],
+              'SINGLE_BUCKET_S3_DESCRIPTION' => storage_provider_attributes[:description],
+              'SINGLE_BUCKET_S3_ACCT' => storage_provider_attributes[:name],
+              'SINGLE_BUCKET_S3_USER' => storage_provider_attributes[:service_user],
+              'SINGLE_BUCKET_S3_PASS' => storage_provider_attributes[:service_pass],
+              'SINGLE_BUCKET_S3_CHUNK_HASH_ALGORITHM' => storage_provider_attributes[:chunk_hash_algorithm],
+              'SINGLE_BUCKET_S3_BUCKET_NAME' => storage_provider_attributes[:bucket_name]
+            }
+          }
+          context 'without existing instance' do
+            before(:example) do
+              expect(SingleBucketS3StorageProvider.where(
+                name: storage_provider_attributes[:name]
+              )).not_to exist
+            end
+            it { invoke_task(expected_stderr: /Configuring.*/) }
+            it { invoke_task(expected_stderr: /SingleBucketS3StorageProvider#configure called.*/) }
+            it {
+              expect {
+                invoke_task
+              }.to change{SingleBucketS3StorageProvider.count}.by(1)
+              expect(SingleBucketS3StorageProvider.where(
+                name: storage_provider_attributes[:name]
+              )).to exist
+            }
+          end
+          context 'with existing instance' do
+            before(:example) do
+              FactoryBot.create(
+                :single_bucket_s3_storage_provider,
+                name: storage_provider_attributes[:name]
+              )
+            end
+            it {
+              invoke_task(expected_stderr: /Storage provider '#{storage_provider_attributes[:name]}' already exists.*/)
+            }
+            it {
+              expect {
+                invoke_task
+              }.not_to change{SingleBucketS3StorageProvider.count}
+              expect(SingleBucketS3StorageProvider.where(
+                name: storage_provider_attributes[:name]
+              )).to exist
+            }
+          end
+        end
+      end
+    end
   end
 end
